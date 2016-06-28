b5046a98c270779d1615551388173486b052219e
Mark Reid <mreid@mozilla.com>
2016-06-28 10:51:03 -0300

    Add SMTP configuration for alerting.
    Per bug 1274968, setup smtp for alert delivery.

--- ansible/files/airflow/entrypoint.sh
+++ ansible/files/airflow/entrypoint.sh
@@ -11,7 +11,7 @@ FERNET_KEY=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fer
 # Generate Fernet key
 sed -i "s/{FERNET_KEY}/${FERNET_KEY}/" $AIRFLOW_HOME/airflow.cfg
 
-# wait for RabbitMQ
+# Wait for RabbitMQ
 j=0
 while ! curl -sI -u $RABBITMQ_CREDS http://$RABBITMQ_HOST:15672/api/whoami |grep '200 OK'; do
   j=`expr $j + 1`
@@ -27,6 +27,9 @@ sed -i "s/{DB_URI}/${DB_URI}/" $AIRFLOW_HOME/airflow.cfg
 sed -i "s/{DB_USER}/${DB_USER}/" $AIRFLOW_HOME/airflow.cfg
 sed -i "s/{DB_PASSWORD}/${DB_PASSWORD}/" $AIRFLOW_HOME/airflow.cfg
 sed -i "s/{AIRFLOW_ENABLE_AUTH}/${AIRFLOW_ENABLE_AUTH}/" $AIRFLOW_HOME/airflow.cfg
+sed -i "s/{SMTP_HOST}/${SMTP_HOST}/" $AIRFLOW_HOME/airflow.cfg
+sed -i "s/{SMTP_USER}/${SMTP_USER}/" $AIRFLOW_HOME/airflow.cfg
+sed -i "s/{SMTP_PASSWORD}/${SMTP_PASSWORD}/" $AIRFLOW_HOME/airflow.cfg
 
 i=0
 while ! nc $MYSQL_HOST $MYSQL_PORT >/dev/null 2>&1 < /dev/null; do
