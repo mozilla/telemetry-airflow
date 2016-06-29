c0b2bce82d44f2e0770df6ffe68aefd817df11e5
Mark Reid <mreid@mozilla.com>
2016-06-29 18:27:35 -0300

    Bug 1274981: Add a DAG for main_summary generation

---
+++ jobs/main_summary_view.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ -z "$bucket" || -z "$date" ]]; then
+   echo "Missing arguments!" 1>&2
+   exit 1
+fi
+
+git clone https://github.com/mozilla/telemetry-batch-view.git
+cd telemetry-batch-view
+sbt assembly
+spark-submit --master yarn-client \
+             --class com.mozilla.telemetry.views.MainSummaryView \
+             target/scala-2.10/telemetry-batch-view-1.1.jar \
+             --bucket $bucket \
+             --from $date \
+             --to $date
