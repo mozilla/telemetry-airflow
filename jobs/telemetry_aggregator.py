b1cc99458d22b8ed54326de6b4eafececb3a8093
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2016-06-30 13:14:50 +0100

    Use simple prints for logging.

--- jobs/telemetry_aggregator.py
+++ jobs/telemetry_aggregator.py
@@ -5,14 +5,9 @@ from os import environ
 from mozaggregator.aggregator import aggregate_metrics
 from mozaggregator.db import submit_aggregates
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-logger.addHandler(logging.StreamHandler())
-
 date = environ['date']
-
-logger.info("Running job for {}".format(date))
+print "Running job for {}".format(date)
 aggregates = aggregate_metrics(sc, ("nightly", "aurora", "beta", "release"), date)
-logger.info("Number of build-id aggregates: {}".format(aggregates[0].count()))
-logger.info("Number of submission date aggregates: {}".format(aggregates[1].count()))
+print "Number of build-id aggregates: {}".format(aggregates[0].count())
+print "Number of submission date aggregates: {}".format(aggregates[1].count())
 submit_aggregates(aggregates)
