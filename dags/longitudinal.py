e32c67197490f7dd5e6c0110d4a1b937ee1ab8c9
Stephen A Pohl <sapohl@users.noreply.github.com>
2016-07-08 13:23:54 -0400

    Add update orphaning job to Airflow
    This depends on the longitudinal job running successfully to run.

--- dags/longitudinal.py
+++ dags/longitudinal.py
@@ -13,7 +13,7 @@ default_args = {
     'retry_delay': timedelta(minutes=30),
 }
 
-dag = DAG('longitudinal', default_args=default_args, schedule_interval='@weekly')
+dag0 = DAG('longitudinal', default_args=default_args, schedule_interval='@weekly')
 
 t0 = EMRSparkOperator(task_id="longitudinal",
                       job_name="Longitudinal View",
@@ -21,4 +21,28 @@ t0 = EMRSparkOperator(task_id="longitudinal",
                       instance_count=30,
                       env = {"date": "{{ ds_nodash }}", "bucket": "{{ task.__class__.airflow_bucket }}"},
                       uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/longitudinal_view.sh",
-                      dag=dag)
+                      dag=dag0)
+
+update_orphaning_args = {
+    'owner': 'spohl@mozilla.com',
+    'depends_on_past': False,
+    'start_date': datetime(2016, 6, 30),
+    'email': ['telemetry-alerts@mozilla.com', 'spohl@mozilla.com',
+              'mhowell@mozilla.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=30),
+}
+
+dag1 = DAG('update_orphaning', default_args=update_orphaning_args, schedule_interval='@weekly')
+
+t1 = EMRSparkOperator(task_id="update_orphaning",
+                      job_name="Update Orphaning View",
+                      execution_timeout=timedelta(hours=10),
+                      instance_count=1,
+                      env = {"date": "{{ ds_nodash }}", "bucket": "{{ task.__class__.airflow_bucket }}"},
+                      uri="https://raw.githubusercontent.com/mozilla-services/data-pipeline/master/reports/update-orphaning/Update%20orphaning%20analysis%20using%20longitudinal%20dataset.ipynb",
+                      dag=dag1)
+
+t1.set_upstream(t0)
