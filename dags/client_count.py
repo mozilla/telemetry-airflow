49c45985d30ff0508abb841baa4aee343fd9f77b
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2016-07-08 09:40:40 +0100

    Add client count view job.

---
+++ dags/client_count.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from operators.emr_spark_operator import EMRSparkOperator
+
+default_args = {
+    'owner': 'rvitillo@mozilla.com',
+    'depends_on_past': False,
+    'start_date': datetime(2016, 7, 7),
+    'email': ['telemetry-alerts@mozilla.com', 'rvitillo@mozilla.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=30),
+}
+
+dag = DAG('client_count', default_args=default_args, schedule_interval='@weekly')
+
+t0 = EMRSparkOperator(task_id="client_count_view",
+                      job_name="Client Count View",
+                      execution_timeout=timedelta(hours=10),
+                      instance_count=20,
+                      env = {"date": "{{ ds_nodash }}", "bucket": "{{ task.__class__.airflow_bucket }}"},
+                      uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/client_count_view.sh",
+                      dag=dag)
---
+++ jobs/client_count_view.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -z "$bucket" || -z "$date" ]]; then
+    echo "Missing arguments!" 1>&2
+    exit 1
+fi
+
+git clone https://github.com/mozilla/telemetry-batch-view.git
+cd telemetry-batch-view
+sbt assembly
+spark-submit --conf spark.memory.useLegacyMode=true \
+             --conf spark.storage.memoryFraction=0 \
+             --master yarn-client \
+             --class com.mozilla.telemetry.views.ClientCountView \
+             target/scala-2.10/telemetry-batch-view-1.1.jar \
+             --to $date \
+             --bucket $bucket
