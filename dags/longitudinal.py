3844ae6d79e58d99c8a66c4ef1f41e7663a21d03
Stephen A Pohl <sapohl@users.noreply.github.com>
2016-07-11 12:29:29 -0400

    Address review feedback

--- dags/longitudinal.py
+++ dags/longitudinal.py
@@ -13,36 +13,25 @@ default_args = {
     'retry_delay': timedelta(minutes=30),
 }
 
-dag0 = DAG('longitudinal', default_args=default_args, schedule_interval='@weekly')
+dag = DAG('longitudinal', default_args=default_args, schedule_interval='@weekly')
 
 t0 = EMRSparkOperator(task_id="longitudinal",
                       job_name="Longitudinal View",
                       execution_timeout=timedelta(hours=10),
                       instance_count=30,
-                      env = {"date": "{{ ds_nodash }}", "bucket": "{{ task.__class__.airflow_bucket }}"},
+                      env={"date": "{{ ds_nodash }}", "bucket": "{{ task.__class__.airflow_bucket }}"},
                       uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/longitudinal_view.sh",
-                      dag=dag0)
-
-update_orphaning_args = {
-    'owner': 'spohl@mozilla.com',
-    'depends_on_past': False,
-    'start_date': datetime(2016, 6, 30),
-    'email': ['telemetry-alerts@mozilla.com', 'spohl@mozilla.com',
-              'mhowell@mozilla.com'],
-    'email_on_failure': True,
-    'email_on_retry': True,
-    'retries': 2,
-    'retry_delay': timedelta(minutes=30),
-}
-
-dag1 = DAG('update_orphaning', default_args=update_orphaning_args, schedule_interval='@weekly')
+                      dag=dag)
 
 t1 = EMRSparkOperator(task_id="update_orphaning",
                       job_name="Update Orphaning View",
                       execution_timeout=timedelta(hours=10),
                       instance_count=1,
-                      env = {"date": "{{ ds_nodash }}", "bucket": "{{ task.__class__.airflow_bucket }}"},
+                      owner="spohl@mozilla.com",
+                      email=["telemetry-alerts@mozilla.com", "spohl@mozilla.com",
+                             "mhowell@mozilla.com"],
+                      env={"date": "{{ ds_nodash }}", "bucket": "{{ task.__class__.airflow_bucket }}"},
                       uri="https://raw.githubusercontent.com/mozilla-services/data-pipeline/master/reports/update-orphaning/Update%20orphaning%20analysis%20using%20longitudinal%20dataset.ipynb",
-                      dag=dag1)
+                      dag=dag)
 
 t1.set_upstream(t0)
