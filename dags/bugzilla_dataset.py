d3ecb5a336b0ba493d7ce0fad8aa949be4242581
Mauro Doglio <mdoglio@mozilla.com>
2016-07-28 15:56:06 +0100

    Bug 1286201 - Add dag for bugzilla dataset update

---
+++ dags/bugzilla_dataset.py
@@ -0,0 +1,36 @@
+from airflow import DAG
+from airflow.hooks import BaseHook
+from datetime import datetime, timedelta
+from operators.emr_spark_operator import EMRSparkOperator
+
+default_args = {
+    'owner': 'mdoglio@mozilla.com',
+    'depends_on_past': False,
+    'start_date': datetime(2016, 7, 28),
+    'email': ['telemetry-alerts@mozilla.com', 'mdoglio@mozilla.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=30),
+}
+
+dag = DAG('bugzilla_dataset', default_args=default_args, schedule_interval='@daily')
+
+connection_details = BaseHook.get_connection('bugzilla_db')
+env = {
+    "DATABASE_USER": connection_details.login,
+    "DATABASE_PASSWORD": connection_details.password,
+    "DATABASE_HOST": connection_details.host,
+    "DATABASE_PORT": connection_details.port,
+    "DATABASE_NAME": connection_details.schema,
+}
+
+t0 = EMRSparkOperator(
+    task_id="update_bugs",
+    job_name="Bugzilla Dataset Update",
+    instance_count=1,
+    env=env,
+    uri = "https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/bugzilla_dataset.sh",
+    dag=dag
+)
+
