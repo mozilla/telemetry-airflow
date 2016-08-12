39628236c033f696ca7f63b465ee659a9820aa34
Mauro Doglio <mdoglio@mozilla.com>
2016-08-12 16:39:00 +0100

    Bug 1286242 - add mobile_clients dag

---
+++ dags/mobile_clients.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from operators.emr_spark_operator import EMRSparkOperator
+
+default_args = {
+    'owner': 'mdoglio@mozilla.com',
+    'depends_on_past': False,
+    'start_date': datetime(2016, 8, 12),
+    'email': ['telemetry-alerts@mozilla.com', 'mdoglio@mozilla.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=30),
+}
+
+dag = DAG('mobile_clients', default_args=default_args, schedule_interval='@daily')
+
+t0 = EMRSparkOperator(task_id="mobile_clients",
+                      job_name="Update mobile clients",
+                      execution_timeout=timedelta(hours=2),
+                      instance_count=5,
+                      owner="mdoglio@mozilla.com",
+                      email=["telemetry-alerts@mozilla.com", "mdoglio@mozilla.com"],
+                      env={"date": "{{ ds_nodash }}"},
+                      uri="https://raw.githubusercontent.com/maurodoglio/user-data-analytics/master/mobile-clients.ipynb",
+                      output_visibility="public",
+                      dag=dag)
