83f89113babfbd0a10749abd236223d35529d2d8
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2016-08-14 17:14:39 +0100

    Generate client_count view daily
    Enable daily tracking of user counts while E10s is being rolled out
    to release.

--- dags/client_count.py
+++ dags/client_count.py
@@ -5,7 +5,7 @@ from operators.emr_spark_operator import EMRSparkOperator
 default_args = {
     'owner': 'rvitillo@mozilla.com',
     'depends_on_past': False,
-    'start_date': datetime(2016, 7, 7),
+    'start_date': datetime(2016, 8, 13),
     'email': ['telemetry-alerts@mozilla.com', 'rvitillo@mozilla.com'],
     'email_on_failure': True,
     'email_on_retry': True,
@@ -13,7 +13,7 @@ default_args = {
     'retry_delay': timedelta(minutes=30),
 }
 
-dag = DAG('client_count', default_args=default_args, schedule_interval='@weekly')
+dag = DAG('client_count', default_args=default_args, schedule_interval='@daily')
 
 t0 = EMRSparkOperator(task_id="client_count_view",
                       job_name="Client Count View",
