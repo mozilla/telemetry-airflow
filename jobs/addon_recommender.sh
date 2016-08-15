a2e95c133b1dee930addbbe03e1a630c7d9e3911
Alessio Placitelli <alessio.placitelli@gmail.com>
2016-08-15 10:44:52 +0200

    Schedule training the Addon Recommender

---
+++ jobs/addon_recommender.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ -z "$privateBucket" || -z "$publicBucket" || -z "$date" ]]; then
+    echo "Missing arguments!" 1>&2
+    exit 1
+fi
+
+git clone https://github.com/mozilla/telemetry-batch-view.git
+cd telemetry-batch-view
+sbt assembly
+mkdir ml_output
+spark-submit --master yarn-client \
+             --class com.mozilla.telemetry.ml.AddonRecommender \
+             target/scala-2.10/telemetry-batch-view-1.1.jar \
+             train \
+             --output ml_output
+             --privateBucket $privateBucket
+             --publicBucket $publicBucket
+             --runDate $date
