80cfddcf34155d0c31a3e0363725a0cc31e170ac
Mauro Doglio <mauro.doglio@gmail.com>
2016-08-15 11:09:47 +0100

    Increase mobile_clients execution timeout
    The mobile_clients task is timing out after 2h, let's try to increase it.

--- dags/mobile_clients.py
+++ dags/mobile_clients.py
@@ -17,7 +17,7 @@ dag = DAG('mobile_clients', default_args=default_args, schedule_interval='@daily
 
 t0 = EMRSparkOperator(task_id="mobile_clients",
                       job_name="Update mobile clients",
-                      execution_timeout=timedelta(hours=2),
+                      execution_timeout=timedelta(hours=4),
                       instance_count=5,
                       owner="mdoglio@mozilla.com",
                       email=["telemetry-alerts@mozilla.com", "mdoglio@mozilla.com"],
