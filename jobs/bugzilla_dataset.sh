b093e0706dcb04256ad9b4ae2e725d54b8011fdd
Mauro Doglio <mdoglio@mozilla.com>
2016-08-18 14:57:42 +0100

    Fix bugzilla dataset bootstrap script
    I forgot to update this file after I restructured the layout of the
    bz2db repository. Ooops.

--- jobs/bugzilla_dataset.sh
+++ jobs/bugzilla_dataset.sh
@@ -3,5 +3,4 @@
 conda install psycopg2 --yes
 git clone https://github.com/maurodoglio/bz2db.git
 pip install -r bz2db/requirements.txt
-cd bz2db
-python update_bugs.py
+cd bz2db && python bz2db/update_bugs.py
