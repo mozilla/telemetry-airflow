9d5edc21e760b92edb840039d12d799fc1b74bc4
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2016-09-19 14:42:14 +0100

    Move client_count dataset creation to Spark 2.0

--- jobs/client_count_view.sh
+++ jobs/client_count_view.sh
@@ -5,13 +5,14 @@ if [[ -z "$bucket" || -z "$date" ]]; then
     exit 1
 fi
 
-git clone -b spark1.6 https://github.com/mozilla/telemetry-batch-view.git
+git clone https://github.com/mozilla/telemetry-batch-view.git
 cd telemetry-batch-view
 sbt assembly
 spark-submit --conf spark.memory.useLegacyMode=true \
              --conf spark.storage.memoryFraction=0 \
-             --master yarn-client \
+             --master yarn \
+             --deploy-mode client \
              --class com.mozilla.telemetry.views.ClientCountView \
-             target/scala-2.10/telemetry-batch-view-1.1.jar \
+             target/scala-2.11/telemetry-batch-view-1.1.jar \
              --to $date \
              --bucket $bucket
