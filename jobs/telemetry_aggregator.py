6721ce7623d682e76e823fa0d9b9c523c5c196ab
Mauro Doglio <mdoglio@mozilla.com>
2016-09-29 13:23:20 +0100

    Bug 1306228 - pyspark programs need a spark context
    Since we deployed the changes required for the EMR migration, we need to
    run pyspark scripts with spark-submit. For these script we now need to
    explicitly create the spark context.

--- jobs/telemetry_aggregator.py
+++ jobs/telemetry_aggregator.py
@@ -2,12 +2,16 @@
 
 import logging
 from os import environ
+from pyspark import SparkContext, SparkConf
 from mozaggregator.aggregator import aggregate_metrics
 from mozaggregator.db import submit_aggregates
 
+conf = SparkConf().setAppName('telemetry-aggregates')
+sc = SparkContext(conf=conf)
 date = environ['date']
 print "Running job for {}".format(date)
 aggregates = aggregate_metrics(sc, ("nightly", "aurora", "beta", "release"), date)
 print "Number of build-id aggregates: {}".format(aggregates[0].count())
 print "Number of submission date aggregates: {}".format(aggregates[1].count())
 submit_aggregates(aggregates)
+sc.stop()
