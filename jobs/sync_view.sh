bddc371872d1dc656ab3a8cf693e42d446439e34
Alessio Placitelli <alessio.placitelli@gmail.com>
2016-10-05 16:39:18 +0200

    Add the sync pings view job on Airflow

---
+++ jobs/sync_view.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -z "$bucket" || -z "$date" ]]; then
+   echo "Missing arguments!" 1>&2
+   exit 1
+fi
+
+git clone https://github.com/mozilla/telemetry-batch-view.git
+cd telemetry-batch-view
+sbt assembly
+spark-submit --master yarn \
+             --deploy-mode client \
+             --class com.mozilla.telemetry.views.SyncView \
+             target/scala-2.11/telemetry-batch-view-1.1.jar \
+             --bucket $bucket \
+             --from $date \
+             --to $date
