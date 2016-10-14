e82da861683a47eb7546c55639ab4e17dfdfe3dd
Sunah Suh <ssuh@mozilla.com>
2016-10-14 13:05:57 -0500

    Use normal execution date if weekly DAG was manually triggered

--- dags/utils/constants.py
+++ dags/utils/constants.py
@@ -1,1 +1,7 @@
-DS_PLUS_7_NO_DASH = '{{ macros.ds_format(macros.ds_add(ds, 7), "%Y-%m-%d", "%Y%m%d") }}'
+DS_PLUS_7_NO_DASH = (
+    '{% if dag_run.external_trigger %}'
+        '{{ ds_nodash }}'
+    '{% else %}'
+        '{{ macros.ds_format(macros.ds_add(ds, 7), "%Y-%m-%d", "%Y%m%d") }}'
+    '{% endif %}'
+)
