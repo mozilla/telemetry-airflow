6aaccd4260b1304b776a0a2baee56418ceb2b989
Sunah Suh <ssuh@mozilla.com>
2016-10-17 18:05:46 -0500

    Bug 1310715 - Weekly date param should add 6 days to execution date, not 7

--- dags/utils/constants.py
+++ dags/utils/constants.py
@@ -2,6 +2,6 @@ DS_WEEKLY = (
     '{% if dag_run.external_trigger %}'
         '{{ ds_nodash }}'
     '{% else %}'
-        '{{ macros.ds_format(macros.ds_add(ds, 7), "%Y-%m-%d", "%Y%m%d") }}'
+        '{{ macros.ds_format(macros.ds_add(ds, 6), "%Y-%m-%d", "%Y%m%d") }}'
     '{% endif %}'
 )
