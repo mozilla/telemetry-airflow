e1095e942c893be0103ebe87f3eb6167b5ecf338
Mauro Doglio <mdoglio@mozilla.com>
2016-10-31 19:05:37 +0100

    Bug 1305423 - Add mobile jobs
    These jobs have been running for months on atmo v1, it's time to move
    them to airflow.

---
+++ dags/android_events.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from operators.emr_spark_operator import EMRSparkOperator
+
+default_args = {
+    'owner': 'mdoglio@mozilla.com',
+    'depends_on_past': False,
+    'start_date': datetime(2016, 9, 20),
+    'email': ['telemetry-alerts@mozilla.com', 'mdoglio@mozilla.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=30),
+}
+
+dag = DAG('android_events', default_args=default_args, schedule_interval='@daily')
+
+t0 = EMRSparkOperator(task_id="android_events",
+                      job_name="Update android events",
+                      execution_timeout=timedelta(hours=4),
+                      instance_count=5,
+                      owner="mdoglio@mozilla.com",
+                      email=["telemetry-alerts@mozilla.com", "mdoglio@mozilla.com"],
+                      env={"date": "{{ ds_nodash }}"},
+                      uri="https://raw.githubusercontent.com/mozilla-services/data-pipeline/master/reports/android-events/android-events.ipynb",
+                      output_visibility="public",
+                      dag=dag)
