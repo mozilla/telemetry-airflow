34b92ced574cca0080d38ca31c6568a6f7b72037
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2016-11-18 07:02:59 -0800

    Bug 1314120 - Add socorro_import dag (#70)
    * Bug 1314120 - Add socorro import dag
    
    This schedules the crash data import job on a daily basis.
    
    * Specify environment and remove redundant args

---
+++ dags/socorro_import.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from operators.emr_spark_operator import EMRSparkOperator
+
+default_args = {
+    'owner': 'amiyaguchi@mozilla.com',
+    'depends_on_past': False,
+    'start_date': datetime(2016, 11, 11),
+    'email': ['telemetry-alerts@mozilla.com', 'amiyaguchi@mozilla.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=30),
+}
+
+dag = DAG('socorro_import', default_args=default_args, schedule_interval='@daily')
+
+t0 = EMRSparkOperator(task_id="socorro_import",
+                      job_name="Import crash data",
+                      execution_timeout=timedelta(hours=4),
+                      release_label="emr-5.0.0",
+                      instance_count=5,
+                      env={"date": "{{ ds_nodash }}"},
+                      uri="https://raw.githubusercontent.com/mozilla-services/data-pipeline/master/reports/socorro_import/ImportCrashData.ipynb",
+                      output_visibility="public",
+                      dag=dag)
