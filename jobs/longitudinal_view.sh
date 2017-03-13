377acca65ba8a5d4fdd764b6318c0bae61f2eda4
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2017-03-13 14:12:52 +0000

    Fix LongitudinalView crash triggered by Spark 2.0.2
    The legacy memory management mode doesn't work properly in Spark
    2.0.2 and causes LongitudinalView to crash.

--- jobs/longitudinal_view.sh
+++ jobs/longitudinal_view.sh
@@ -9,8 +9,6 @@ git clone https://github.com/mozilla/telemetry-batch-view.git
 cd telemetry-batch-view
 sbt assembly
 spark-submit --executor-cores 8 \
-             --conf spark.memory.useLegacyMode=true \
-             --conf spark.storage.memoryFraction=0 \
              --master yarn \
              --deploy-mode client \
              --class com.mozilla.telemetry.views.LongitudinalView \
