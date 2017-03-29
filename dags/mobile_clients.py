949229494bf5d6c9a0795c559a19599028c0fe10
Frank Bertsch <fbertsch@mozilla.com>
2017-03-29 08:36:35 -0500

    Extend mobile clients timeout

--- dags/mobile_clients.py
+++ dags/mobile_clients.py
@@ -17,7 +17,7 @@ dag = DAG('mobile_clients', default_args=default_args, schedule_interval='@daily
 
 t0 = EMRSparkOperator(task_id="mobile_clients",
                       job_name="Update mobile clients",
-                      execution_timeout=timedelta(hours=4),
+                      execution_timeout=timedelta(hours=8),
                       instance_count=10,
                       owner="mdoglio@mozilla.com",
                       email=["telemetry-alerts@mozilla.com", "mdoglio@mozilla.com", "frank@mozilla.com"],
