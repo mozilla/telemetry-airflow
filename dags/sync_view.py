c6b5639fbe088c8338754fd35382b7f9866a5759
Sunah Suh <ssuh@mozilla.com>
2017-04-19 14:45:21 +0200

    Add sync events view task to sync dag

--- dags/sync_view.py
+++ dags/sync_view.py
@@ -22,3 +22,12 @@ t0 = EMRSparkOperator(task_id="sync_view",
                       env={"date": "{{ ds_nodash }}", "bucket": "{{ task.__class__.private_output_bucket }}"},
                       uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/sync_view.sh",
                       dag=dag)
+
+t1 = EMRSparkOperator(task_id="sync_events_view",
+                      job_name="Sync Events View",
+                      execution_timeout=timedelta(hours=10),
+                      instance_count=1,
+                      email=['ssuh@mozilla.com'],
+                      env={"date": "{{ ds_nodash }}", "bucket": "{{ task.__class__.private_output_bucket }}"},
+                      uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/sync_events_view.sh",
+                      dag=dag)
