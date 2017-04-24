1629fe4039ff7bc5fa33c5a2def8bbe8c3b9892f
Ryan Harter <harterrt@gmail.com>
2017-04-24 11:16:43 -0400

    Add job for pulse testpilot ETL

---
+++ dags/txp_pulse.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from operators.emr_spark_operator import EMRSparkOperator
+
+default_args = {
+    'owner': 'harterrt@mozilla.com',
+    'depends_on_past': False,
+    'start_date': datetime(2017, 4, 24),
+    'email': ['telemetry-alerts@mozilla.com', 'harterrt@mozilla.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=30),
+}
+
+dag = DAG('txp_pulse', default_args=default_args, schedule_interval='@daily')
+
+t0 = EMRSparkOperator(task_id="txp_pulse",
+                      job_name="Pulse Testpilot Experiment ETL",
+                      execution_timeout=timedelta(hours=2),
+r                     instance_count=5,
+                      env={},
+                      uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/txp_pulse.sh",
+                      dag=dag)
