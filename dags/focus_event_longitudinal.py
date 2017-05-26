efa83f66533a5c2b385de07fbd210a6f03834dc1
Frank Bertsch <fbertsch@mozilla.com>
2017-05-26 10:40:41 -0500

    Add focus-event longitudinal job

---
+++ dags/focus_event_longitudinal.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from operators.emr_spark_operator import EMRSparkOperator
+from utils.constants import DS_WEEKLY
+
+default_args = {
+    'owner': 'frank@mozilla.com',
+    'depends_on_past': False,
+    'start_date': datetime(2017, 5, 26),
+    'email': ['telemetry-alerts@mozilla.com', 'frank@mozilla.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=30),
+}
+
+dag = DAG('focus_event_longitudinal', default_args=default_args, schedule_interval='@weekly')
+
+t0 = EMRSparkOperator(task_id="focus_event_longitudinal",
+                      job_name="Focus Event Longitudinal View",
+                      execution_timeout=timedelta(hours=6),
+                      instance_count=1,
+                      env={"date": DS_WEEKLY, "bucket": "{{ task.__class__.private_output_bucket }}"},
+                      uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/focus_event_longitudinal_view.sh",
+                      dag=dag)
