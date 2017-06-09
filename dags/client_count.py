b88bc251b06239327e53aec59d4706027a741864
Frank Bertsch <fbertsch@mozilla.com>
2017-06-09 13:44:36 -0500

    Make Frank owner of his jobs

--- dags/client_count.py
+++ dags/client_count.py
@@ -3,10 +3,10 @@ from datetime import datetime, timedelta
 from operators.emr_spark_operator import EMRSparkOperator
 
 default_args = {
-    'owner': 'rvitillo@mozilla.com',
+    'owner': 'frank@mozilla.com',
     'depends_on_past': False,
     'start_date': datetime(2016, 8, 13),
-    'email': ['telemetry-alerts@mozilla.com', 'rvitillo@mozilla.com'],
+    'email': ['telemetry-alerts@mozilla.com', 'frank@mozilla.com'],
     'email_on_failure': True,
     'email_on_retry': True,
     'retries': 2,
@@ -18,6 +18,7 @@ dag = DAG('client_count', default_args=default_args, schedule_interval='@daily')
 t0 = EMRSparkOperator(task_id="client_count_view",
                       job_name="Client Count View",
                       execution_timeout=timedelta(hours=10),
+                      owner="frank@mozilla.com",
                       instance_count=20,
                       env = {"date": "{{ ds_nodash }}", "bucket": "{{ task.__class__.private_output_bucket }}"},
                       uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/client_count_view.sh",
--- dags/telemetry_aggregates.py
+++ dags/telemetry_aggregates.py
@@ -3,10 +3,10 @@ from datetime import datetime, timedelta
 from operators.emr_spark_operator import EMRSparkOperator
 
 default_args = {
-    'owner': 'rvitillo@mozilla.com',
+    'owner': 'frank@mozilla.com',
     'depends_on_past': True,
     'start_date': datetime(2016, 6, 29),
-    'email': ['telemetry-alerts@mozilla.com', 'rvitillo@mozilla.com', 'fbertsch@mozilla.com'],
+    'email': ['telemetry-alerts@mozilla.com', 'frank@mozilla.com'],
     'email_on_failure': True,
     'email_on_retry': True,
     'retries': 3,
@@ -17,6 +17,7 @@ dag = DAG('telemetry_aggregates', default_args=default_args, schedule_interval='
 
 t0 = EMRSparkOperator(task_id = "telemetry_aggregate_view",
                       job_name = "Telemetry Aggregate View",
+                      owner = "frank@mozilla.com",
                       instance_count = 10,
                       execution_timeout=timedelta(hours=12),
                       env = {"date": "{{ ds_nodash }}"},
