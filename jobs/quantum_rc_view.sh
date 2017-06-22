16a1d818f6d435e93964b4f4ea40e8292781f00a
Frank Bertsch <fbertsch@mozilla.com>
2017-06-21 23:30:36 -0500

    Add quantum release criteria job

---
+++ jobs/quantum_rc_view.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -z "$bucket" || -z "$date" ]]; then
+    echo "Missing arguments!" 1>&2
+    exit 1
+fi
+
+git clone https://github.com/mozilla/telemetry-batch-view.git
+cd telemetry-batch-view
+sbt assembly
+
+spark-submit --master yarn \
+             --deploy-mode client \
+             --class com.mozilla.telemetry.views.QuantumRCView \
+             target/scala-2.11/telemetry-batch-view-1.1.jar \
+             --to "$date" \
+             --bucket "$bucket"
