d783427ed595078d1f68601d2fdc45976df9071a
Mark Reid <mreid@mozilla.com>
2017-06-27 13:05:59 -0700

    Add required arguments for latest code

--- jobs/search_rollups.sh
+++ jobs/search_rollups.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+if [[ -z "$mode" || -z "$date" ]]; then
+   echo "Missing arguments!" 1>&2
+   exit 1
+fi
+
 # Create the package for distribution across the cluster
 git clone https://github.com/mozilla/python_mozetl.git
 cd python_mozetl
 python setup.py bdist_egg
+
+bucket="net-mozaws-prod-us-west-2-pipeline-analysis"
+prefix="spenrose/search/to_vertica"
 
 # Generate the driver script
 echo 'from mozetl.search import search_rollups as sr; sr.main()' > run.py
@@ -14,4 +22,4 @@ unset PYSPARK_DRIVER_PYTHON
 spark-submit --master yarn \
              --deploy-mode client \
              --py-files dist/*.egg \
-             run.py
+             run.py $mode $date $bucket $prefix
