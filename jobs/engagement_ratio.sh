d75f5a2a7f610e53a2543377af15c8edac15619c
Sam Penrose <spenrose@mozilla.com>
2017-06-28 14:07:30 -0700

    replace MauDau.ipynb with mozetl/maudau

---
+++ jobs/engagement_ratio.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Create the package for distribution across the cluster
+git clone https://github.com/mozilla/python_mozetl.git
+cd python_mozetl
+python setup.py bdist_egg
+
+# Generate the driver script
+echo 'from mozetl.maudau import maudau; maudau.main()' > run.py
+
+# Avoid errors caused by jupyter
+unset PYSPARK_DRIVER_PYTHON
+
+spark-submit --master yarn \
+             --deploy-mode client \
+             --py-files dist/*.egg \
+             run.py
