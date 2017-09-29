e9548f2a611d3e7f917761f76308858d18b804de
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2017-09-29 13:09:40 -0700

    Fix indentation in churn

--- dags/churn.py
+++ dags/churn.py
@@ -16,24 +16,26 @@ default_args = {
 
 dag = DAG('churn', default_args=default_args, schedule_interval='0 0 * * 3')
 
-t0 = EMRSparkOperator(task_id="churn",
-                      job_name="Generate weekly desktop retention dataset",
-                      execution_timeout=timedelta(hours=4),
-                      instance_count=5,
-                      env=mozetl_envvar("churn", {
-                          "start_date": "{{ ds_nodash }}",
-                          "bucket": "{{ task.__class__.private_output_bucket }}"
-                      }),
-                      uri="https://raw.githubusercontent.com/mozilla/python_mozetl/master/bin/mozetl-submit.sh",
-                      output_visibility="public",
-                      dag=dag)
+churn = EMRSparkOperator(
+    task_id="churn",
+    job_name="Generate weekly desktop retention dataset",
+    execution_timeout=timedelta(hours=4),
+    instance_count=5,
+    env=mozetl_envvar("churn", {
+        "start_date": "{{ ds_nodash }}",
+        "bucket": "{{ task.__class__.private_output_bucket }}"
+    }),
+    uri="https://raw.githubusercontent.com/mozilla/python_mozetl/master/bin/mozetl-submit.sh",
+    output_visibility="public",
+    dag=dag)
 
-t1 = EMRSparkOperator(task_id="churn_to_csv",
-                      job_name="Generate Churn CSV files",
-                      execution_timeout=timedelta(hours=4),
-                      instance_count=1,
-                      env={"date": "{{ ds_nodash }}"},
-                      uri="https://raw.githubusercontent.com/mozilla/mozilla-reports/master/etl/churn_to_csv.kp/orig_src/churn_to_csv.ipynb",
-                      dag=dag)
+churn_to_csv = EMRSparkOperator(
+    task_id="churn_to_csv",
+    job_name="Generate Churn CSV files",
+    execution_timeout=timedelta(hours=4),
+    instance_count=1,
+    env={"date": "{{ ds_nodash }}"},
+    uri="https://raw.githubusercontent.com/mozilla/mozilla-reports/master/etl/churn_to_csv.kp/orig_src/churn_to_csv.ipynb",
+    dag=dag)
 
-t1.set_upstream(t0)
+churn_to_csv.set_upstream(churn)
