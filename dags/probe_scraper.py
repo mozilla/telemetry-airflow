18a80c24fbf54b1b6315961c2b670a51ea6dbe32
Alessio Placitelli <alessio.placitelli@gmail.com>
2018-01-18 15:48:21 +0100

    Add the probe scraper DAG

---
+++ dags/probe_scraper.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from datetime import timedelta, datetime
+from operators.emr_spark_operator import EMRSparkOperator
+
+default_args = {
+    'owner': 'gfritzsche@mozilla.com',
+    'depends_on_past': False,
+    'start_date': datetime(2017, 3, 26),
+    'email': ['telemetry-client-dev@mozilla.com', 'gfritzsche@mozilla.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=30),
+}
+
+dag_daily = DAG('probe_scraper',
+                default_args=default_args,
+                schedule_interval='@daily')
+
+probe_scraper = EMRSparkOperator(
+    task_id="probe_scraper",
+    job_name="Probe Scraper",
+    execution_timeout=timedelta(hours=4),
+    instance_count=1,
+    owner="gfritzsche@mozilla.com",
+    email=['telemetry-client-dev@mozilla.com', 'gfritzsche@mozilla.com', 'aplacitelli@mozilla.com'],
+    env={},
+    uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/probe_scraper.sh",
+    output_visibility="public",
+    dag=dag_daily)
---
+++ jobs/probe_scraper.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Print all the executed commands to the terminal.
+set -x
+
+BUCKET="telemetry-public-analysis-2"
+PREFIX="probe-scraper/data-rest/"
+CACHE_DIR="probe_cache"
+OUTPUT_DIR="probe_data"
+
+# Clone and setup the scraper
+git clone https://github.com/mozilla/probe-scraper.git
+cd probe-scraper
+python setup.py bdist_egg
+pip install -r requirements.txt
+
+# Create the output and cache directories.
+mkdir $CACHE_DIR $OUTPUT_DIR
+
+# Finally run the scraper.
+python probe_scraper/runner.py --outdir $OUTPUT_DIR --tempdir $CACHE_DIR
+
+# Recursively GZIP the files in the output dir. Each file
+# will be compressed separately, creating a 'file.gz' file.
+gzip -9 -r $OUTPUT_DIR
+
+# Drop the '.gz' extension from the files.
+find $OUTPUT_DIR -type f -name '*.gz' | while read f; do mv "$f" "${f%.gz}"; done
+
+# Upload to S3.
+aws s3 cp $OUTPUT_DIR s3://$BUCKET/$PREFIX \
+       --recursive \
+       --content-encoding 'gzip' \
+       --content-type 'application/json' \
+       --cache-control 'max-age=28800' \
+       --acl public-read
