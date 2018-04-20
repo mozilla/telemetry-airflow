40b296ece7831b7eb3d0e35f9a23bdb76252c536
Frank Bertsch <fbertsch@mozilla.com>
2018-04-20 15:55:42 -0500

    Use telemetry-artifacts for events_to_amplitude
    * Include new artifacts-bucket environment variable
    * Retrieve JAR from artifacts-bucket for events_to_amplitude
    * Allow different artifacts for dev, stage, and prod

--- jobs/focus_android_events_to_amplitude.sh
+++ jobs/focus_android_events_to_amplitude.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-if [[ -z "$date" || -z "$max_requests" || -z "$key_file" ]]; then
+if [[ -z "$date" || -z "$max_requests" || -z "$key_file" || -z "$artifact" ]]; then
     echo "Missing arguments!" 1>&2
     exit 1
 fi
 
-git clone https://github.com/mozilla/telemetry-streaming.git
-cd telemetry-streaming
-sbt assembly
+artifact_filename="artifact.jar"
+aws s3 cp "$artifact" "$artifact_filename"
 
 key_filename="amplitude_key_file"
 aws s3 cp "$key_file" "$key_filename"
@@ -15,8 +14,7 @@ export AMPLITUDE_API_KEY=$(cat "$key_filename")
 
 spark-submit --master yarn \
              --deploy-mode client \
-             --class com.mozilla.telemetry.streaming.EventsToAmplitude \
-             target/scala-2.11/telemetry-streaming-assembly-0.1-SNAPSHOT.jar \
+             --class com.mozilla.telemetry.streaming.EventsToAmplitude $artifact_filename \
              --config-file-path ./configs/focus_android_events_schemas.json \
              --url https://api.amplitude.com/httpapi \
              --from $date \
