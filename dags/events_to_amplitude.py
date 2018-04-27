1c3d1f253c79afdb9698889ac27dd187da9d36a0
Frank Bertsch <fbertsch@mozilla.com>
2018-04-27 10:22:48 -0500

    Make utility function for retrieving artifact URLs
    This also now takes the region from the environment

--- dags/events_to_amplitude.py
+++ dags/events_to_amplitude.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from operators.emr_spark_operator import EMRSparkOperator
 from utils.constants import DS_WEEKLY
 from utils.mozetl import mozetl_envvar
-from utils.tbv import tbv_envvar
+from utils.deploy import get_artifact_url
 
 FOCUS_ANDROID_INSTANCES = 10
 VCPUS_PER_INSTANCE = 16
@@ -11,12 +11,8 @@ VCPUS_PER_INSTANCE = 16
 environment = "{{ task.__class__.deploy_environment }}"
 key_file = "s3://telemetry-airflow/config/amplitude/{}/apiKey".format(environment)
 
-bucket = "{{ task.__class__.artifacts_bucket }}"
-mozilla_slug = "{{ test.__class__.mozilla_slug }}"
 slug = "{{ task.__class__.telemetry_streaming_slug }}"
-deploy_tag = "{{ task.__class__.deploy_tag }}"
-url = "https://s3-us-west-2.amazonaws.com/{bucket}/{mozilla_slug}/{slug}/{deploy_tag}/{slug}.jar".format(
-    bucket=bucket, mozilla_slug=mozilla_slug, slug=slug, deploy_tag=deploy_tag)
+url = get_artifact_url(slug)
 
 default_args = {
     'owner': 'frank@mozilla.com',
