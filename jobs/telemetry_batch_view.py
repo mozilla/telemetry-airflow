67e3fc298562cadfe80c79d566d5de24e8006f36
Frank Bertsch <fbertsch@mozilla.com>
2018-06-12 12:30:28 -0700

    Make longitudinal use TBV release process

--- jobs/telemetry_batch_view.py
+++ jobs/telemetry_batch_view.py
@@ -3,7 +3,7 @@
 import requests
 from os import chdir
 from os import environ
-from subprocess import call
+from subprocess import call, PIPE, Popen
 
 
 artifact_file = "artifact.jar"
@@ -43,8 +43,22 @@ def submit_job():
     call_exit_errors(command)
 
 
+def update_metastore(location, hive_server):
+    p2h_cmd = ("parquet2hive", "-ulv=1", "--sql", location)
+    beeline_cmd = ("beeline", "-u", "jdbc:hive2://{}:10000".format(hive_server))
+    print("+ {}".format(" ".join(p2h_cmd)))
+    p2h = Popen(p2h_cmd, stdout=PIPE)
+    print("+ {}".format(" ".join(beeline_cmd)))
+    rc = call(beeline_cmd, stdin=p2h.stdout)
+    if rc > 0:
+        exit(rc)
+
+
 if environ.get("DO_RETRIEVE", "True") == "True":
     retrieve_jar()
 
 if environ.get("DO_SUBMIT", "True") == "True":
     submit_job()
+
+if environ.get("METASTORE_LOCATION") != None:
+    update_metastore(environ.get("METASTORE_LOCATION"), environ.get("HIVE_SERVER"))
