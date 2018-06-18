295ff0b960b7f31d7d727506321a8a373e2a36bc
Frank Bertsch <fbertsch@mozilla.com>
2018-06-18 10:00:54 -0500

    Set executable bits explicitly

--- jobs/run_telemetry_aggregator.sh
+++ jobs/run_telemetry_aggregator.sh
@@ -7,4 +7,5 @@ package_file=$(pwd)/dist/$(ls dist)
 
 wget https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/telemetry_aggregator.py
 pip install -U py4j
+chmod 777 telemetry_aggregator.py
 ./telemetry_aggregator.py $package_file
