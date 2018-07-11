57cc3500ac3aec5e399985488ed37a9c04426adf
Jeff Klukas <jeff@klukas.net>
2018-07-11 14:02:56 -0400

    Bug 1474987 Register task for Savant events to Amplitude

--- dags/events_to_amplitude.py
+++ dags/events_to_amplitude.py
@@ -6,11 +6,11 @@ from utils.mozetl import mozetl_envvar
 from utils.deploy import get_artifact_url
 
 FOCUS_ANDROID_INSTANCES = 10
+SAVANT_INSTANCES = 10
 VCPUS_PER_INSTANCE = 16
 
 environment = "{{ task.__class__.deploy_environment }}"
 key_file = "s3://telemetry-airflow/config/amplitude/{}/apiKey".format(environment)
-config_file = "focus_android_events_schemas.json"
 
 slug = "{{ task.__class__.telemetry_streaming_slug }}"
 tag = "v1.0.1"
@@ -31,7 +31,7 @@ dag = DAG('events_to_amplitude', default_args=default_args, schedule_interval='0
 
 
 
-events_to_amplitude = EMRSparkOperator(
+focus_events_to_amplitude = EMRSparkOperator(
     task_id="focus_android_events_to_amplitude",
     job_name="Focus Android Events to Amplitude",
     execution_timeout=timedelta(hours=8),
@@ -41,7 +41,22 @@ events_to_amplitude = EMRSparkOperator(
         "max_requests": FOCUS_ANDROID_INSTANCES * VCPUS_PER_INSTANCE,
         "key_file": key_file,
         "artifact": url,
-        "config_filename": config_file
+        "config_filename": "focus_android_events_schemas.json",
+    },
+    uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/events_to_amplitude.sh",
+    dag=dag)
+
+savant_events_to_amplitude = EMRSparkOperator(
+    task_id="desktop_savant_events_to_amplitude",
+    job_name="Desktop Events for Savant Study to Amplitude",
+    execution_timeout=timedelta(hours=8),
+    instance_count=SAVANT_INSTANCES,
+    env={
+        "date": "{{ ds_nodash }}",
+        "max_requests": SAVANT_INSTANCES * VCPUS_PER_INSTANCE,
+        "key_file": key_file,
+        "artifact": url,
+        "config_filename": "desktop_savant_events_schemas.json",
     },
     uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/events_to_amplitude.sh",
     dag=dag)
