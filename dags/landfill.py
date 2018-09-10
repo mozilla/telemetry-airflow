788c5ace9c33757a011fdd5ee3c4baf6a7495e2d
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2018-09-10 13:00:22 -0700

    Bug 1490099 - Add landfill sampler

---
+++ dags/landfill.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.moz_databricks import MozDatabricksSubmitRunOperator
+from utils.mozetl import mozetl_envvar
+
+default_args = {
+    'owner': 'amiyaguchi@mozilla.com',
+    'depends_on_past': False,
+    'start_date': datetime(2018, 9, 10),
+    'email': ['telemetry-alerts@mozilla.com', 'amiyaguchi@mozilla.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=30),
+}
+
+dag = DAG('main_summary', default_args=default_args, schedule_interval='0 1 * * *')
+
+
+landfill_sampler = MozDatabricksSubmitRunOperator(
+    task_id="landfill_sampler",
+    job_name="Landfill Sampler",
+    execution_timeout=timedelta(hours=2),
+    instance_count=3,
+    env=mozetl_envvar("landfill_sampler", {
+        "submission-date": "{{ ds_nodash }}",
+        "bucket": "{{ task.__class__.private_output_bucket }}",
+        "prefix": "santitized-landfill-sample",
+    }),
+    uri="https://raw.githubusercontent.com/mozilla/python_mozetl/master/bin/mozetl-submit.sh",
+    dag=dag)
