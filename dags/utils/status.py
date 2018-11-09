0f22112bdf1b9c988e88917261a8c3518948195e
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2018-11-09 11:02:47 -0800

    Add a default on_success=False option to register_status

--- dags/utils/status.py
+++ dags/utils/status.py
@@ -1,24 +1,30 @@
 from airflow.operators.dataset_status import DatasetStatusOperator
 
 
-def register_status(operator, name, description):
+def register_status(operator, name, description, on_success=False):
     """Wrap an operator with an external status page.
     
-    :param operator: An Airflow operator to set upstream
-    :type airflow.models.BaseOperator:
+    The default behavior will only set the state of a dataset to partial_outage.
 
-    :returns: The original airflow operator with downstream dependenc
+    :param airflow.models.BaseOperator operator: An Airflow operator to set upstream
+    :param str name:            The name of the dataset
+    :param str description:     A short (1-2 sentence) description of the dataset.
+    :param boolean on_success:  Indicator to set the dataset state to operational on success
+
+    :returns: The original airflow operator with downstream dependencies
     """
 
     kwargs = {"name": name, "description": description, "dag": operator.dag}
 
-    # create and operator on the success case
-    success = DatasetStatusOperator(
-        trigger_rule="all_success",
-        task_id="{}_success".format(operator.task_id),
-        status="operational",
-        **kwargs
-    )
+    if on_success:
+        # create and operator on the success case
+        success = DatasetStatusOperator(
+            trigger_rule="all_success",
+            task_id="{}_success".format(operator.task_id),
+            status="operational",
+            **kwargs
+        )
+        operator >> success
 
     # create an operator on the failure case
     failure = DatasetStatusOperator(
@@ -28,7 +34,6 @@ def register_status(operator, name, description):
         **kwargs
     )
 
-    operator >> success
     operator >> failure
 
     return operator
--- tests/test_dag_utils_status.py
+++ tests/test_dag_utils_status.py
@@ -57,45 +57,23 @@ def mock_hook(mocker):
     return mocker.patch("plugins.statuspage.operator.DatasetStatusHook")
 
 
-def test_subdag_structure(dag, mock_hook):
+def test_subdag_structure_default(dag, mock_hook):
     operator = PythonOperator(task_id="test", python_callable=lambda: True, dag=dag)
     _ = register_status(operator, "test", "test description")
+    assert {ti.task_id for ti in dag.topological_sort()} == {
+        "test",
+        "test_failure",
+    }
+
+
+def test_subdag_structure_on_success_opt_in(dag, mock_hook):
+    operator = PythonOperator(task_id="test", python_callable=lambda: True, dag=dag)
+    _ = register_status(operator, "test", "test description", on_success=True)
     assert {ti.task_id for ti in dag.topological_sort()} == {
         "test",
         "test_success",
         "test_failure",
     }
-
-
-def test_execute_success(dag, mock_hook):
-    testing_component_id = 42
-
-    mock_conn = mock_hook.return_value.get_conn()
-    mock_conn.get_or_create.return_value = testing_component_id
-
-    success_op = PythonOperator(task_id="test", python_callable=lambda: True, dag=dag)
-    _ = register_status(success_op, "Test Success", "Testing operational status")
-
-    # run each of the operators in order
-    for operator in dag.topological_sort():
-        operator.run(start_date=DEFAULT_DATE, end_date=DEFAULT_DATE)
-
-    # get the dag state
-    dagrun = dag.create_dagrun(
-        run_id="manual__",
-        start_date=datetime.utcnow(),
-        execution_date=DEFAULT_DATE,
-        state=State.RUNNING,
-    )
-
-    expected = {
-        "test": State.SUCCESS,
-        "test_success": State.SUCCESS,
-        "test_failure": State.NONE,
-    }
-
-    assert all([expected[ti.task_id] == ti.state for ti in dagrun.get_task_instances()])
-    mock_conn.update.assert_called_once_with(testing_component_id, "operational")
 
 
 def test_execute_failure(dag, mock_hook):
@@ -161,3 +139,34 @@ def test_no_execution_on_retry(dag, mock_hook):
 
     assert all([expected[ti.task_id] == ti.state for ti in dagrun.get_task_instances()])
     assert not mock_hook.return_value.get_conn().update.called
+
+
+def test_execute_success_opt_in(dag, mock_hook):
+    testing_component_id = 42
+
+    mock_conn = mock_hook.return_value.get_conn()
+    mock_conn.get_or_create.return_value = testing_component_id
+
+    success_op = PythonOperator(task_id="test", python_callable=lambda: True, dag=dag)
+    _ = register_status(success_op, "Test Success", "Testing operational status", on_success=True)
+
+    # run each of the operators in order
+    for operator in dag.topological_sort():
+        operator.run(start_date=DEFAULT_DATE, end_date=DEFAULT_DATE)
+
+    # get the dag state
+    dagrun = dag.create_dagrun(
+        run_id="manual__",
+        start_date=datetime.utcnow(),
+        execution_date=DEFAULT_DATE,
+        state=State.RUNNING,
+    )
+
+    expected = {
+        "test": State.SUCCESS,
+        "test_success": State.SUCCESS,
+        "test_failure": State.NONE,
+    }
+
+    assert all([expected[ti.task_id] == ti.state for ti in dagrun.get_task_instances()])
+    mock_conn.update.assert_called_once_with(testing_component_id, "operational")
