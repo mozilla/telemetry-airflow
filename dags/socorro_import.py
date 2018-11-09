b12ffe3c84be55df3f55954875e65631c38fad8a
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2018-11-09 13:36:58 -0800

    Bug 1506252 - Register socorro crash reports with Statuspage

--- dags/socorro_import.py
+++ dags/socorro_import.py
@@ -1,26 +1,36 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from operators.emr_spark_operator import EMRSparkOperator
+from utils.status import register_status
 
 default_args = {
-    'owner': 'amiyaguchi@mozilla.com',
-    'depends_on_past': False,
-    'start_date': datetime(2016, 11, 11),
-    'email': ['telemetry-alerts@mozilla.com', 'amiyaguchi@mozilla.com'],
-    'email_on_failure': True,
-    'email_on_retry': True,
-    'retries': 2,
-    'retry_delay': timedelta(minutes=30),
+    "owner": "amiyaguchi@mozilla.com",
+    "depends_on_past": False,
+    "start_date": datetime(2016, 11, 11),
+    "email": ["telemetry-alerts@mozilla.com", "amiyaguchi@mozilla.com"],
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=30),
 }
 
-dag = DAG('socorro_import', default_args=default_args, schedule_interval='@daily')
+dag = DAG("socorro_import", default_args=default_args, schedule_interval="@daily")
 
-socorro_import = EMRSparkOperator(
-    task_id="socorro_import",
-    job_name="Import crash data",
+# input: crashstats-telemetry-crashes-prod-us-west-2/v1/crash_report
+# output: telemetry-parquet/socorro_crash/v2
+crash_report_parquet = EMRSparkOperator(
+    task_id="crash_report_parquet",
+    job_name="Socorro Crash Reports Parquet",
     execution_timeout=timedelta(hours=4),
     instance_count=10,
     env={"date": "{{ ds_nodash }}"},
     uri="https://raw.githubusercontent.com/mozilla-services/data-pipeline/master/reports/socorro_import/ImportCrashData.ipynb",
     output_visibility="public",
-    dag=dag)
+    dag=dag,
+)
+
+register_status(
+    crash_report_parquet,
+    "Socorro Crash Reports Parquet",
+    "Convert processed crash reports into parquet for analysis",
+)
