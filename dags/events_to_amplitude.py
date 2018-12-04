adc661157dcdba59b3a6aee0adab367bb6a07e18
Sunah Suh <ssuh@mozilla.com>
2018-12-04 16:52:23 -0600

    Use tbv_envvar for devtools release e2a job

--- dags/events_to_amplitude.py
+++ dags/events_to_amplitude.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from datetime import datetime, timedelta
 from operators.emr_spark_operator import EMRSparkOperator
 from utils.constants import DS_WEEKLY
-from utils.mozetl import mozetl_envvar
+from utils.tbv import tbv_envvar
 from utils.deploy import get_artifact_url
 
 FOCUS_ANDROID_INSTANCES = 10
@@ -15,6 +15,12 @@ environment = "{{ task.__class__.deploy_environment }}"
 def key_file(project):
     return (
         "s3://telemetry-airflow/config/amplitude/{}/{}/apiKey"
+        .format(environment, project)
+    )
+
+def key_path(project):
+    return (
+        "config/amplitude/{}/{}/apiKey"
         .format(environment, project)
     )
 
@@ -56,13 +62,13 @@ devtools_prerelease_events_to_amplitude = EMRSparkOperator(
     execution_timeout=timedelta(hours=8),
     instance_count=DEVTOOLS_INSTANCES,
     email=['ssuh@mozilla.com', 'telemetry-alerts@mozilla.com'],
+    owner='ssuh@mozilla.com',
     env={
         "date": "{{ ds_nodash }}",
         "max_requests": DEVTOOLS_INSTANCES * VCPUS_PER_INSTANCE,
         "key_file": key_file("devtools"),
         "artifact": get_artifact_url(slug, branch="master"),
         "config_filename": "devtools_prerelease_schemas.json",
-        "owner": "ssuh@mozilla.com"
     },
     uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/events_to_amplitude.sh",
     dag=dag)
@@ -84,3 +90,33 @@ rocket_android_events_to_amplitude = EMRSparkOperator(
     uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/events_to_amplitude.sh",
     dag=dag
 )
+
+devtools_release_events_to_amplitude = EMRSparkOperator(
+    task_id="devtools_release_events_to_amplitude",
+    job_name="DevTools Release Events to Amplitude",
+    execution_timeout=timedelta(hours=8),
+    instance_count=DEVTOOLS_INSTANCES,
+    dev_instance_count=DEVTOOLS_INSTANCES,
+    email=['ssuh@mozilla.com', 'telemetry-alerts@mozilla.com'],
+    owner='ssuh@mozilla.com',
+    env=tbv_envvar(
+        "com.mozilla.telemetry.streaming.EventsToAmplitude",
+        {
+            "from": "{{ ds_nodash }}",
+            "to": "{{ ds_nodash }}",
+            "max_parallel_requests": str(DEVTOOLS_INSTANCES * VCPUS_PER_INSTANCE),
+            "config_file_path": "devtools_release_schemas.json",
+            "url": "https://api.amplitude.com/httpapi",
+            "sample": "0.5",
+            "partition_multiplier": "5"
+        },
+        artifact_url=get_artifact_url(slug, branch="remove-main-pings"),
+        other={
+            "KEY_BUCKET": "telemetry-airflow",
+            "KEY_PATH": key_path("devtools"),
+            "DO_EVENTS_TO_AMPLITUDE_SETUP": "True"
+        }
+    ),
+    uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/telemetry_batch_view.py",
+    start_date=datetime(2018, 12, 4),
+    dag=dag)
