c9f256789230bc25c4c029f525861a7c5dc3c3fd
Frank Bertsch <fbertsch@mozilla.com>
2018-12-19 07:51:22 -0600

    Move main summary to databricks

--- dags/main_summary.py
+++ dags/main_summary.py
@@ -49,24 +49,30 @@ main_summary_all_histograms = MozDatabricksSubmitRunOperator(
         }),
     dag=dag)
 
-main_summary = EMRSparkOperator(
+main_summary = MozDatabricksSubmitRunOperator(
     task_id="main_summary",
     job_name="Main Summary View",
     execution_timeout=timedelta(hours=9),
     email=["telemetry-alerts@mozilla.com", "frank@mozilla.com", "main_summary_dataset@moz-svc-ops.pagerduty.com"],
-    instance_count=40,
+    instance_count=5,
+    max_instance_count=40,
+    enable_autoscale=True,
+    instance_type="c4.4xlarge",
+    spot_bid_price_percent=50,
+    ebs_volume_count=1,
+    ebs_volume_size=250,
     env=tbv_envvar("com.mozilla.telemetry.views.MainSummaryView",
         options={
             "from": "{{ ds_nodash }}",
             "to": "{{ ds_nodash }}",
             "schema-report-location": "s3://{{ task.__class__.private_output_bucket }}/schema/main_summary/submission_date_s3={{ ds_nodash }}",
-            "bucket": "{{ task.__class__.private_output_bucket }}"
+            "bucket": "{{ task.__class__.private_output_bucket }}",
+            "read-mode": "aligned",
+            "input-partition-multiplier": "400"
         },
         dev_options={
             "channel": "nightly",   # run on smaller nightly data rather than release
-            "read-mode": "aligned", # more efficient RDD splitting for small datasets
         }),
-    uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/telemetry_batch_view.py",
     dag=dag)
 
 register_status(main_summary, "Main Summary", "A summary view of main pings.")
