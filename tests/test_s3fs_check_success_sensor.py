da439cf4f8fa9ff487f1721f4e837722bdbe4fc1
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-01-03 14:48:17 -0800

    Add test showing default behavior of retries breaks sensors

--- tests/test_s3fs_check_success_sensor.py
+++ tests/test_s3fs_check_success_sensor.py
@@ -17,13 +17,6 @@ from plugins.s3fs_check_success import S3FSCheckSuccessSensor
 DEFAULT_DATE = datetime(2019, 1, 1)
 
 
-@pytest.fixture
-def dag():
-    return DAG(
-        "test_dag", default_args={"owner": "airflow", "start_date": DEFAULT_DATE}
-    )
-
-
 @mock_s3
 def test_single_partition_contains_success():
     bucket = "test"
@@ -54,7 +47,11 @@ def test_single_partition_not_contains_success():
 
 
 @mock_s3
-def test_single_partition_with_templates(dag):
+def test_single_partition_with_templates():
+    """Run the entire DAG, instead of just the operator. This is necessary to
+    instantiate the templating functionality, which requires context from the
+    scheduler."""
+
     bucket = "test"
     prefix = "dataset/v1/submission_date=20190101"
 
@@ -62,6 +59,8 @@ def test_single_partition_with_templates(dag):
     client.create_bucket(Bucket=bucket)
     client.put_object(Bucket=bucket, Body="", Key=prefix + "/part=1/_SUCCESS")
     client.put_object(Bucket=bucket, Body="", Key=prefix + "/part=2/garbage")
+
+    dag = DAG("test_dag", default_args={"owner": "airflow", "start_date": DEFAULT_DATE})
 
     sensor_success = S3FSCheckSuccessSensor(
         task_id="test_success_template",
@@ -137,3 +136,69 @@ def test_partitions_contain_partial_success():
         task_id="test_failure", bucket=bucket, prefix=prefix, num_partitions=3
     )
     assert not sensor.poke(None)
+
+
+@mock_s3
+def test_dag_retry_limit_causes_premature_failure():
+    """Verify that that setting a retry limit on the DAG breaks the sensor functionality.
+    We want to make sure that the sensors are allowed to retry until they timeout."""
+
+    bucket = "test"
+    prefix = "dataset/v1/submission_date=20190101"
+
+    client = boto3.client("s3")
+    client.create_bucket(Bucket=bucket)
+
+    dag = DAG(
+        "test_dag",
+        default_args={
+            "owner": "airflow",
+            "start_date": DEFAULT_DATE,
+            "retries": 1,
+            "retry_delay": timedelta(seconds=1),
+        },
+    )
+
+    sensor_retry = S3FSCheckSuccessSensor(
+        task_id="test_retry_template",
+        bucket=bucket,
+        prefix="dataset/v1/submission_date={{ ds_nodash }}",
+        num_partitions=1,
+        poke_interval=1,
+        timeout=2,
+        dag=dag,
+    )
+    sensor_failure = S3FSCheckSuccessSensor(
+        task_id="test_failure_template",
+        bucket=bucket,
+        prefix="dataset/v1/submission_date={{ ds_nodash }}",
+        num_partitions=1,
+        poke_interval=1,
+        timeout=2,
+        retries=0,  # disable retries for the correct behavior
+        dag=dag,
+    )
+
+    # execute everything for templating to work
+    with pytest.raises(AirflowSensorTimeout):
+        sensor_retry.run(start_date=DEFAULT_DATE, end_date=DEFAULT_DATE)
+    with pytest.raises(AirflowSensorTimeout):
+        sensor_failure.run(start_date=DEFAULT_DATE, end_date=DEFAULT_DATE)
+
+    session = Session()
+    tis = session.query(TaskInstance).filter(
+        TaskInstance.dag_id == dag.dag_id, TaskInstance.execution_date == DEFAULT_DATE
+    )
+    session.close()
+
+    count = 0
+    for ti in tis:
+        if ti.task_id == "test_retry_template":
+            assert ti.state == State.UP_FOR_RETRY
+        elif ti.task_id == "test_failure_template":
+            assert ti.state == State.FAILED
+        else:
+            print(ti.task_id, ti.state)
+            assert False
+        count += 1
+    assert count == 2
