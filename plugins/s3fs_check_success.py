672983c2e173d915285e33e26aed1bbff3d439e2
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2019-01-14 11:04:59 -0800

    Add an operator to check for _SUCCESS files (#406)
    * Add a S3FSCheckSuccessOperator and fix imports
    
    * Remove py36 from envlist due to snakebite import in sensors
    
    * Fix #404 - Set the number of expected partitions as a lower bound

--- plugins/s3fs_check_success.py
+++ plugins/s3fs_check_success.py
@@ -2,7 +2,7 @@ import logging
 
 import boto3
 from airflow.exceptions import AirflowException
-from airflow.operators import BaseSensorOperator
+from airflow.operators import BaseOperator, BaseSensorOperator
 from airflow.plugins_manager import AirflowPlugin
 
 
@@ -15,8 +15,27 @@ def check_s3fs_success(bucket, prefix, num_partitions):
     s3 = boto3.resource("s3")
     objects = s3.Bucket(bucket).objects.filter(Prefix=prefix)
     success = set([obj.key for obj in objects if "_SUCCESS" in obj.key])
+    logging.info("Found {n_success} files".format(n_success=len(success)))
+    return len(success) >= num_partitions
 
-    return len(success) == num_partitions
+
+class S3FSCheckSuccessOperator(BaseOperator):
+    template_fields = ("prefix",)
+
+    def __init__(self, bucket, prefix, num_partitions, *args, **kwargs):
+        self.bucket = bucket
+        self.prefix = prefix
+        self.num_partitions = num_partitions
+        super(S3FSCheckSuccessOperator, self).__init__(*args, **kwargs)
+
+    def execute(self, context):
+        logging.info(
+            "Running check against s3:/{}/{} with {} partitions".format(
+                self.bucket, self.prefix, self.num_partitions
+            )
+        )
+        if not check_s3fs_success(self.bucket, self.prefix, self.num_partitions):
+            raise ValueError("Wrong number of _SUCCESS files")
 
 
 class S3FSCheckSuccessSensor(BaseSensorOperator):
@@ -39,4 +58,5 @@ class S3FSCheckSuccessSensor(BaseSensorOperator):
 
 class S3FSCheckSuccessPlugin(AirflowPlugin):
     name = "s3fs_check_success"
-    operators = [S3FSCheckSuccessSensor]
+    operators = [S3FSCheckSuccessOperator]
+    sensors = [S3FSCheckSuccessSensor]
