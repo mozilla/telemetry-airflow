9379164c9afdda7be5eba5bf324726e8d5bc1114
William Lachance <wlachance@mozilla.com>
2019-02-08 13:40:48 -0500

    Downgrade pandas to 0.18.1 for tab spinner job only

--- jobs/run-severity-generator.sh
+++ jobs/run-severity-generator.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#!/bin/bash
+
+# This script does not use mozetl-submit, as we need to do some
+# hacky downgrading to get it to work, see:
+# https://bugzilla.mozilla.org/show_bug.cgi?id=1525022
 
 # We use jupyter by default, but here we want to use python
 unset PYSPARK_DRIVER_PYTHON
@@ -8,6 +11,7 @@ unset PYSPARK_DRIVER_PYTHON
 git clone https://github.com/mozilla/python_mozetl.git
 cd python_mozetl
 pip install .
+pip install pandas==0.18.1 # DOWNGRADE pandas to the one installed in EMR
 python setup.py bdist_egg
 
 spark-submit scheduling/tab_spinner.py
