b62f5081121b145e3ced1f4b22471c953690ff36
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2019-02-22 12:50:20 -0800

    Add python3 support to moz_databricks and set it to default (#432)
    * Add python3 support to Databricks clusters
    
    * Set default python version to 3
    
    * Update moz_databricks test pattern to include json payload
    
    * Refactor mock_hook into a fixture
    
    * Add test asserting value of `PYSPARK_VERSION`
    
    * Fix environment variable name to PYSPARK_PYTHON
    
    * Address review by improving error handling and comments

--- tests/test_moz_databricks_operator.py
+++ tests/test_moz_databricks_operator.py
@@ -11,16 +11,22 @@ from plugins.moz_databricks import MozDatabricksSubmitRunOperator
 # so the variables are defined in `tox.ini` instead.
 
 
-def test_missing_tbv_or_mozetl_env(mocker):
+@pytest.fixture()
+def mock_hook(mocker):
     mock_hook = mocker.patch("plugins.databricks.databricks_operator.DatabricksHook")
+    mock_hook_instance = mock_hook.return_value
+    mock_hook_instance.submit_run.return_value = 1
+    return mock_hook_instance
+
+
+def test_missing_tbv_or_mozetl_env(mock_hook):
     with pytest.raises(ValueError):
         MozDatabricksSubmitRunOperator(
             job_name="test_databricks", env={}, instance_count=1
         )
 
 
-def test_mozetl_success(mocker):
-    mock_hook = mocker.patch("plugins.databricks.databricks_operator.DatabricksHook")
+def test_mozetl_success(mock_hook):
     operator = MozDatabricksSubmitRunOperator(
         task_id="test_databricks",
         job_name="test_databricks",
@@ -28,11 +34,16 @@ def test_mozetl_success(mocker):
         instance_count=1,
     )
     operator.execute(None)
-    assert mock_hook.called
+    mock_hook.submit_run.assert_called_once()
+
+    # https://docs.python.org/3.3/library/unittest.mock.html#unittest.mock.Mock.call_args
+    # call_args is a tuple where the first element is a list of elements. The first element
+    # in `submit_run` is the constructed json blob.
+    json = mock_hook.submit_run.call_args[0][0]
+    assert json.get("spark_python_task") is not None
 
 
-def test_tbv_success(mocker):
-    mock_hook = mocker.patch("plugins.databricks.databricks_operator.DatabricksHook")
+def test_tbv_success(mock_hook):
     operator = MozDatabricksSubmitRunOperator(
         task_id="test_databricks",
         job_name="test_databricks",
@@ -40,4 +51,47 @@ def test_tbv_success(mocker):
         instance_count=1,
     )
     operator.execute(None)
-    assert mock_hook.called
+    mock_hook.submit_run.assert_called_once()
+
+    json = mock_hook.submit_run.call_args[0][0]
+    assert json.get("spark_jar_task") is not None
+
+
+def test_default_python_version(mock_hook):
+    # run with default
+    operator = MozDatabricksSubmitRunOperator(
+        task_id="test_databricks",
+        job_name="test_databricks",
+        env={"MOZETL_COMMAND": "test"},
+        instance_count=1,
+    )
+    operator.execute(None)
+    mock_hook.submit_run.assert_called_once()
+
+    json = mock_hook.submit_run.call_args[0][0]
+    assert (
+        json["new_cluster"]["spark_env_vars"]["PYSPARK_PYTHON"]
+        == "/databricks/python3/bin/python3"
+    )
+
+    # run with python 2 specifically
+    operator = MozDatabricksSubmitRunOperator(
+        task_id="test_databricks",
+        job_name="test_databricks",
+        env={"MOZETL_COMMAND": "test"},
+        python_version=2,
+        instance_count=1,
+    )
+    operator.execute(None)
+
+    json = mock_hook.submit_run.call_args[0][0]
+    assert json["new_cluster"]["spark_env_vars"].get("PYSPARK_PYTHON") is None
+
+    with pytest.raises(ValueError):
+        MozDatabricksSubmitRunOperator(
+            task_id="test_databricks",
+            job_name="test_databricks",
+            env={"MOZETL_COMMAND": "test"},
+            python_version=4,
+            instance_count=1,
+        ).execute(None)
