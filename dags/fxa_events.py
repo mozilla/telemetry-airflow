c837e456b5336fd6c1d4f15f7469fc2e0d9ebd3c
Jason Thomas <jason@lithiumfox.com>
2019-04-15 15:50:46 -0400

    Update email address for fxa_events dag

--- dags/fxa_events.py
+++ dags/fxa_events.py
@@ -5,7 +5,7 @@ from airflow.contrib.operators.bigquery_operator import BigQueryOperator
 
 default_args = {
     'start_date': datetime.datetime(2019, 3, 1),
-    'email': ['telemetry-alerts@mozilla.com', 'mreid@mozilla.com'],
+    'email': ['telemetry-alerts@mozilla.com', 'jklukas@mozilla.com'],
     'email_on_failure': True,
     'email_on_retry': True,
     'depends_on_past': True,
