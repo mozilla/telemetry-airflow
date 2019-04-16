755420abd7cceacfaee522501567e4d6d938dcae
Jason Thomas <jason@lithiumfox.com>
2019-04-16 17:16:05 -0400

    Revert "Add load_to_bigquery tasks to sync_view dags"
    This reverts commit c40132919d5bea564335fd6d14cf7e8a54b8a7d2.

--- dags/sync_view.py
+++ dags/sync_view.py
@@ -1,9 +1,7 @@
 from airflow import DAG
-from airflow.operators.subdag_operator import SubDagOperator
 from datetime import datetime, timedelta
 from operators.emr_spark_operator import EMRSparkOperator
 from utils.mozetl import mozetl_envvar
-from utils.gcp import load_to_bigquery
 from utils.tbv import tbv_envvar
 
 
@@ -32,19 +30,6 @@ sync_view = EMRSparkOperator(
     uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/telemetry_batch_view.py",
     dag=dag)
 
-sync_view_bigquery_load = SubDagOperator(
-    subdag=load_to_bigquery(
-        parent_dag_name="sync_view",
-        default_args=default_args,
-        dataset_s3_bucket="{{ task.__class__.private_output_bucket }}",
-        aws_conn_id="aws_dev_iam_s3",
-        dataset="sync_summary",
-        dataset_version="v2",
-        gke_cluster_name="bq-load-gke-1",
-        ),
-    task_id="sync_view_bigquery_load",
-    dag=dag)
-
 sync_events_view = EMRSparkOperator(
     task_id="sync_events_view",
     job_name="Sync Events View",
@@ -58,19 +43,6 @@ sync_events_view = EMRSparkOperator(
     uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/telemetry_batch_view.py",
     dag=dag)
 
-sync_events_view_bigquery_load = SubDagOperator(
-    subdag=load_to_bigquery(
-        parent_dag_name="sync_events_view",
-        default_args=default_args,
-        dataset_s3_bucket="{{ task.__class__.private_output_bucket }}",
-        aws_conn_id="aws_dev_iam_s3",
-        dataset="sync_events",
-        dataset_version="v1",
-        gke_cluster_name="bq-load-gke-1",
-        ),
-    task_id="sync_events_view_bigquery_load",
-    dag=dag)
-
 sync_flat_view = EMRSparkOperator(
     task_id="sync_flat_view",
     job_name="Flattened Sync Pings View",
@@ -81,19 +53,6 @@ sync_flat_view = EMRSparkOperator(
         "to": "{{ ds_nodash }}",
         "bucket": "{{ task.__class__.private_output_bucket }}"}),
     uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/telemetry_batch_view.py",
-    dag=dag)
-
-sync_flat_view_bigquery_load = SubDagOperator(
-    subdag=load_to_bigquery(
-        parent_dag_name="sync_flat_view",
-        default_args=default_args,
-        dataset_s3_bucket="{{ task.__class__.private_output_bucket }}",
-        aws_conn_id="aws_dev_iam_s3",
-        dataset="sync_flat_summary",
-        dataset_version="v1",
-        gke_cluster_name="bq-load-gke-1",
-        ),
-    task_id="sync_flat_view_bigquery_load",
     dag=dag)
 
 sync_bookmark_validation = EMRSparkOperator(
@@ -111,9 +70,3 @@ sync_bookmark_validation = EMRSparkOperator(
 
 
 sync_bookmark_validation.set_upstream(sync_view)
-
-sync_view_bigquery_load.set_upstream(sync_view)
-
-sync_events_view_bigquery_load.set_upstream(sync_events_view)
-
-sync_flat_view_bigquery_load.set_upstream(sync_flat_view)
