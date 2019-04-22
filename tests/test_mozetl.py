a06b27038aeedd95a8ba3025979be0e727e5e3ab
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-04-22 15:19:28 -0700

    Add missing changes to test_mozetl

--- tests/test_mozetl.py
+++ tests/test_mozetl.py
@@ -1,11 +1,12 @@
+import boto3
 import pytest
 from moto import mock_s3
 from plugins.mozetl import generate_runner
 
 
 @mock_s3
-def test_generate_runner():
-    module_name = "mozetl"
+@pytest.mark.parametrize("module_name", ["mozetl", "custom"])
+def test_generate_runner(module_name):
     bucket = "test-bucket"
     prefix = "test-prefix"
     conn = boto3.resource("s3")
@@ -14,9 +15,9 @@ def test_generate_runner():
     generate_runner(module_name, bucket, prefix)
 
     body = (
-        conn.Object(bucket, "{}/{}_runner.py".format())
+        conn.Object(bucket, "{}/{}_runner.py".format(prefix, module_name))
         .get()["Body"]
         .read()
         .decode("utf-8")
     )
-    assert body.split("\n")[2] == "from mozetl import cli"
+    assert body.split("\n")[3] == "from {} import cli".format(module_name)
