02ab6b7d489e7de545913c46938a5c85fb7e07db
Brian Wright <40579480+brianwright2@users.noreply.github.com>
2019-06-24 11:10:20 -0700

    adding addons_daily jobs (#533)
    * adding addons_daily jobs
    
    * updating start date
    
    * Delete addons_daily.sh
    
    We no longer need this file.
    
    * Update addons_daily.py
    
    This changes the job to fit into the `MozDatabricksSubmitRunOperator` framework based on conversations with relud.
    
    * Update addons_daily.py

---
+++ dags/addons_daily.py
@@ -0,0 +1,62 @@
+from airflow import DAG
+from airflow.operators.sensors import ExternalTaskSensor
+from datetime import datetime, timedelta
+from airflow.operators.moz_databricks import MozDatabricksSubmitRunOperator
+from utils.mozetl import mozetl_envvar
+
+default_args = {
+    "owner": "bmiroglio@mozilla.com",
+    "depends_on_past": True,
+    "start_date": datetime(2019, 6, 10),
+    "email": [
+        "telemetry-alerts@mozilla.com",
+        "bmiroglio@mozilla.com",
+        "smelancon@mozilla.com",
+        "bwright@mozilla.com",
+        "dthorn@mozilla.com",
+    ],
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=30),
+}
+
+dag = DAG("addons_daily", default_args=default_args, schedule_interval="@daily")
+
+# most downstream dependency is search_clients_daily
+wait_for_search_clients_daily = ExternalTaskSensor(
+    task_id="wait_for_search_clients_daily",
+    external_dag_id="main_summary",
+    external_task_id="search_clients_daily",
+    execution_delta=timedelta(hours=-1),
+    dag=dag,
+)
+
+addons_daily = MozDatabricksSubmitRunOperator(
+    task_id="addons_daily",
+    job_name="Addons Daily",
+    execution_timeout=timedelta(hours=4),
+    instance_count=10,
+    owner="bmiroglio@mozilla.com",
+    email=[
+        "telemetry-alerts@mozilla.com",
+        "bmiroglio@mozilla.com",
+        "smelancon@mozilla.com",
+        "bwright@mozilla.com",
+        "dthorn@mozilla.com",
+    ],
+    env=mozetl_envvar(
+        "addons_report",
+        {
+            "date": "{{ ds_nodash }}",
+            "deploy_environment": "{{ task.__class__.deploy_environment }}",
+        },
+        other={
+            "MOZETL_GIT_PATH": "https://github.com/mozilla/addons_daily.git",
+            "MOZETL_EXTERNAL_MODULE": "addons_daily",
+        },
+    ),
+    dag=dag,
+)
+
+addons_daily.set_upstream(wait_for_search_clients_daily)
