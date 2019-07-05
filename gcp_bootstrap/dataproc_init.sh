8148adcb0677971e3b05175c538f6971f9e1016b
Harold Woo <hwoo@mozilla.com>
2019-07-05 15:16:15 -0700

    [Bug 1563847] Add dataproc functionality to airflow

---
+++ gcp_bootstrap/dataproc_init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# To be copied to gs://moz-fx-data-prod-airflow-artifacts
+
+set -exo pipefail
+
+# Logs will be available on the dataproc nodes at /var/log/dataproc-initialization-script-X.log
+# or via the GCP Dataproc UI
+
+ARTIFACTS_BUCKET=gs://moz-fx-data-prod-airflow-artifacts
+
+ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
+if [[ "${ROLE}" == 'Master' ]]; then
+  # You can put any master-specific logic here
+  echo "Running dataproc_init.sh on master..."
+fi
+
+gsutil cp $ARTIFACTS_BUCKET/jars/* /usr/lib/spark/jars/
+
+# Install spark packages
+# See https://github.com/mozilla/telemetry-spark-packages-assembly
+TSPA_VERSION=v1.0.0
+TSPA_GS_PATH=$ARTIFACTS_BUCKET/mozilla/telemetry-spark-packages-assembly/$TSPA_VERSION/telemetry-spark-packages-assembly.jar
+TSPA_JAR=/usr/lib/spark/jars/telemetry-spark-packages-assembly.jar
+gsutil cp $TSPA_GS_PATH $TSPA_JAR
+
+# Install python packages
+PIP_REQUIREMENTS_FILE=/tmp/requirements.txt
+gsutil cp $ARTIFACTS_BUCKET/bootstrap/python-requirements.txt $PIP_REQUIREMENTS_FILE
+/opt/conda/default/bin/pip install --upgrade pip
+/opt/conda/default/bin/pip install -r $PIP_REQUIREMENTS_FILE
