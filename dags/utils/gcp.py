mozilla/telemetry-airflow

commit b903eeb12517e22a4a488037b9bd2f0f374177af

Add options for controlling bigquery-etl project, dataset, and sql path (#569)
* Add options for controlling bigquery-etl project, dataset, and sql path

* make dataset_id required

dags/utils/gcp.py
@@ -10,6 +10,8 @@ from operators.gcp_container_operator import GKEPodOperator
 from airflow.contrib.operators.bigquery_table_delete_operator import BigQueryTableDeleteOperator # noqa
 from airflow.contrib.operators.s3_to_gcs_transfer_operator import S3ToGoogleCloudStorageTransferOperator # noqa
 
+import re
+
 
 def load_to_bigquery(parent_dag_name=None,
                      default_args=None,
@@ -383,8 +385,11 @@ def export_to_parquet(
 
 def bigquery_etl_query(
     destination_table,
+    dataset_id,
     parameters=(),
     arguments=(),
+    project_id=None,
+    sql_file_path=None,
     gcp_conn_id="google_cloud_derived_datasets",
     gke_location="us-central1-a",
     gke_cluster_name="bq-load-gke-1",
@@ -397,8 +402,12 @@ def bigquery_etl_query(
     """ Generate.
 
     :param str destination_table:                  [Required] BigQuery destination table
+    :param str dataset_id:                         [Required] BigQuery default dataset id
     :param Tuple[str] parameters:                  Parameters passed to bq query
     :param Tuple[str] arguments:                   Additional bq query arguments
+    :param Optional[str] project_id:               BigQuery default project id
+    :param Optional[str] sql_file_path:            Optional override for path to the
+                                                   SQL query file to run
     :param str gcp_conn_id:                        Airflow connection id for GCP access
     :param str gke_location:                       GKE cluster location
     :param str gke_cluster_name:                   GKE cluster name
@@ -417,7 +426,6 @@ def bigquery_etl_query(
     """
     kwargs["task_id"] = kwargs.get("task_id", destination_table)
     kwargs["name"] = kwargs.get("name", kwargs["task_id"].replace("_", "-"))
-    sql_file_path = "sql/{}.sql".format(destination_table)
     if date_partition_parameter is not None:
         destination_table = destination_table + "${{ds_nodash}}"
         parameters += (date_partition_parameter + ":DATE:{{ds}}",)
@@ -430,9 +438,11 @@ def bigquery_etl_query(
         image=docker_image,
         arguments=["query"]
         + ["--destination_table=" + destination_table]
+        + ["--dataset_id=" + dataset_id]
+        + (["--project_id=" + project_id] if project_id else [])
         + ["--parameter=" + parameter for parameter in parameters]
         + list(arguments)
-        + [sql_file_path],
+        + [sql_file_path or "sql/{}/{}.sql".format(dataset_id, destination_table)],
         image_pull_policy=image_pull_policy,
         **kwargs
     )

dags/bq_events_to_amplitude.py
@@ -40,6 +40,7 @@ with models.DAG(
     table_name = 'fenix_events_amplitude_v1'
     table_creation = bigquery_etl_query(
         destination_table=table_name,
+        dataset_id='telemetry',
         date_partition_parameter=None,
         parameters=('submission_date' + ':DATE:{{ ds }}',)
     )
@@ -53,7 +54,7 @@ with models.DAG(
     # Export from bq to gcs
     # Docs: https://github.com/apache/airflow/blob/master/airflow/contrib/operators/bigquery_to_gcs.py#L28
     table_extract = BigQueryToCloudStorageOperator(
-        task_id='bq_to_gcs', 
+        task_id='bq_to_gcs',
         source_project_dataset_table='{project}.telemetry.{table}'.format(project=project_id, table=table_name),
         destination_cloud_storage_uris=['gs://{bucket}/{dir}*{ext}'.format(bucket=gcs_bucket, dir=directory, ext=extension)],
         bigquery_conn_id=gcp_conn_id,

dags/crash_summary.py
@@ -48,7 +48,7 @@ crash_summary_view_bigquery_load = SubDagOperator(
         dataset_version="v2",
         date_submission_col="submission_date",
         gke_cluster_name="bq-load-gke-1",
-        bigquery_dataset="telemetry_raw",
+        bigquery_dataset="telemetry_derived",
         ),
     task_id="crash_summary_view_bigquery_load",
     dag=dag)

dags/direct2parquet_bigquery_load.py
@@ -78,7 +78,7 @@ with DAG(
             'aws_conn_id': 'aws_prod_iam_s3',
             'dataset': dataset,
             'gke_cluster_name': 'bq-load-gke-1',
-            'bigquery_dataset': 'telemetry_raw',
+            'bigquery_dataset': 'telemetry_derived',
         }
 
         kwargs.update(values)
@@ -93,6 +93,7 @@ with DAG(
     core_clients_daily = bigquery_etl_query(
         task_id='core_clients_daily',
         destination_table='core_clients_daily_v1',
+        dataset_id='telemetry',
     )
 
     tasks['telemetry_core_parquet_bigquery_load'] >> core_clients_daily
@@ -100,6 +101,7 @@ with DAG(
     core_clients_last_seen = bigquery_etl_query(
         task_id='core_clients_last_seen',
         destination_table='core_clients_last_seen_raw_v1',
+        dataset_id='telemetry',
         depends_on_past=True,
     )
 
@@ -111,12 +113,14 @@ with DAG(
     glean_clients_daily = bigquery_etl_query(
         task_id='glean_clients_daily',
         destination_table='glean_clients_daily_v1',
+        dataset_id='telemetry',
         start_date=datetime(2019, 7, 1),
     )
 
     glean_clients_last_seen = bigquery_etl_query(
         task_id='glean_clients_last_seen',
         destination_table='glean_clients_last_seen_raw_v1',
+        dataset_id='telemetry',
         start_date=datetime(2019, 7, 1),
         depends_on_past=True,
     )
@@ -129,6 +133,7 @@ with DAG(
     firefox_nondesktop_exact_mau28_raw = bigquery_etl_query(
         task_id='firefox_nondesktop_exact_mau28_raw',
         destination_table='firefox_nondesktop_exact_mau28_raw_v1',
+        dataset_id='telemetry',
     )
 
     core_clients_last_seen >> firefox_nondesktop_exact_mau28_raw
@@ -137,6 +142,7 @@ with DAG(
     smoot_usage_nondesktop_raw = bigquery_etl_query(
         task_id='smoot_usage_nondesktop_raw',
         destination_table='smoot_usage_nondesktop_raw_v1',
+        dataset_id='telemetry',
     )
 
     core_clients_last_seen >> smoot_usage_nondesktop_raw

dags/first_shutdown_summary.py
@@ -46,7 +46,7 @@ first_shutdown_summary_bigquery_load = SubDagOperator(
         dataset="first_shutdown_summary",
         dataset_version="v4",
         gke_cluster_name="bq-load-gke-1",
-        bigquery_dataset="telemetry_raw",
+        bigquery_dataset="telemetry_derived",
         cluster_by=["sample_id"],
         drop=["submission_date"],
         rename={"submission_date_s3": "submission_date"},

dags/fxa_events.py
@@ -26,24 +26,28 @@ with models.DAG(
     fxa_auth_events = bigquery_etl_query(
         task_id='fxa_auth_events',
         destination_table='fxa_auth_events_v1',
+        dataset_id='telemetry',
         arguments=('--schema_update_option=ALLOW_FIELD_ADDITION',),
     )
 
     fxa_auth_bounce_events = bigquery_etl_query(
         task_id='fxa_auth_bounce_events',
         destination_table='fxa_auth_bounce_events_v1',
+        dataset_id='telemetry',
         arguments=('--schema_update_option=ALLOW_FIELD_ADDITION',),
     )
 
     fxa_content_events = bigquery_etl_query(
         task_id='fxa_content_events',
         destination_table='fxa_content_events_v1',
+        dataset_id='telemetry',
         arguments=('--schema_update_option=ALLOW_FIELD_ADDITION',),
     )
 
     fxa_users_daily = bigquery_etl_query(
         task_id='fxa_users_daily',
         destination_table='fxa_users_daily_v1',
+        dataset_id='telemetry',
     )
 
     fxa_users_daily << fxa_auth_events
@@ -53,6 +57,7 @@ with models.DAG(
     fxa_users_last_seen = bigquery_etl_query(
         task_id='fxa_users_last_seen',
         destination_table='fxa_users_last_seen_raw_v1',
+        dataset_id='telemetry',
         depends_on_past=True,
         start_date=datetime.datetime(2019, 4, 23),
     )
@@ -62,6 +67,7 @@ with models.DAG(
     firefox_accounts_exact_mau28_raw = bigquery_etl_query(
         task_id='firefox_accounts_exact_mau28_raw',
         destination_table='firefox_accounts_exact_mau28_raw_v1',
+        dataset_id='telemetry',
     )
 
     fxa_users_last_seen >> firefox_accounts_exact_mau28_raw
@@ -69,6 +75,7 @@ with models.DAG(
     smoot_usage_fxa_raw = bigquery_etl_query(
         task_id='smoot_usage_fxa_raw',
         destination_table='smoot_usage_fxa_raw_v1',
+        dataset_id='telemetry',
     )
 
     fxa_users_last_seen >> smoot_usage_fxa_raw

dags/kpi_dashboard.py
@@ -25,10 +25,12 @@ with models.DAG(
 
     kpi_dashboard = bigquery_etl_query(
         destination_table='firefox_kpi_dashboard_v1',
+        dataset_id='telemetry',
         date_partition_parameter=None,
     )
 
     smoot_usage_all_mtr = bigquery_etl_query(
         destination_table='smoot_usage_all_mtr_v1',
+        dataset_id='telemetry',
         date_partition_parameter=None,
     )

dags/main_summary.py
@@ -95,7 +95,7 @@ main_summary_bigquery_load = SubDagOperator(
         dataset="main_summary",
         dataset_version="v4",
         gke_cluster_name="bq-load-gke-1",
-        bigquery_dataset="telemetry_raw",
+        bigquery_dataset="telemetry_derived",
         cluster_by=["sample_id"],
         drop=["submission_date"],
         rename={"submission_date_s3": "submission_date"},
@@ -143,7 +143,7 @@ addons_bigquery_load = SubDagOperator(
         dataset="addons",
         dataset_version="v2",
         gke_cluster_name="bq-load-gke-1",
-        bigquery_dataset="telemetry_raw",
+        bigquery_dataset="telemetry_derived",
         cluster_by=["sample_id"],
         rename={"submission_date_s3": "submission_date"},
         replace=["SAFE_CAST(sample_id AS INT64) AS sample_id"],
@@ -175,7 +175,7 @@ main_events_bigquery_load = SubDagOperator(
         dataset="events",
         dataset_version="v1",
         gke_cluster_name="bq-load-gke-1",
-        bigquery_dataset="telemetry_raw",
+        bigquery_dataset="telemetry_derived",
         cluster_by=["sample_id"],
         rename={"submission_date_s3": "submission_date"},
         replace=["SAFE_CAST(sample_id AS INT64) AS sample_id"],
@@ -209,7 +209,7 @@ addon_aggregates_bigquery_load = SubDagOperator(
         dataset_version="v2",
         gke_cluster_name="bq-load-gke-1",
         p2b_table_alias="addons_aggregates_v2",
-        bigquery_dataset="telemetry_raw",
+        bigquery_dataset="telemetry_derived",
         cluster_by=["sample_id"],
         rename={"submission_date_s3": "submission_date"},
         replace=["SAFE_CAST(sample_id AS INT64) AS sample_id"],
@@ -249,7 +249,7 @@ main_summary_experiments_bigquery_load = SubDagOperator(
         gke_cluster_name="bq-load-gke-1",
         p2b_resume=True,
         reprocess=True,
-        bigquery_dataset="telemetry_raw",
+        bigquery_dataset="telemetry_derived",
         ),
     task_id="main_summary_experiments_bigquery_load",
     dag=dag)
@@ -379,7 +379,7 @@ clients_daily_v6_bigquery_load = SubDagOperator(
         dataset_version="v6",
         gke_cluster_name="bq-load-gke-1",
         reprocess=True,
-        bigquery_dataset="telemetry_raw",
+        bigquery_dataset="telemetry_derived",
         cluster_by=["sample_id"],
         rename={"submission_date_s3": "submission_date"},
         replace=["SAFE_CAST(sample_id AS INT64) AS sample_id"],
@@ -389,8 +389,8 @@ clients_daily_v6_bigquery_load = SubDagOperator(
 
 clients_last_seen = bigquery_etl_query(
     task_id="clients_last_seen",
-    arguments=("--dataset_id", "telemetry_raw"),
-    destination_table="clients_last_seen_raw_v1",
+    destination_table="clients_last_seen_v1",
+    dataset_id="telemetry_derived",
     owner="relud@mozilla.com",
     email=["telemetry-alerts@mozilla.com", "relud@mozilla.com", "jklukas@mozilla.com"],
     depends_on_past=True,
@@ -399,9 +399,9 @@ clients_last_seen = bigquery_etl_query(
 
 clients_last_seen_export = SubDagOperator(
     subdag=export_to_parquet(
-        table="clients_last_seen_raw_v1",
+        table="clients_last_seen_v1",
         arguments=[
-            "--dataset=telemetry_raw",
+            "--dataset=telemetry_derived",
             "--submission-date={{ds}}",
             "--destination-table=clients_last_seen_v1",
             "--select",
@@ -421,6 +421,7 @@ clients_last_seen_export = SubDagOperator(
 exact_mau_by_dimensions = bigquery_etl_query(
     task_id="exact_mau_by_dimensions",
     destination_table="firefox_desktop_exact_mau28_by_dimensions_v1",
+    dataset_id="telemetry",
     owner="relud@mozilla.com",
     email=["telemetry-alerts@mozilla.com", "relud@mozilla.com"],
     dag=dag)
@@ -438,6 +439,7 @@ exact_mau_by_dimensions_export = SubDagOperator(
 smoot_usage_desktop_raw = bigquery_etl_query(
     task_id='smoot_usage_desktop_raw',
     destination_table='smoot_usage_desktop_raw_v1',
+    dataset_id='telemetry',
     owner="jklukas@mozilla.com",
     email=["telemetry-alerts@mozilla.com", "jklukas@mozilla.com"],
     dag=dag)

commit e3782d72c6981cb462170ec6f415611fce756da5

Build sql_file_path before appending date to destination_table
Fixes bug introduced in #569 that lead to some failures this morning like:

    /app/script/entrypoint: line 31: sql/telemetry_derived/clients_last_seen_v1$20190730.sql: No such file or directory

dags/utils/gcp.py
@@ -426,6 +426,7 @@ def bigquery_etl_query(
     """
     kwargs["task_id"] = kwargs.get("task_id", destination_table)
     kwargs["name"] = kwargs.get("name", kwargs["task_id"].replace("_", "-"))
+    sql_file_path = sql_file_pathor or "sql/{}/{}.sql".format(dataset_id, destination_table)
     if date_partition_parameter is not None:
         destination_table = destination_table + "${{ds_nodash}}"
         parameters += (date_partition_parameter + ":DATE:{{ds}}",)
@@ -442,7 +443,7 @@ def bigquery_etl_query(
         + (["--project_id=" + project_id] if project_id else [])
         + ["--parameter=" + parameter for parameter in parameters]
         + list(arguments)
-        + [sql_file_path or "sql/{}/{}.sql".format(dataset_id, destination_table)],
+        + [sql_file_path],
         image_pull_policy=image_pull_policy,
         **kwargs
     )