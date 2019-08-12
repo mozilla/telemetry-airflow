ace85c73ea4954a494eb88e398e08254d6e2701e
Frank Bertsch <fbertsch@mozilla.com>
2019-08-12 15:02:08 -0400

    Manually install all pandas deps

--- jobs/fx_usage_report.sh
+++ jobs/fx_usage_report.sh
@@ -6,7 +6,9 @@ fi
 
 pip install py4j --upgrade
 pip install numpy==1.16.4
-pip install pandas==0.24 --upgrade
+pip install python-dateutil==2.5.0
+pip install pytz==2011k
+pip install --no-dependencies pandas==0.24
 
 git clone https://www.github.com/mozilla/Fx_Usage_Report.git
 cd Fx_Usage_Report
