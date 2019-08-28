9c585a62165846f3c8894d09825457df5f70c0f2
Daniel Thorn <dthorn@mozilla.com>
2019-08-28 10:17:11 -0700

    fix ssl_ratios dag

--- dags/ssl_ratios.py
+++ dags/ssl_ratios.py
@@ -16,18 +16,20 @@ default_args = {
     "retry_delay": timedelta(minutes=30),
 }
 
-with DAG("ssl_ratios", default_args=default_args, schedule_interval="@daily"):
+with DAG("ssl_ratios", default_args=default_args, schedule_interval="@daily") as dag:
     # most downstream dependency is search_clients_daily
     wait_for_main_summary = ExternalTaskSensor(
         task_id="wait_for_main_summary",
         external_dag_id="main_summary",
         external_task_id="main_summary",
         execution_delta=timedelta(hours=-1),
+        dag=dag,
     )
 
     ssl_ratios = bigquery_etl_query(
         destination_table="ssl_ratios_v1",
         dataset_id="telemetry_derived",
+        dag=dag,
     )
 
     ssl_ratios.set_upstream(wait_for_main_summary)
