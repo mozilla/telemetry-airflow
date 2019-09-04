b4bb582e16f91d2213504b75154c93bcceb56883
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-09-04 16:59:31 -0700

    Move telemetry_aggregates_parquet to databricks

--- dags/telemetry_aggregates_parquet.py
+++ dags/telemetry_aggregates_parquet.py
@@ -1,29 +1,45 @@
 from airflow import DAG
 from datetime import datetime, timedelta
-from operators.emr_spark_operator import EMRSparkOperator
+from airflow.operators.moz_databricks import MozDatabricksSubmitRunOperator
+from utils.mozetl import mozetl_envvar
 
 default_args = {
-    'owner': 'robhudson@mozilla.com',
-    'depends_on_past': True,
-    'start_date': datetime(2018, 11, 28),
-    'email': ['telemetry-alerts@mozilla.com', 'robhudson@mozilla.com', 'frank@mozilla.com'],
-    'email_on_failure': True,
-    'email_on_retry': True,
-    'retries': 3,
-    'retry_delay': timedelta(minutes=30),
+    "owner": "robhudson@mozilla.com",
+    "depends_on_past": True,
+    "start_date": datetime(2018, 11, 28),
+    "email": [
+        "telemetry-alerts@mozilla.com",
+        "robhudson@mozilla.com",
+        "frank@mozilla.com",
+    ],
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 3,
+    "retry_delay": timedelta(minutes=30),
 }
 
-dag = DAG('telemetry_aggregates_parquet', default_args=default_args, schedule_interval='@daily')
+dag = DAG(
+    "telemetry_aggregates_parquet",
+    default_args=default_args,
+    schedule_interval="@daily",
+)
 
-telemetry_aggregate_parquet_view = EMRSparkOperator(
+telemetry_aggregate_parquet_view = MozDatabricksSubmitRunOperator(
     task_id="telemetry_aggregate_parquet_view",
     job_name="Telemetry Aggregate Parquet View",
     instance_count=5,
     execution_timeout=timedelta(hours=12),
-    env={
-      "date": "{{ ds_nodash }}",
-      "channels": "nightly",
-      "bucket": "{{ task.__class__.private_output_bucket }}",
-    },
-    uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/run_telemetry_aggregator_parquet.sh",
-    dag=dag)
+    env=mozetl_envvar(
+        "parquet",
+        {
+            "date": "{{ ds_nodash }}",
+            "channels": "nightly",
+            "output": "s3://{{ task.__class__.private_output_bucket }}/aggregates_poc/v1",
+        },
+        other={
+            "MOZETL_GIT_PATH": "https://github.com/mozilla/python_mozaggregator.git",
+            "MOZETL_EXTERNAL_MODULE": "mozaggregator",
+        },
+    ),
+    dag=dag,
+)
