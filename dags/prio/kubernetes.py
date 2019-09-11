4cf2930ebe17c3ba2e965ac148d3fbd50b416765
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-09-11 16:51:55 -0700

    Prepare Prio DAGs for weekly/daily processing (#561)
    * Run black on prio files
    
    * Rename prio dag
    
    * Create both prio dags in the same file
    
    * Add testing credentials from prio-processor/processor/docker-compose.yml
    
    * Set the application to processor/bin/process
    
    * Use sub-dags for coordinating prio servers
    
    * Update owner and email
    
    * Add boostrapping stage before running dataproc
    
    * Black on gke util
    
    * Add subnetwork to create_gke_config
    
    * Add is_dev to create_gke_config
    
    * Add dataproc job for prio-processor staging
    
    * Update dataproc configuration with initization code
    
    * Add transfer job to trigger processing job
    
    * Fix transfer job and add service account to dataproc
    
    * Rename main processing function
    
    * Fix logic for setting the default subnet
    
    * Add module doc-string
    
    * Refactor prio_processor dag into a subfolder
    
    * [wip] Refactor into utility factories
    
    * Update prio processor into separate submodule
    
    * Function cleanup and renaming
    
    * Update staging subdag to be consistent with processor subdag
    
    * Rename transfer job to load_processor_{id}
    
    * Add docstrings for subdag factories
    
    * Annotate prio processor dag
    
    * Add Airflow variables for processor keypairs
    
    * Add method to clean buckets, fix bugs
    
    * Copy resulting aggregates into the prio admin project
    
    * Add comment about "master authorized networks config"
    
    * Move shared_secret into an airflow variable
    
    * Replace service account and bucket strings into variables
    
    * Update for prod/nonprod projects
    
    * Move arguments for dataproc job into main dag file
    
    * Update variables to distinguish between realm and environment
    
    * Update with new config
    
    * Sleep between GKE cluster creation and container runs
    
    * Expose machine type to DAG
    
    * Update start date and processor timeout
    
    * Increase processor retry and reduce airflow retry delay
    
    Remove the cluster if it already exists
    
    * Clean the whole bucket to avoid inserting duplicate values
    
    * Update owner and team labels for GKE
    
    * Update comments
    
    * Add defaults for running containers
    
    * Use variables for connection string
    
    * Change default zone, limit active runs, and add dag to each task

---
+++ dags/prio/kubernetes.py
@@ -0,0 +1,112 @@
+from datetime import timedelta
+from os import environ
+
+from airflow import DAG
+from airflow.contrib.hooks.gcp_api_base_hook import GoogleCloudBaseHook
+from operators.gcp_container_operator import (
+    GKEClusterCreateOperator,
+    GKEClusterDeleteOperator,
+    GKEPodOperator,
+)
+from utils.gke import create_gke_config
+from airflow.operators.bash_operator import BashOperator
+
+
+def container_subdag(
+    parent_dag_name,
+    child_dag_name,
+    default_args,
+    gcp_conn_id,
+    service_account,
+    server_id,
+    env_vars={},
+    arguments=[],
+    machine_type="n1-standard-1",
+    image="mozilla/prio-processor:latest",
+    location="us-west1-b",
+    owner_label="amiyaguchi",
+    team_label="dataeng",
+):
+    """Run a command on an ephemeral container running the
+    `mozilla/prio-processor:latest` image.
+
+    :param str parent_dag_name:         Name of the parent DAG.
+    :param str child_dag_name:          Name of the child DAG.
+    :param Dict[str, Any] default_args: Default arguments for the child DAG.
+    :param str gcp_conn_id:             Name of the connection string.
+    :param str service_account:         The address of the service account.
+    :param str server_id:               The identifier for the Prio processor
+    :param Dict[str, str] env_vars:     Environment variables for configuring
+                                        the processor.
+    :param List[str] arguments:         The command to run after loading the
+                                        image.
+    :param str machine_type:            The machine type for running the image.
+    :param str image:                   Dockerhub image
+    :param str location:                The region of the GKE cluster.
+    :param str owner_label:             Label for associating the owner
+    :param str team_label:              Label for associating the team
+    :return: DAG
+    """
+    assert server_id in ["a", "b", "admin"]
+
+    connection = GoogleCloudBaseHook(gcp_conn_id=gcp_conn_id)
+
+    cluster_name = "gke-prio-{}".format(server_id)
+
+    shared_config = {
+        "project_id": connection.project_id,
+        "gcp_conn_id": gcp_conn_id,
+        "location": location,
+    }
+
+    with DAG(
+        "{}.{}".format(parent_dag_name, child_dag_name), default_args=default_args
+    ) as dag:
+        create_gke_cluster = GKEClusterCreateOperator(
+            task_id="create_gke_cluster",
+            body=create_gke_config(
+                name=cluster_name,
+                service_account=service_account,
+                owner_label=owner_label,
+                team_label=team_label,
+                machine_type=machine_type,
+                # DataProc clusters require VPC with auto-created subnets
+                subnetwork="default" if server_id == "admin" else "gke-subnet",
+                is_dev=environ.get("DEPLOY_ENVIRONMENT") == "dev",
+            ),
+            dag=dag,
+            **shared_config
+        )
+
+        # Running the pod without any time in-between will cause the scope-based
+        # authentication in Google Cloud Platform to fail. For example:
+        #
+        # `ServiceException: 401 Anonymous caller does not have
+        # storage.objects.get access to moz-fx-prio-dev-a-private/processed/`
+        #
+        # Sleeping by a small amount solves this problem. This issue was first
+        # noticed intermittently on 2019-09-09.
+        sleep = BashOperator(task_id="sleep", bash_command="sleep 60", dag=dag)
+
+        run_prio = GKEPodOperator(
+            task_id="processor_{}".format(server_id),
+            name="run-prio-project-{}".format(server_id),
+            cluster_name=cluster_name,
+            namespace="default",
+            image=image,
+            arguments=arguments,
+            env_vars=env_vars,
+            dag=dag,
+            **shared_config
+        )
+
+        delete_gke_cluster = GKEClusterDeleteOperator(
+            task_id="delete_gke_cluster",
+            name=cluster_name,
+            trigger_rule="all_done",
+            dag=dag,
+            **shared_config
+        )
+
+        create_gke_cluster >> sleep >> run_prio >> delete_gke_cluster
+        return dag
--- dags/utils/gke.py
+++ dags/utils/gke.py
@@ -7,7 +7,10 @@ def create_gke_config(
     disk_size_gb=100,
     preemptible=True,
     disk_type="pd-standard",
-    location="us-west1-b"):
+    location="us-west1-b",
+    subnetwork="default",
+    is_dev=False,
+):
 
     """
     Helper function to create gke cluster definition dict.
@@ -17,13 +20,13 @@ def create_gke_config(
     underscores, and dashes. E.g. owner_label='hwoo', team_label='dataops'
 
     """
-    
+
     cluster_def_dict = {
         "name": name,
-        "masterAuthorizedNetworksConfig":
-            {
-            "enabled": True,
-            },
+        # Setting `{"enabled": true}` will open the GKE cluster to the world.
+        # This is config is disabled when run locally, otherwise job submissions
+        # will fail.
+        "masterAuthorizedNetworksConfig": {"enabled": not is_dev},
         "nodePools": [
             {
                 "name": name,
@@ -31,6 +34,7 @@ def create_gke_config(
                     "machineType": machine_type,
                     "diskSizeGb": disk_size_gb,
                     "oauthScopes": [
+                        "https://www.googleapis.com/auth/bigquery",
                         "https://www.googleapis.com/auth/devstorage.read_write",
                         "https://www.googleapis.com/auth/logging.write",
                         "https://www.googleapis.com/auth/monitoring",
@@ -39,26 +43,17 @@ def create_gke_config(
                         "https://www.googleapis.com/auth/trace.append",
                     ],
                     "serviceAccount": service_account,
-                    "labels": {
-                        "owner": owner_label,
-                        "team": team_label
-                    },
+                    "labels": {"owner": owner_label, "team": team_label},
                     "preemptible": preemptible,
-                    "diskType": disk_type
+                    "diskType": disk_type,
                 },
                 "initialNodeCount": 1,
-                "autoscaling": {
-                    "enabled" : True,
-                    "minNodeCount": 1,
-                    "maxNodeCount": 5
-                }
+                "autoscaling": {"enabled": True, "minNodeCount": 1, "maxNodeCount": 5},
             }
         ],
-        "locations": [
-            location
-        ],
+        "locations": [location],
         "network": "default",
-        "subnetwork": "gke-subnet"
+        "subnetwork": subnetwork,
     }
 
     return cluster_def_dict
