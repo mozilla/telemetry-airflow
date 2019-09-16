78150bb25fd367951cd9d5a581044ea98a9e0ca0
Victor Ng <victor@crankycoder.com>
2019-09-16 01:59:07 -0400

    reworked taar_lite job to use moz_dataproc_pyspark_runner

--- dags/taar_amodump.py
+++ dags/taar_amodump.py
@@ -2,21 +2,47 @@ from airflow import DAG
 from datetime import datetime, timedelta
 
 from operators.emr_spark_operator import EMRSparkOperator
+from airflow.operators.subdag_operator import SubDagOperator
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.contrib.hooks.gcp_api_base_hook import GoogleCloudBaseHook
 
 from utils.mozetl import mozetl_envvar
+from utils.dataproc import moz_dataproc_pyspark_runner
+
+# Unsalted cluster name so subsequent runs fail if the cluster name exists
+cluster_name = "taarlite-dataproc-cluster"
+
+# We use an application-specific gcs bucket since the copy operator can't set the destination
+# bucket prefix, and unfortunately socorro data in s3 has prefix version/dataset instead
+# of having the dataset name come first
+gcs_data_bucket = "moz-fx-data-prod-taarlite-data"
+
+dataset = "taarlite_guidranking"
+
+# Defined in Airflow's UI -> Admin -> Connections
+gcp_conn_id = "google_cloud_airflow_dataproc"
+connection = GoogleCloudBaseHook(gcp_conn_id=gcp_conn_id)
+
+# Required to copy socorro json data from aws prod s3 to gcs
+read_aws_conn_id = "aws_taarlite_readonly_s3"
+
+# Required to write parquet data back to s3://telemetry-parquet
+write_aws_conn_id = "aws_dev_taarlite_telemetry_parquet_s3"
+aws_access_key, aws_secret_key, session = AwsHook(write_aws_conn_id).get_credentials()
+
 
 default_args = {
-    'owner': 'amiyaguchi@mozilla.com',
-    'depends_on_past': False,
-    'start_date': datetime(2018, 11, 26),
-    'email': ['telemetry-alerts@mozilla.com', 'amiyaguchi@mozilla.com'],
-    'email_on_failure': True,
-    'email_on_retry': True,
-    'retries': 3,
-    'retry_delay': timedelta(minutes=30),
+    "owner": "amiyaguchi@mozilla.com",
+    "depends_on_past": False,
+    "start_date": datetime(2018, 11, 26),
+    "email": ["telemetry-alerts@mozilla.com", "amiyaguchi@mozilla.com"],
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 3,
+    "retry_delay": timedelta(minutes=30),
 }
 
-dag = DAG('taar_amodump', default_args=default_args, schedule_interval='@daily')
+dag = DAG("taar_amodump", default_args=default_args, schedule_interval="@daily")
 
 amodump = EMRSparkOperator(
     task_id="taar_amodump",
@@ -25,12 +51,14 @@ amodump = EMRSparkOperator(
     instance_count=1,
     owner="vng@mozilla.com",
     email=["mlopatka@mozilla.com", "vng@mozilla.com"],
-    env=mozetl_envvar("taar_amodump",
-                      {"date": "{{ ds_nodash }}"},
-                      {'MOZETL_SUBMISSION_METHOD': 'python'}),
+    env=mozetl_envvar(
+        "taar_amodump",
+        {"date": "{{ ds_nodash }}"},
+        {"MOZETL_SUBMISSION_METHOD": "python"},
+    ),
     uri="https://raw.githubusercontent.com/mozilla/python_mozetl/master/bin/mozetl-submit.sh",
     output_visibility="private",
-    dag=dag
+    dag=dag,
 )
 
 amowhitelist = EMRSparkOperator(
@@ -40,12 +68,10 @@ amowhitelist = EMRSparkOperator(
     instance_count=1,
     owner="vng@mozilla.com",
     email=["mlopatka@mozilla.com", "vng@mozilla.com"],
-    env=mozetl_envvar("taar_amowhitelist",
-                      {},
-                      {'MOZETL_SUBMISSION_METHOD': 'spark'}),
+    env=mozetl_envvar("taar_amowhitelist", {}, {"MOZETL_SUBMISSION_METHOD": "spark"}),
     uri="https://raw.githubusercontent.com/mozilla/python_mozetl/master/bin/mozetl-submit.sh",
     output_visibility="private",
-    dag=dag
+    dag=dag,
 )
 
 editorial_whitelist = EMRSparkOperator(
@@ -55,27 +81,37 @@ editorial_whitelist = EMRSparkOperator(
     instance_count=1,
     owner="vng@mozilla.com",
     email=["mlopatka@mozilla.com", "vng@mozilla.com"],
-    env=mozetl_envvar("taar_update_whitelist",
-                      {"date": "{{ ds_nodash }}"},
-                      {'MOZETL_SUBMISSION_METHOD': 'spark'}),
+    env=mozetl_envvar(
+        "taar_update_whitelist",
+        {"date": "{{ ds_nodash }}"},
+        {"MOZETL_SUBMISSION_METHOD": "spark"},
+    ),
     uri="https://raw.githubusercontent.com/mozilla/python_mozetl/master/bin/mozetl-submit.sh",
     output_visibility="private",
-    dag=dag
+    dag=dag,
 )
 
-taar_lite = EMRSparkOperator(
+
+# Spark job reads gcs json and writes gcs parquet
+taar_lite = SubDagOperator(
     task_id="taar_lite",
-    job_name="Generate GUID coinstallation JSON for TAAR",
-    instance_count=5,
-    execution_timeout=timedelta(hours=4),
-    owner="mlopatka@mozilla.com",
-    email=["mlopatka@mozilla.com", "vng@mozilla.com"],
-    env=mozetl_envvar("taar_lite",
-                      {"date": "{{ ds_nodash }}"},
-                      {'MOZETL_SUBMISSION_METHOD': 'spark'}),
-    uri="https://raw.githubusercontent.com/mozilla/python_mozetl/master/bin/mozetl-submit.sh",
-    output_visibility="private",
-    dag=dag
+    dag=dag,
+    subdag=moz_dataproc_pyspark_runner(
+        parent_dag_name=dag.dag_id,
+        default_args=default_args,
+        cluster_name=cluster_name,
+        job_name="Generate GUID coinstallation JSON for TAAR",
+        # TODO - CI/CD for gcs and repo sync for artifact bucket jobs and gcp bootstrap folders
+        # TODO: the currentcode lives at - need to get it into GCS
+        # https://github.com/mozilla/python_mozetl/blob/master/mozetl/taar/taar_lite_guidguid.py
+        python_driver_code="gs://moz-fx-data-prod-airflow-dataproc-artifacts/jobs/socorro_import_crash_data.py",
+        py_args=["--date", "{{ ds_nodash }}"],
+        idle_delete_ttl="14400",
+        num_workers=5,
+        worker_machine_type="n1-standard-8",
+        aws_conn_id=read_aws_conn_id,
+        gcp_conn_id=gcp_conn_id,
+    ),
 )
 
 # Set a dependency on amodump from amowhitelist
