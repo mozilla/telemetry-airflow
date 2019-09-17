10191a032a8ef016de87bd64a45f19e460132b15
Harold Woo <hwoo@mozilla.com>
2019-09-17 08:37:27 -0700

    Modify Fx usage report to use Dataproc instead of EMR

--- dags/fx_usage_report.py
+++ dags/fx_usage_report.py
@@ -1,13 +1,13 @@
 from airflow import DAG
 from airflow.operators.sensors import ExternalTaskSensor
 from datetime import datetime, timedelta
-from operators.emr_spark_operator import EMRSparkOperator
+
 from utils.constants import DS_WEEKLY
 
 default_args = {
     'owner': 'frank@mozilla.com',
     'depends_on_past': True,
-    'start_date': datetime(2018, 11, 25),
+    'start_date': datetime(2019, 9, 25),
     'email': ['telemetry-alerts@mozilla.com', 'frank@mozilla.com'],
     'email_on_failure': True,
     'email_on_retry': True,
@@ -15,8 +15,12 @@ default_args = {
     'retry_delay': timedelta(minutes=30),
 }
 
-dag = DAG('fx_usage_report', default_args=default_args, schedule_interval='@weekly')
+# TODO - change back to weekly
+#dag = DAG('fx_usage_report', default_args=default_args, schedule_interval='@weekly')
+dag = DAG('fx_usage_report', default_args=default_args, schedule_interval='@daily')
 
+# TODO - add back
+"""
 wait_for_main_summary = ExternalTaskSensor(
     task_id='wait_for_main_summary',
     external_dag_id='main_summary',
@@ -36,5 +40,34 @@ usage_report = EMRSparkOperator(
          "deploy_environment": "{{ task.__class__.deploy_environment }}"},
     uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/fx_usage_report.sh",
     dag=dag)
+"""
 
-usage_report.set_upstream(wait_for_main_summary)
+cluster_name = 'fx-usage-report-dataproc-cluster'
+gcp_conn_id = 'google_cloud_airflow_dataproc'
+
+# AWS credentials to ???
+aws_conn_id = 'aws_prod_fx_usage_report'
+
+usage_report = SubDagOperator(
+    task_id="fx_usage_report",
+    dag=dag,
+    subdag = moz_dataproc_scriptrunner(
+        job_name="Fx_Usage_Report",
+        dag_name='run_fx_usage_report_script',
+        default_args=default_args,
+        cluster_name=cluster_name,
+        uri="https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/fx_usage_report.sh",
+        env={"date": DS_WEEKLY,
+             "bucket": "net-mozaws-prod-us-west-2-data-public",
+             "deploy_environment": "{{ task.__class__.deploy_environment }}"
+        },
+        gcp_conn_id=gcp_conn_id,
+        aws_conn_id=aws_conn_id,
+        num_workers=8,
+        owner="frank@mozilla.com",
+        email=["telemetry-alerts@mozilla.com", "frank@mozilla.com", "shong@mozilla.com"],
+    )
+)
+
+# TODO - add back
+#usage_report.set_upstream(wait_for_main_summary)
