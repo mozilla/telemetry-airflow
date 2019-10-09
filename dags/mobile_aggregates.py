4cf48e031c606df2108cb28869e2cf333e0bd775
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-10-09 12:35:17 -0700

    Move mozaggregator jobs to Databricks (#593)
    * Move telemetry_aggregates_parquet to databricks
    
    * Move telemetry aggregates to databricks
    
    * Update mobile aggregates to use databricks
    
    * Propagate DB_TOKEN properly
    
    * Use python2 by default for mozaggregator jobs
    
    * Up dev instance count for prerelease aggregate view

--- dags/mobile_aggregates.py
+++ dags/mobile_aggregates.py
@@ -1,45 +1,51 @@
 from datetime import datetime, timedelta
 
 from airflow import DAG
-from operators.emr_spark_operator import EMRSparkOperator
-
+from airflow.operators.moz_databricks import MozDatabricksSubmitRunOperator
+from utils.mozetl import mozetl_envvar
 from utils.status import register_status
 
-
 default_args = {
-    'owner': 'robhudson@mozilla.com',
-    'depends_on_past': True,
-    'start_date': datetime(2019, 1, 1),
-    'email': ['telemetry-alerts@mozilla.com',
-              'robhudson@mozilla.com', 'frank@mozilla.com'],
-    'email_on_failure': True,
-    'email_on_retry': True,
-    'retries': 3,
-    'retry_delay': timedelta(minutes=30),
+    "owner": "robhudson@mozilla.com",
+    "depends_on_past": True,
+    "start_date": datetime(2019, 1, 1),
+    "email": [
+        "telemetry-alerts@mozilla.com",
+        "robhudson@mozilla.com",
+        "frank@mozilla.com",
+    ],
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 3,
+    "retry_delay": timedelta(minutes=30),
 }
 
-dag = DAG(
-    'mobile_aggregates',
-    default_args=default_args,
-    schedule_interval='@daily'
-)
+dag = DAG("mobile_aggregates", default_args=default_args, schedule_interval="@daily")
 
-mobile_aggregate_view = EMRSparkOperator(
+mobile_aggregate_view = MozDatabricksSubmitRunOperator(
     task_id="mobile_aggregate_view",
     job_name="Mobile Aggregate View",
     instance_count=5,
     execution_timeout=timedelta(hours=12),
-    env={
-      "date": "{{ ds_nodash }}",
-      "channels": "nightly",
-      "bucket": "{{ task.__class__.private_output_bucket }}",
-    },
-    uri=("https://raw.githubusercontent.com/"
-         "mozilla/telemetry-airflow/master/jobs/run_mobile_aggregator.sh"),
-    dag=dag)
+    python_version=2,
+    env=mozetl_envvar(
+        "mobile",
+        {
+            "date": "{{ ds_nodash }}",
+            "channels": "nightly",
+            "output": "s3://{{ task.__class__.private_output_bucket }}/mobile_metrics_aggregates/v2",
+            "num-partitions": 5*32
+        },
+        other={
+            "MOZETL_GIT_PATH": "https://github.com/mozilla/python_mozaggregator.git",
+            "MOZETL_EXTERNAL_MODULE": "mozaggregator",
+        },
+    ),
+    dag=dag,
+)
 
 register_status(
     mobile_aggregate_view,
-    'Mobile Aggregates',
-    'Aggregates of metrics sent through the mobile-events pings.'
+    "Mobile Aggregates",
+    "Aggregates of metrics sent through the mobile-events pings.",
 )
--- dags/release_telemetry_aggregates.py
+++ dags/release_telemetry_aggregates.py
@@ -1,28 +1,45 @@
 from airflow import DAG
 from datetime import datetime, timedelta
-from operators.emr_spark_operator import EMRSparkOperator
+from airflow.operators.moz_databricks import MozDatabricksSubmitRunOperator
+from utils.mozetl import mozetl_envvar
 
 default_args = {
-    'owner': 'frank@mozilla.com',
-    'depends_on_past': True,
-    'start_date': datetime(2018, 12, 17),
-    'email': ['telemetry-alerts@mozilla.com', 'frank@mozilla.com'],
-    'email_on_failure': True,
-    'email_on_retry': True,
-    'retries': 3,
-    'retry_delay': timedelta(minutes=30),
+    "owner": "frank@mozilla.com",
+    "depends_on_past": True,
+    "start_date": datetime(2018, 12, 17),
+    "email": ["telemetry-alerts@mozilla.com", "frank@mozilla.com"],
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 3,
+    "retry_delay": timedelta(minutes=30),
 }
 
-dag = DAG('release_telemetry_aggregates', default_args=default_args, schedule_interval='@daily')
+dag = DAG(
+    "release_telemetry_aggregates",
+    default_args=default_args,
+    schedule_interval="@daily",
+)
 
-release_telemetry_aggregate_view = EMRSparkOperator(
-    task_id = "release_telemetry_aggregate_view",
-    job_name = "Release Telemetry Aggregate View",
-    instance_count = 40,
+release_telemetry_aggregate_view = MozDatabricksSubmitRunOperator(
+    task_id="release_telemetry_aggregate_view",
+    job_name="Release Telemetry Aggregate View",
+    instance_count=40,
     execution_timeout=timedelta(hours=12),
-    env = {
-      "date": "{{ ds_nodash }}",
-      "channels": "release"
-    },
-    uri = "https://raw.githubusercontent.com/mozilla/telemetry-airflow/master/jobs/run_telemetry_aggregator.sh",
-    dag = dag)
+    python_version=2,
+    env=mozetl_envvar(
+        "aggregator",
+        {
+            "date": "{{ ds_nodash }}",
+            "channels": "release",
+            "credentials-bucket": "telemetry-spark-emr-2",
+            "credentials-prefix": "aggregator_database_envvars.json",
+            "num-partitions": 40 * 32,
+        },
+        dev_options={"credentials-prefix": "aggregator_dev_database_envvars.json"},
+        other={
+            "MOZETL_GIT_PATH": "https://github.com/mozilla/python_mozaggregator.git",
+            "MOZETL_EXTERNAL_MODULE": "mozaggregator",
+        },
+    ),
+    dag=dag,
+)
