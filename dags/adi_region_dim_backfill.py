4dc995a6ef0c78895db59e8d5dd5b0e009585ba9
Harold Woo <hwoo@mozilla.com>
2019-10-24 16:21:37 -0700

    initial commit, adding backfill job for adi by region

---
+++ dags/adi_region_dim_backfill.py
@@ -0,0 +1,58 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.contrib.hooks.gcp_api_base_hook import GoogleCloudBaseHook
+from operators.gcp_container_operator import GKEPodOperator
+
+
+DEFAULT_ARGS = {
+    'owner': 'hwoo@mozilla.com',
+    'depends_on_past': False,
+    'start_date': datetime(2018, 1, 1),
+    'end_date': datetime(2019, 10, 1),
+    'email': ['hwoo@mozilla.com', 'dataops+alerts@mozilla.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=15),
+}
+
+gcp_conn_id = "google_cloud_derived_datasets"
+connection = GoogleCloudBaseHook(gcp_conn_id=gcp_conn_id)
+
+blp_dag = DAG(
+    'adi_region_dim_backfill',
+    default_args=DEFAULT_ARGS,
+    dagrun_timeout=timedelta(hours=1),
+    schedule_interval='0 0 * * *'
+)
+
+# TODO - create bq table first 
+# partition by year!
+# yr,mnth,region,country_code,domain,tot_requests,product
+# 2018,1,AA,US,blocklists.settings.services.mozilla.com,89,Chrome
+# 2018,1,AA,US,blocklists.settings.services.mozilla.com,1807,Firefox
+load_args = [
+    'bq',
+    '--location=US',
+    'load',
+    '--source_format=CSV',
+    '--skip_leading_rows=1',
+    '--replace',
+    '--field_delimiter=,',
+    'blpadi.adi_by_region${{ ds_nodash_onlymonth or year? }}',
+    'gs://dp2-stage-vertica/adi_by_region/adi_by_region-{{ ds onlymonth }}.csv',
+]
+
+load_bq = GKEPodOperator(
+    task_id='bq_load',
+    gcp_conn_id=gcp_conn_id,
+    project_id=connection.project_id,
+    location='us-central1-a',
+    cluster_name='bq-load-gke-1',
+    name='bq-load',
+    namespace='default',
+    image='google/cloud-sdk:242.0.0-alpine',
+    arguments=load_args,
+    dag=blp_dag
+)
+
