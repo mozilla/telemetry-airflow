437d24339f821954ec01c046ed674a2799026e89
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-11-08 11:10:35 -0500

    Revert "Merge telemetry aggregates job into a single file (#694)"
    This reverts commit 713b0c2f7c068aaa581f7f78368dd46df6ce413f.

---
+++ dags/release_telemetry_aggregates.py
@@ -0,0 +1,45 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.moz_databricks import MozDatabricksSubmitRunOperator
+from utils.mozetl import mozetl_envvar
+
+default_args = {
+    "owner": "frank@mozilla.com",
+    "depends_on_past": True,
+    "start_date": datetime(2018, 12, 17),
+    "email": ["telemetry-alerts@mozilla.com", "frank@mozilla.com"],
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 3,
+    "retry_delay": timedelta(minutes=30),
+}
+
+dag = DAG(
+    "release_telemetry_aggregates",
+    default_args=default_args,
+    schedule_interval="@daily",
+)
+
+release_telemetry_aggregate_view = MozDatabricksSubmitRunOperator(
+    task_id="release_telemetry_aggregate_view",
+    job_name="Release Telemetry Aggregate View",
+    instance_count=40,
+    execution_timeout=timedelta(hours=12),
+    python_version=2,
+    env=mozetl_envvar(
+        "aggregator",
+        {
+            "date": "{{ ds_nodash }}",
+            "channels": "release",
+            "credentials-bucket": "telemetry-spark-emr-2",
+            "credentials-prefix": "aggregator_database_envvars.json",
+            "num-partitions": 40 * 32,
+        },
+        dev_options={"credentials-prefix": "aggregator_dev_database_envvars.json"},
+        other={
+            "MOZETL_GIT_PATH": "https://github.com/mozilla/python_mozaggregator.git",
+            "MOZETL_EXTERNAL_MODULE": "mozaggregator",
+        },
+    ),
+    dag=dag,
+)
