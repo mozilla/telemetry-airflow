b9d7c135a071defd5e03e89ff53f61de590d170b
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-11-14 17:29:45 -0800

    Bug 1572115 - Add dataproc version aggregates job that writes to a dev database (#727)
    * Bug 1572115 - Add dataproc version aggregates job that writes to a dev database
    
    * Add bigquery source and project-id
    
    * Expose storage_bucket and artifact_bucket for local testing
    
    * Add GOOGLE_APPLICATION_CREDENTIALS to docker-compose for local testing
    
    * Update prerelease aggregates to run locally
    
    * Minimize mozaggregator runner
    
    * Use default service account on production
    
    * Use n1-standard-8 on mozaggregator job

--- dags/mozaggregator_prerelease.py
+++ dags/mozaggregator_prerelease.py
@@ -1,13 +1,24 @@
-from airflow import DAG
+import json
+import os
 from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.contrib.hooks.gcp_api_base_hook import GoogleCloudBaseHook
+from airflow.operators.bash_operator import BashOperator
 from airflow.operators.moz_databricks import MozDatabricksSubmitRunOperator
+from airflow.operators.subdag_operator import SubDagOperator
+from utils.dataproc import moz_dataproc_pyspark_runner
 from utils.mozetl import mozetl_envvar
 
 default_args = {
     "owner": "frank@mozilla.com",
     "depends_on_past": True,
     "start_date": datetime(2018, 12, 23),
-    "email": ["telemetry-alerts@mozilla.com", "frank@mozilla.com"],
+    "email": [
+        "telemetry-alerts@mozilla.com",
+        "frank@mozilla.com",
+        "amiyaguchi@mozilla.com",
+    ],
     "email_on_failure": True,
     "email_on_retry": True,
     "retries": 3,
@@ -34,7 +45,7 @@ prerelease_telemetry_aggregate_view = MozDatabricksSubmitRunOperator(
             "channels": "nightly,aurora,beta",
             "credentials-bucket": "telemetry-spark-emr-2",
             "credentials-prefix": "aggregator_database_envvars.json",
-            "num-partitions": 10*32,
+            "num-partitions": 10 * 32,
         },
         dev_options={"credentials-prefix": "aggregator_dev_database_envvars.json"},
         other={
@@ -44,3 +55,110 @@ prerelease_telemetry_aggregate_view = MozDatabricksSubmitRunOperator(
     ),
     dag=dag,
 )
+
+
+subdag_args = default_args.copy()
+subdag_args["retries"] = 0
+
+task_id = "prerelease_telemetry_aggregate_view_dataproc"
+gcp_conn = GoogleCloudBaseHook("google_cloud_airflow_dataproc")
+keyfile = json.loads(gcp_conn.extras["extra__google_cloud_platform__keyfile_dict"])
+project_id = keyfile["project_id"]
+
+is_dev = os.environ.get("DEPLOY_ENVIRONMENT") == "dev"
+client_email = (
+    keyfile["client_email"]
+    if is_dev
+    else "dataproc-runner-prod@airflow-dataproc.iam.gserviceaccount.com"
+)
+artifact_bucket = (
+    "{}-dataproc-artifacts".format(project_id)
+    if is_dev
+    else "moz-fx-data-prod-dataproc-artifacts"
+)
+storage_bucket = (
+    "{}-dataproc-scratch".format(project_id)
+    if is_dev
+    else "moz-fx-data-prod-dataproc-scratch"
+)
+
+
+def copy_artifacts_dev(dag, project_id, artifact_bucket, storage_bucket):
+    return BashOperator(
+        task_id="copy_to_dev_artifacts",
+        bash_command="""
+        gcloud auth activate-service-account --key-file ~/.credentials
+
+        gsutil mb gs://${ARTIFACT_BUCKET}
+        gsutil mb gs://${STORAGE_BUCKET}
+
+        gsutil -m cp -r ~/dataproc_bootstrap gs://${ARTIFACT_BUCKET}
+        gsutil -m cp -r ~/jobs gs://${ARTIFACT_BUCKET}
+
+        echo "listing artifacts..."
+        gsutil ls -r gs://${ARTIFACT_BUCKET}
+        """,
+        env={
+            # https://github.com/GoogleCloudPlatform/gsutil/issues/236
+            "CLOUDSDK_PYTHON": "python",
+            "PROJECT_ID": project_id,
+            "ARTIFACT_BUCKET": artifact_bucket,
+            "STORAGE_BUCKET": storage_bucket,
+        },
+        dag=dag,
+    )
+
+
+prerelease_telemetry_aggregate_view_dataproc = SubDagOperator(
+    task_id=task_id,
+    dag=dag,
+    subdag=moz_dataproc_pyspark_runner(
+        parent_dag_name=dag.dag_id,
+        dag_name=task_id,
+        job_name="prerelease_aggregates",
+        cluster_name="prerelease-telemetry-aggregates-{{ ds_nodash }}",
+        idle_delete_ttl="600",
+        num_workers=10,
+        worker_machine_type="n1-standard-8",
+        init_actions_uris=[
+            "gs://dataproc-initialization-actions/python/pip-install.sh"
+        ],
+        additional_properties={
+            "spark:spark.jars": "gs://spark-lib/bigquery/spark-bigquery-latest.jar"
+        },
+        additional_metadata={
+            "PIP_PACKAGES": "git+https://github.com/mozilla/python_mozaggregator.git"
+        },
+        python_driver_code="gs://{}/jobs/mozaggregator_runner.py".format(
+            artifact_bucket
+        ),
+        py_args=[
+            "aggregator",
+            "--date",
+            "{{ ds_nodash }}",
+            "--channels",
+            "nightly,aurora,beta",
+            "--credentials-bucket",
+            "telemetry-spark-emr-2",
+            "--credentials-prefix",
+            "aggregator_dev_database_envvars.json",
+            "--num-partitions",
+            str(10 * 32),
+            "--source",
+            "bigquery",
+            "--project-id",
+            "moz-fx-data-shared-prod",
+        ],
+        aws_conn_id="aws_dev_iam_s3",
+        gcp_conn_id=gcp_conn.gcp_conn_id,
+        service_account=client_email,
+        artifact_bucket=artifact_bucket,
+        storage_bucket=storage_bucket,
+        default_args=subdag_args,
+    ),
+)
+
+# copy over artifacts if we're running in dev
+if is_dev:
+    copy_to_dev = copy_artifacts_dev(dag, project_id, artifact_bucket, storage_bucket)
+    copy_to_dev.set_downstream(prerelease_telemetry_aggregate_view_dataproc)
