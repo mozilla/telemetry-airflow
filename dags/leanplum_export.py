9d94d9a3f1443ccc38648085de001f2f4337c0bb
Frank Bertsch <fbertsch@mozilla.com>
2019-11-21 12:46:17 -0500

    Add addl leanplum project exports

--- dags/leanplum_export.py
+++ dags/leanplum_export.py
@@ -19,16 +19,57 @@ with DAG('leanplum_export',
          default_args=default_args,
          schedule_interval='@daily') as dag:
 
-    app_id = Variable.get('LEANPLUM_FENNEC_NIGHTLY_APP_ID')
-    client_key = Variable.get('LEANPLUM_FENNEC_NIGHTLY_CLIENT_KEY')
-
     fennec_nightly_export = leanplum.export(
         task_id='fennec_nightly_export',
-        leanplum_app_id=app_id,
-        leanplum_client_key=client_key,
+        leanplum_app_id=Variable.get('LEANPLUM_FENNEC_NIGHTLY_APP_ID'),
+        leanplum_client_key=Variable.get('LEANPLUM_FENNEC_NIGHTLY_CLIENT_KEY'),
         bq_project='moz-fx-data-shared-prod',
         gcs_prefix='firefox/android/nightly',
         bq_dataset_id='firefox_android_nightly_external',
+        table_prefix='leanplum',
+        dag=dag
+    )
+
+    fennec_beta_export = leanplum.export(
+        task_id='fennec_beta_export',
+        leanplum_app_id=Variable.get('LEANPLUM_FENNEC_BETA_APP_ID'),
+        leanplum_client_key=Variable.get('LEANPLUM_FENNEC_BETA_CLIENT_KEY'),
+        bq_project='moz-fx-data-shared-prod',
+        gcs_prefix='firefox/android/beta',
+        bq_dataset_id='firefox_android_beta_external',
+        table_prefix='leanplum',
+        dag=dag
+    )
+
+    fennec_release_export = leanplum.export(
+        task_id='fennec_release_export',
+        leanplum_app_id=Variable.get('LEANPLUM_FENNEC_RELEASE_APP_ID'),
+        leanplum_client_key=Variable.get('LEANPLUM_FENNEC_RELEASE_CLIENT_KEY'),
+        bq_project='moz-fx-data-shared-prod',
+        gcs_prefix='firefox/android/release',
+        bq_dataset_id='firefox_android_release_external',
+        table_prefix='leanplum',
+        dag=dag
+    )
+
+    firefox_preview_export = leanplum.export(
+        task_id='firefox_preview_export',
+        leanplum_app_id=Variable.get('LEANPLUM_FIREFOX_PREVIEW_APP_ID'),
+        leanplum_client_key=Variable.get('LEANPLUM_FIREFOX_PREVIEW_CLIENT_KEY'),
+        bq_project='moz-fx-data-shared-prod',
+        gcs_prefix='firefox-preview/android/release',
+        bq_dataset_id='firefox_preview_external',
+        table_prefix='leanplum',
+        dag=dag
+    )
+
+    firefox_ios_release_export = leanplum.export(
+        task_id='firefox_ios_release_export',
+        leanplum_app_id=Variable.get('LEANPLUM_FIREFOX_IOS_RELEASE_APP_ID'),
+        leanplum_client_key=Variable.get('LEANPLUM_FIREFOX_IOS_RELEASE_CLIENT_KEY'),
+        bq_project='moz-fx-data-shared-prod',
+        gcs_prefix='firefox/ios/release',
+        bq_dataset_id='firefox_ios_release_external',
         table_prefix='leanplum',
         dag=dag
     )
