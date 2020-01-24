e06e4aa30c4b7eccc1c4457acc72273d78a9905d
Anna Scholtz <anna@scholtzan.net>
2020-01-24 12:30:57 -0800

    Add queries for VR browser pings to copy_deduplicate

--- dags/copy_deduplicate.py
+++ dags/copy_deduplicate.py
@@ -154,6 +154,41 @@ with models.DAG(
         email=['telemetry-alerts@mozilla.com', 'jklukas@mozilla.com', 'pmcdermott@mozilla.com', 'dzielaski@mozilla.com', 'jmundi@mozilla.com'],
     )
 
+    # Daily and last seen views on top of VR browser pings.
+
+    vrbrowser_baseline_daily = bigquery_etl_query(
+        task_id='vrbrowser_baseline_daily',
+        project_id='moz-fx-data-shared-prod',
+        destination_table='baseline_daily_v1',
+        dataset_id='org_mozilla_vrbrowser_derived',
+        email=['telemetry-alerts@mozilla.com', 'jklukas@mozilla.com', 'ascholtz@mozilla.com'],
+    )
+
+    vrbrowser_metrics_daily = bigquery_etl_query(
+        task_id='vrbrowser_metrics_daily',
+        project_id='moz-fx-data-shared-prod',
+        destination_table='metrics_daily_v1',
+        dataset_id='org_mozilla_vrbrowser_derived',
+        email=['telemetry-alerts@mozilla.com', 'jklukas@mozilla.com', 'ascholtz@mozilla.com'],
+    )
+
+    vrbrowser_clients_daily = bigquery_etl_query(
+        task_id='vrbrowser_clients_daily',
+        project_id='moz-fx-data-shared-prod',
+        destination_table='clients_daily_v1',
+        dataset_id='org_mozilla_vrbrowser_derived',
+        email=['telemetry-alerts@mozilla.com', 'jklukas@mozilla.com', 'ascholtz@mozilla.com'],
+    )
+
+    vrbrowser_clients_last_seen = bigquery_etl_query(
+        task_id='vrbrowser_clients_last_seen',
+        project_id='moz-fx-data-shared-prod',
+        destination_table='clients_last_seen_v1',
+        dataset_id='org_mozilla_vrbrowser_derived',
+        depends_on_past=True,
+        email=['telemetry-alerts@mozilla.com', 'jklukas@mozilla.com', 'ascholtz@mozilla.com'],
+    )
+
     # Aggregated nondesktop tables and their dependency chains.
 
     firefox_nondesktop_exact_mau28 = bigquery_etl_query(
