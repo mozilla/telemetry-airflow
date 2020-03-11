bb263e56c56ca6be3892d78c3dcbaab301f5d4ef
Anna Scholtz <anna@scholtzan.net>
2020-03-11 15:17:37 -0700

    Fix owner in pensieve DAG

--- dags/pensieve.py
+++ dags/pensieve.py
@@ -4,41 +4,53 @@ from datetime import timedelta, datetime
 from operators.gcp_container_operator import GKEPodOperator
 
 default_args = {
-    'owner': ['ascholtz@mozilla.com', 'ssuh@mozilla.com', 'tdsmith@mozilla.com'],
-    'depends_on_past': False,
-    'start_date': datetime(2020, 3, 9),
-    'email_on_failure': True,
-    'email_on_retry': True,
-    'retries': 2,
-    'retry_delay': timedelta(minutes=30),
+    "owner": "ascholtz@mozilla.com",
+    "email": [
+        "ascholtz@mozilla.com",
+        "ssuh@mozilla.com",
+        "tdsmith@mozilla.com",
+    ],
+    "depends_on_past": False,
+    "start_date": datetime(2020, 3, 12),
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=30),
 }
 
-with DAG('pensieve',
-         default_args=default_args,
-         schedule_interval='0 1 * * *') as dag:
+with DAG(
+    "pensieve", default_args=default_args, schedule_interval="0 1 * * *"
+) as dag:
 
     # Built from repo https://github.com/mozilla/pensieve
-    pensieve_image = 'gcr.io/moz-fx-data-experiments/pensieve:latest'
+    pensieve_image = "gcr.io/moz-fx-data-experiments/pensieve:latest"
 
     pensieve = GKEPodOperator(
         task_id="pensieve",
-        name='pensieve',
+        name="pensieve",
         image=pensieve_image,
-        email=['ascholtz@mozilla.com', 'ssuh@mozilla.com', 'tdsmith@mozilla.com'],
-        arguments=['--date={{ds}}'],
-        dag=dag)
+        email=[
+            "ascholtz@mozilla.com",
+            "ssuh@mozilla.com",
+            "tdsmith@mozilla.com",
+        ],
+        arguments=["--date={{ds}}"],
+        dag=dag,
+    )
 
     wait_for_clients_daily_export = ExternalTaskSensor(
         task_id="wait_for_clients_daily_export",
         external_dag_id="main_summary",
         external_task_id="clients_daily_export",
-        dag=dag)
+        dag=dag,
+    )
 
     wait_for_main_summary_export = ExternalTaskSensor(
         task_id="wait_for_main_summary_export",
         external_dag_id="main_summary",
         external_task_id="main_summary_export",
-        dag=dag)
+        dag=dag,
+    )
 
     wait_for_search_clients_daily = ExternalTaskSensor(
         task_id="wait_for_search_clients_daily",
