19a93bfda73dd8ddc2c8e10492577b94a2c15885
Anna Scholtz <anna@scholtzan.net>
2020-03-27 08:46:32 -0700

    Schedule deviations tables copy

---
+++ dags/analysis.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.operators.sensors import ExternalTaskSensor
+from datetime import datetime, timedelta
+from utils.gcp import bigquery_etl_query
+
+default_args = {
+    "depends_on_past": False,
+    "start_date": datetime(2020, 3, 29),
+    "owner": "ascholtz@mozilla.com",
+    "email": [
+        "telemetry-alerts@mozilla.com",
+        "jmccrosky@mozilla.com",
+        "ascholtz@mozilla.com",
+    ],
+    "email_on_failure": True,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=30),
+}
+
+with DAG("analysis", default_args=default_args, schedule_interval="0 1 * * *") as dag:
+    deviations = bigquery_etl_query(
+        destination_table="deviations_v1",
+        dataset_id="analysis",
+        date_partition_parameter=None,
+        dag=dag,
+    )
