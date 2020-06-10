2c9ad8cd3915c49967e16ac07b20a5c651418fd7
Frank Bertsch <fbertsch@mozilla.com>
2020-06-10 13:58:51 -0400

    Update adjust_import for external tables

--- jobs/adjust_import.py
+++ jobs/adjust_import.py
@@ -1,9 +1,13 @@
 import click
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import isnull, col, lit, udf
+import logging
+from google.cloud import bigquery
 
 
 @click.command()
+@click.option('--pbkdf2', 'hash_type', flag_value='pbkdf2', default=True)
+@click.option('--bcrypt', 'hash_type', flag_value='bcrypt')
 @click.option("--salt", required=True)
 @click.option("--iterations", default=1000)
 @click.option("--klen", default=32)
@@ -12,6 +16,7 @@ from pyspark.sql.functions import isnull, col, lit, udf
 @click.option("--output_table", required=True)
 @click.option("--bucket", required=True)
 def main(
+    hash_type,
     salt,
     iterations,
     klen,
@@ -19,7 +24,23 @@ def main(
     input_table,
     output_table,
     bucket,
+    **kwargs
 ):
+    bq = bigquery.Client()
+    job_config = bigquery.QueryJobConfig()
+    tmp_table_name = input_table.replace(".", "_")
+    job_config.destination = bq.dataset("tmp", project=project).table(input_table.replace(".", "_"))
+    job_config.write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE
+
+    query = f"""
+	SELECT
+	    *
+	FROM
+	    `{project}.{input_table}`
+	"""
+    query_job = bq.query(query, job_config=job_config)
+    query_job.result()
+
     spark = (SparkSession
         .builder
         .appName("adjust_gps_hash")
@@ -33,11 +54,25 @@ def main(
             hashlib.pbkdf2_hmac('sha1', str.encode(msg), str.encode(salt), iterations, klen)
         ).decode()
 
+    @udf("string")
+    def bcrypt(msg, salt, iterations, klen):
+        import bcrypt
+        if msg is None:
+            return None
+        return bcrypt.hashpw(str.encode(msg), str.encode(salt)).decode('utf-8')
+
+    if hash_type == "pbkdf2":
+        hash_func = pbkdf2_sha1hmac
+    elif hash_type == "bcrypt":
+        hash_func = bcrypt
+
     (spark.read
-        .format("bigquery").option("table", f"{project}.{input_table}").load()
+        .format("bigquery").option("table", f"{project}.tmp.{tmp_table_name}").load()
+        .repartition(1000)
+        #.csv(input_table)
         .where(~isnull("gps_adid"))
         .withColumn("identifier",
-            pbkdf2_sha1hmac(
+            hash_func(
                 col("gps_adid"),
                 lit(salt),
                 lit(iterations),
