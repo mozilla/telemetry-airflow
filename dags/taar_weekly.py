3600e88e7a541937fe501ab8bdfdbb2b495377af
Victor Ng <victor@crankycoder.com>
2020-06-26 08:37:55 -0400

    Add comment pointing to taar etl github repo
    Added a comment to trace the source for the container image used
    for the GKEPodOperator

--- dags/taar_weekly.py
+++ dags/taar_weekly.py
@@ -22,6 +22,7 @@ TAAR_ETL_STORAGE_BUCKET = Variable.get("taar_etl_storage_bucket")
 TAAR_PROFILE_PROJECT_ID = Variable.get("taar_gcp_project_id")
 TAAR_DATAFLOW_SUBNETWORK = Variable.get("taar_dataflow_subnetwork")
 
+# This uses a circleci built docker image from github.com/mozilla/taar_gcp_etl
 TAAR_ETL_CONTAINER_IMAGE = (
     "gcr.io/moz-fx-data-airflow-prod-88e0/taar_gcp_etl:0.4.7"
 )
