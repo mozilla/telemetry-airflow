db12b38a04d9187ec17a4cbb8f4805dc88c7dc06
Benjamin Wu <benjaminwu124@gmail.com>
2020-07-03 19:14:24 -0400

    Update arguments

--- dags/glam_subdags/generate_query.py
+++ dags/glam_subdags/generate_query.py
@@ -3,22 +3,37 @@ from utils.gcp import gke_command
 
 def generate_and_run_query(task_id,
                            project_id,
-                           dataset_id,
+                           source_dataset_id,
                            sample_size,
-                           append_results,
+                           overwrite,
                            probe_type,
+                           destination_dataset_id=None,
                            process=None,
                            docker_image="mozilla/bigquery-etl:latest",
                            gcp_conn_id="google_cloud_derived_datasets",
                            **kwargs):
+    """
+    :param task_id:                     Airflow task id
+    :param project_id:                  GCP project to write to
+    :param source_dataset_id:           Bigquery dataset to read from in queries
+    :param sample_size:                 Value to use for windows release client sampling
+    :param overwrite:                   Overwrite the destination table
+    :param probe_type:                  Probe type to generate query
+    :param destination_dataset_id:      Bigquery dataset to write results to.  Defaults to source_dataset_id
+    :param process:                     Process to filter probes for.  Gets all processes by default.
+    :param docker_image:                Docker image
+    :param gcp_conn_id:                 Airflow GCP connection
+    """
+    if destination_dataset_id is None:
+        destination_dataset_id = source_dataset_id
     env_vars = {
         "PROJECT": project_id,
-        "PROD_DATASET": dataset_id,
-        "DATASET": dataset_id,
+        "PROD_DATASET": source_dataset_id,
+        "DATASET": destination_dataset_id,
         "SUBMISSION_DATE": "{{ ds }}",
         "RUN_QUERY": "t",
     }
-    if append_results:
+    if not overwrite:
         env_vars["APPEND"] = "t"
 
     command = [
@@ -36,5 +51,5 @@ def generate_and_run_query(task_id,
         command=command,
         docker_image=docker_image,
         gcp_conn_id=gcp_conn_id,
-        **kwargs,
+        **kwargs
     )
