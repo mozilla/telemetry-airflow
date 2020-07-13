e06c02a8e800da1d4b09fca378bb28102ad20fa5
Frank Bertsch <fbertsch@mozilla.com>
2020-07-13 10:16:41 -0400

    Truncate existing results for ltv

--- dags/ltv.py
+++ dags/ltv.py
@@ -116,6 +116,7 @@ ltv_revenue_join=BigQueryOperator(
     use_legacy_sql=False,
     default_args=default_args,
     time_partitioning={"type": "DAY", "field": "submission_date"},
+    write_disposition='WRITE_TRUNCATE',
 )
 
 response = urlopen('/'.join([
