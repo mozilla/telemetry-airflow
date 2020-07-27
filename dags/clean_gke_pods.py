53d592537963af4ebd8580eb8e793b96f1bd28e1
Harold Woo <hwoo@mozilla.com>
2020-07-27 11:47:29 -0700

    Reduce retention of gke pods

--- dags/clean_gke_pods.py
+++ dags/clean_gke_pods.py
@@ -26,7 +26,7 @@ docker_args = [
     '--project', 'moz-fx-data-derived-datasets',
     '--gke-cluster', gke_cluster_name,
     '--region', gke_location,
-    '--retention-days', '4'
+    '--retention-days', '2'
 ]
 
 clean_gke_pods = GKEPodOperator(
