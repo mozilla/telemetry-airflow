e5de501d8063cc366e9bb546135f3866136cb47d
Frank Bertsch <fbertsch@mozilla.com>
2020-09-10 17:18:28 -0400

    Fix date format

--- dags/fxa_export_to_amplitude.py
+++ dags/fxa_export_to_amplitude.py
@@ -50,7 +50,7 @@ with models.DAG(
         destination_table='fxa_amplitude_export_v1',
         dataset_id='firefox_accounts_derived',
         depends_on_past=True,
-        allow_field_addition_on_date="20200909"
+        allow_field_addition_on_date="2020-09-09"
     )
 
     task_id = 'fxa_amplitude_export_task'
