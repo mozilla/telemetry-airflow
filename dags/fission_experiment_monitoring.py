ee22cffc3c21231de84123354bd452ec9d47cb1f
Arkadiusz Komarzewski <akomarzewski@mozilla.com>
2021-02-05 14:09:54 +0100

    Fission experiment monitoring: Don't send emails on sensor retry

--- dags/fission_experiment_monitoring.py
+++ dags/fission_experiment_monitoring.py
@@ -32,6 +32,7 @@ with models.DAG(
         execution_delta=datetime.timedelta(hours=1),
         mode="reschedule",
         pool="DATA_ENG_EXTERNALTASKSENSOR",
+        email_on_retry=False,
         dag=dag,
     )
 
@@ -50,6 +51,7 @@ with models.DAG(
         execution_delta=datetime.timedelta(hours=1),
         mode="reschedule",
         pool="DATA_ENG_EXTERNALTASKSENSOR",
+        email_on_retry=False,
         dag=dag,
     )
 
