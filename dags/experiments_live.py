0871efe51adc13b826f51e74a7f58da26bdf0f4a
Anna Scholtz <anna@scholtzan.net>
2021-03-08 15:36:40 -0800

    Remove tasks for running experiment search metrics queries

--- dags/experiments_live.py
+++ dags/experiments_live.py
@@ -22,28 +22,16 @@ with DAG('experiments_live',
          max_active_runs=1,
          schedule_interval="*/5 * * * *") as dag:
 
-    docker_image = "mozilla/bigquery-etl:latest"
-
-    experiment_search_aggregates_recents = bigquery_etl_query(
-        task_id="experiment_search_aggregates_recents",
-        destination_table="experiment_search_aggregates_recents_v1",
-        dataset_id="telemetry_derived",
-        project_id="moz-fx-data-shared-prod",
-        owner="ascholtz@mozilla.com",
-        email=["ascholtz@mozilla.com", "telemetry-alerts@mozilla.com"],
-        date_partition_parameter=None,
-        parameters=["submission_timestamp:TIMESTAMP:{{ts}}"],
-        dag=dag,
-        is_delete_operator_pod=True,
-    )
-
-
-    # list of datasets to execute query for and export
-    experiment_enrollment_datasets = [
+    # list of datasets to export data to GCS
+    experiment_datasets = [
         "moz-fx-data-shared-prod.telemetry_derived.experiment_enrollment_other_events_overall_v1",
         "moz-fx-data-shared-prod.telemetry_derived.experiment_enrollment_cumulative_population_estimate_v1",
         "moz-fx-data-shared-prod.telemetry_derived.experiment_enrollment_overall_v1",
         "moz-fx-data-shared-prod.telemetry_derived.experiment_unenrollment_overall_v1",
+        "moz-fx-data-shared-prod.telemetry_derived.experiment_cumulative_ad_clicks_v1",
+        "moz-fx-data-shared-prod.telemetry_derived.experiment_cumulative_search_count_v1",
+        "moz-fx-data-shared-prod.telemetry_derived.experiment_cumulative_search_with_ads_count_v1",
+        "moz-fx-data-shared-prod.telemetry.experiment_enrollment_daily_active_population"
     ]
 
     experiment_enrollment_export = gke_command(
@@ -51,55 +39,7 @@ with DAG('experiments_live',
         command=[
             "python", "experiments_monitoring_data_export/export.py",
             "--datasets",
-        ] + experiment_enrollment_datasets,
+        ] + experiment_datasets,
         docker_image="gcr.io/moz-fx-data-airflow-prod-88e0/experiments-monitoring-data-export_docker_etl:latest",
         dag=dag,
-    )
-
-    # search data export
-    # todo: use materialized views
-
-    experiment_search_datasets = [
-        "moz-fx-data-shared-prod.telemetry_derived.experiment_cumulative_ad_clicks_v1",
-        "moz-fx-data-shared-prod.telemetry_derived.experiment_cumulative_search_count_v1",
-        "moz-fx-data-shared-prod.telemetry_derived.experiment_cumulative_search_with_ads_count_v1"
-    ]
-
-    export_search_monitoring_data = gke_command(
-        task_id="export_enrollments_monitoring_data",
-        command=[
-            "python",
-            "script/experiments/export_experiment_monitoring_data.py",
-            "--datasets"
-        ] + experiment_search_datasets,
-        docker_image=docker_image,
-        is_delete_operator_pod=True,
-    )
-
-    for dataset in experiment_search_datasets:
-        task_id = dataset.split(".")[-1]
-
-        query_etl = bigquery_etl_query(
-            task_id=task_id,
-            destination_table=task_id,
-            dataset_id="telemetry_derived",
-            project_id="moz-fx-data-shared-prod",
-            owner="ascholtz@mozilla.com",
-            email=["ascholtz@mozilla.com", "telemetry-alerts@mozilla.com"],
-            date_partition_parameter=None,
-            dag=dag,
-            is_delete_operator_pod=True,
-        )
-
-        query_etl.set_upstream(experiment_search_aggregates_recents)
-        export_search_monitoring_data.set_upstream(query_etl)
-
-    export_daily_active_population_monitoring_data = gke_command(
-        task_id=f"export_experiment_enrollment_daily_active_population",
-        command=[
-            "python",
-            "script/experiments/export_experiment_monitoring_data.py",
-            "--datasets", "moz-fx-data-shared-prod.telemetry.experiment_enrollment_daily_active_population"
-        ],
-        docker_image=docker_image
     )
