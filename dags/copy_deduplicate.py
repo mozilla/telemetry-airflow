mozilla/telemetry-airflow

commit a73599cbe0baf256a032577e407afaabb7a3b7e7

Run baseline backfill without generating queries (#1317)
Reverts https://github.com/mozilla/telemetry-airflow/pull/1312

Relies on https://github.com/mozilla/bigquery-etl/pull/2062

dags/copy_deduplicate.py
@@ -157,43 +157,40 @@ with models.DAG(
 
     copy_deduplicate_all >> telemetry_derived__core_clients_first_seen__v1
 
+    baseline_args = [
+        "--project-id=moz-fx-data-shared-prod",
+        "--start_date={{ ds }}",
+        "--end_date={{ ds }}"
+    ]
+
     baseline_clients_first_seen = gke_command(
         task_id="baseline_clients_first_seen",
-        command = [
-            "/bin/bash",
-            "-c",
-            "./script/generate_sql " + 
-            "--target-project moz-fx-data-shared-prod " + 
-            "&& bqetl query backfill *.baseline_clients_first_seen_v1 " +
-            "--project-id=moz-fx-data-shared-prod " + 
-            "--start_date={{ ds }} --end_date={{ ds }} --no-partition"
-        ],
+        command=[
+            "bqetl",
+            "query",
+            "backfill",
+            "*.baseline_clients_first_seen_v1",
+        ] + baseline_args,
         docker_image="mozilla/bigquery-etl:latest",
     )
     baseline_clients_daily = gke_command(
         task_id="baseline_clients_daily",
-        command = [
-            "/bin/bash",
-            "-c",
-            "./script/generate_sql " + 
-            "--target-project moz-fx-data-shared-prod " + 
-            "&& bqetl query backfill *.baseline_clients_daily_v1 " +
-            "--project-id=moz-fx-data-shared-prod " + 
-            "--start_date={{ ds }} --end_date={{ ds }}"
-        ],
+        command=[
+            "bqetl",
+            "query",
+            "backfill",
+            "*.baseline_clients_daily_v1",
+        ] + baseline_args,
         docker_image="mozilla/bigquery-etl:latest",
     )
     baseline_clients_last_seen = gke_command(
         task_id="baseline_clients_last_seen",
-        command = [
-            "/bin/bash",
-            "-c",
-            "./script/generate_sql " + 
-            "--target-project moz-fx-data-shared-prod " + 
-            "&& bqetl query backfill *.baseline_clients_last_seen_v1 " +
-            "--project-id=moz-fx-data-shared-prod " + 
-            "--start_date={{ ds }} --end_date={{ ds }}"
-        ],
+        command=[
+            "bqetl",
+            "query",
+            "backfill",
+            "*.baseline_clients_last_seen_v1",
+        ] + baseline_args,
         depends_on_past=True,
         docker_image="mozilla/bigquery-etl:latest",
     )

commit 2df370ab342b8110ba7b3aa0c558c37943040409

Bug 1712696 Run baseline_clients_first_seen with --no-partition
See https://bugzilla.mozilla.org/show_bug.cgi?id=1712696

I introduced this regression in
https://github.com/mozilla/telemetry-airflow/pull/1317
which @scholtzan had previously fixed in
https://github.com/mozilla/telemetry-airflow/pull/1313

dags/copy_deduplicate.py
@@ -170,6 +170,7 @@ with models.DAG(
             "query",
             "backfill",
             "*.baseline_clients_first_seen_v1",
+            "--no-partition",
         ] + baseline_args,
         docker_image="mozilla/bigquery-etl:latest",
     )