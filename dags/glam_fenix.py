bb81388b44757d2a56921d51b5840d35166e07ff
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-07-08 14:24:49 -0700

    Bug 1719188 - Set max_active_runs to 1 (#1333)
    * Set max_active_runs to 1
    
    * Set max active runs in dag parameters

--- dags/glam_fenix.py
+++ dags/glam_fenix.py
@@ -48,7 +48,12 @@ LOGICAL_MAPPING = {
     "org_mozilla_fenix_glam_release": ["org_mozilla_firefox"],
 }
 
-dag = DAG("glam_fenix", default_args=default_args, schedule_interval="0 2 * * *")
+dag = DAG(
+    "glam_fenix",
+    default_args=default_args,
+    max_active_runs=1,
+    schedule_interval="0 2 * * *",
+)
 
 wait_for_copy_deduplicate = ExternalTaskCompletedSensor(
     task_id="wait_for_copy_deduplicate",
