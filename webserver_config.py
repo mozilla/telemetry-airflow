2be0cc34f77fed7f42aab6c92bf940bb4bd66415
Harold Woo <hwoo@mozilla.com>
2021-09-14 09:22:39 -0700

    [DSRE-159] Build new wtmo 1.10.15 container for addressing IAP and google oauth issue (bug 1728197)

--- webserver_config.py
+++ webserver_config.py
@@ -70,14 +70,14 @@ OAUTH_PROVIDERS = [{
     'token_key':'access_token',
     'icon':'fa-google',
         'remote_app': {
-            'base_url':'https://www.googleapis.com/oauth2/v2/',
-            'request_token_params':{
+            'api_base_url':'https://www.googleapis.com/oauth2/v2/',
+            'client_kwargs':{
                 'scope': 'email profile'
             },
             'access_token_url':'https://accounts.google.com/o/oauth2/token',
             'authorize_url':'https://accounts.google.com/o/oauth2/auth',
             'request_token_url': None,
-            'consumer_key': GOOGLE_KEY,
-            'consumer_secret': GOOGLE_SECRET,
+            'client_id': GOOGLE_KEY,
+            'client_secret': GOOGLE_SECRET,
         }
 }]
