16c0f9bb27ed57f3743594a630899a807b31a078
Harold Woo <hwoo@mozilla.com>
2021-10-26 11:19:36 -0700

    Revert celery config change.

--- dags/moz_celery_config.py
+++
@@ -1,6 +0,0 @@
-from airflow.config_templates.default_celery import DEFAULT_CELERY_CONFIG
-
-CELERY_CONFIG = dict(DEFAULT_CELERY_CONFIG,
-                     **{"worker_log_format": "[%(asctime)s] %(levelname)s - %(processName)s - %(message)s",
-                        "worker_redirect_stdouts_level": "INFO"
-                     })
