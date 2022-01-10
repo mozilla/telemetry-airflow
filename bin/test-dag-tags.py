88d31f2e012ac39136294a3f74df5c5d2b067670
= <=>
2022-01-10 19:41:45 +0100

    trying out dag tagg validation through parsing

--- bin/test-dag-tags.py
+++ bin/test-dag-tags.py
@@ -28,23 +28,62 @@ def retrieve_existing_airflow_dags():
     return dags
 
 
+import glob, os
+import re
+
+# for file in glob.glob("*.txt"):
+#     print(file)
+
+def get_dag_tags(dag_dir: str):
+    tag_mapping = {
+        "Tag.ImpactTier.tier_1": "impact/tier_1",
+        "Tag.ImpactTier.tier_2": "impact/tier_2",
+        "Tag.ImpactTier.tier_3": "impact/tier_3",
+        "repo/telemetry-airflow": "impact/tier_3",
+        "repo/bigquery-etl": "repo/bigquery-etl",
+    }
+
+
+    dags = defaultdict(list)
+
+    for file in glob.glob("dag_dir/*.py"):
+        if "init" in file:
+            continue
+
+        with open(file) as _dag:
+            try:
+                match = re.search("tags\s=\s\[(.+)]", _dag.read()).group(1)
+                tags = match.strip().translate({44: "", 34: "",}).split(" ")
+
+            except AttributeError:
+                print("DAG file: %s appears to be missing tags.")
+                tags = list()
+
+            dags[file.split("/")[-1]] = [tag_mapping[tag] for tag in tags]
+
+    return dags
+
+
 
 if __name__ == "__main__":
     tag_errors = 0
 
     # bin/run script waits for db and reddis to be up before interacting with webserver container in test-parse script
-    dags = retrieve_existing_airflow_dags()
+    # dags = retrieve_existing_airflow_dags()
+    dag_directory = "dags"
+    dags = get_dag_tags(dag_directory)
 
-    for dag_id, tags in dags.items():
+    for file_name, tags in dags.items():
         tag_categories = [category.split("/")[0] for category in tags]
+
 
         if not all(req_tag in tag_categories for req_tag in REQUIRED_TAG_TYPES):
             tag_errors += 1
-            print('%s is missing a required tag. Required tags include: %s. Please refer to: https://mozilla.github.io/bigquery-etl/reference/airflow_tags/ for more information.' % (dag_id, REQUIRED_TAG_TYPES))
+            print('%s is missing a required tag. Required tags include: %s. Please refer to: https://mozilla.github.io/bigquery-etl/reference/airflow_tags/ for more information.' % (file_name, REQUIRED_TAG_TYPES))
 
         if any(tag not in VALID_TAGS for tag in tags):
             tag_errors += 1
-            print('DAG: %s contains an invalid tag. Tags specified: %s, valid tags: %s.' % (dag_id, tags, VALID_TAGS))
+            print('DAG file: %s contains an invalid tag. Tags specified: %s, valid tags: %s.' % (file_name, tags, VALID_TAGS))
 
     if tag_errors:
         raise DagValidationError("DAG tags validation failed.")
