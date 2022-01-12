66b1d9c17b2dd0e29275d7937e14c4cee1a5db84
= <=>
2022-01-12 16:22:56 +0100

    Using SQL approach for validation, added extra logging and clean up

--- bin/test-dag-tags.py
+++ bin/test-dag-tags.py
@@ -11,71 +11,46 @@ class DagValidationError(Exception):
     pass
 
 
-def retrieve_existing_airflow_dags():
-    output = subprocess.run(
-        "docker-compose exec web mysql -Ns -h db -u root -psecret airflow -e 'SELECT dag_id, name FROM dag_tag;'",
+def retrieve_existing_airflow_dags_from_db(pswd):
+    mask_string = "###"
+    shell_cmd = "docker-compose exec web mysql -Ns -h db -u root -p{pswd} airflow -e 'SELECT dag_id, name FROM dag_tag;'"
+
+    print("Executing command: %s" % (shell_cmd.format(pswd=mask_string)))
+    cmd_output = subprocess.run(
+        shell_cmd.format(pswd=pswd),
         shell=True,
-        capture_output=True
-    ).stdout.decode('ascii').strip()
+        capture_output=True,
+        text=True,
+    )
+    try:
+        cmd_output.check_returncode()
+    except subprocess.CalledProcessError as _err:
+        print(cmd_output.stdout)
+        print(cmd_output.stderr)
+        raise _err
+
+    print("Command executed successfully, processing output...")
+    processed_cmd_output = cmd_output.stdout.strip().replace("\r", "").split("\n")
 
     dags = defaultdict(list)
 
-    for dag in output.replace("\r", "").split("\n"):
+    for dag in processed_cmd_output:
         dag_name, tag_name = dag.split("\t")
-
         dags[dag_name] = dags[dag_name] + [tag_name]
 
-    return dags
-
-
-import glob, os
-import re
-
-# for file in glob.glob("*.txt"):
-#     print(file)
-
-def get_dag_tags(dag_dir: str):
-    tag_mapping = {
-        "Tag.ImpactTier.tier_1": "impact/tier_1",
-        "Tag.ImpactTier.tier_2": "impact/tier_2",
-        "Tag.ImpactTier.tier_3": "impact/tier_3",
-        "repo/telemetry-airflow": "impact/tier_3",
-        "repo/bigquery-etl": "repo/bigquery-etl",
-    }
-
-
-    dags = defaultdict(list)
-
-    for file in glob.glob("dag_dir/*.py"):
-        if "init" in file:
-            continue
-
-        with open(file) as _dag:
-            try:
-                match = re.search("tags\s=\s\[(.+)]", _dag.read()).group(1)
-                tags = match.strip().translate({44: "", 34: "",}).split(" ")
-
-            except AttributeError:
-                print("DAG file: %s appears to be missing tags.")
-                tags = list()
-
-            dags[file.split("/")[-1]] = [tag_mapping[tag] for tag in tags]
+    print("Number of DAGs found: %s" % (len(dags)))
 
     return dags
-
 
 
 if __name__ == "__main__":
+    # Assumes the web and db containers are already running
     tag_errors = 0
 
-    # bin/run script waits for db and reddis to be up before interacting with webserver container in test-parse script
-    # dags = retrieve_existing_airflow_dags()
-    dag_directory = "dags"
-    dags = get_dag_tags(dag_directory)
+    dags = retrieve_existing_airflow_dags_from_db("secret")
 
     for file_name, tags in dags.items():
         tag_categories = [category.split("/")[0] for category in tags]
-
 
         if not all(req_tag in tag_categories for req_tag in REQUIRED_TAG_TYPES):
             tag_errors += 1
@@ -88,4 +63,4 @@ if __name__ == "__main__":
     if tag_errors:
         raise DagValidationError("DAG tags validation failed.")
 
-    print("DAG tags validation successful.")
+    print("DAG tags validation for %s DAGs successful." % (len(dags)))
