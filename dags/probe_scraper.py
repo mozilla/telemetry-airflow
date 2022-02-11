84329bda794004ec2f20548d5c724f278408f76c
Chris H-C <chutten@mozilla.com>
2022-02-11 15:47:16 -0500

    bug 1696907 - Give probe_scraper.runner a copy of the BMO API key
    The FOG checks that the runner kicks off may need it to file bugs for expiring
    metrics.

--- dags/probe_scraper.py
+++ dags/probe_scraper.py
@@ -71,6 +71,7 @@ with DAG('probe_scraper',
         '--output-bucket', 'net-mozaws-prod-us-west-2-data-pitmo',
         '--cache-bucket', 'telemetry-airflow-cache',
         '--env', 'prod'
+        "--bugzilla-api-key", "{{ var.value.bugzilla_probe_expiry_bot_api_key }}"
     ]
 
     # Cluster autoscaling works on pod resource requests, instead of usage
