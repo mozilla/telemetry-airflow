4a8c5a66f43cdb3e408a0796bb306af3a8341162
Jeff Klukas <jeff@klukas.net>
2022-04-06 15:58:20 -0400

    Bug 1763116 Separate saved_session copy_deduplicate task (#1485)
    Follow-up to #1482 to be logically consistent.

--- dags/copy_deduplicate.py
+++ dags/copy_deduplicate.py
@@ -81,11 +81,14 @@ with models.DAG(
         except_tables=[
             "telemetry_live.main_v4",
             "telemetry_live.event_v4",
-            "telemetry_live.first_shutdown_v4"],
+            "telemetry_live.first_shutdown_v4",
+            "telemetry_live.saved_session_v4"],
         node_selectors={"nodepool": "highmem"},
         resources=resources,
     )
 
+    # We split out main ping since it's the highest volume and has a distinct
+    # set of downstream dependencies.
     copy_deduplicate_main_ping = bigquery_etl_copy_deduplicate(
         task_id="copy_deduplicate_main_ping",
         target_project_id="moz-fx-data-shared-prod",
@@ -102,16 +105,8 @@ with models.DAG(
         ],
     )
 
-    copy_deduplicate_event_ping = bigquery_etl_copy_deduplicate(
-        task_id="copy_deduplicate_event_ping",
-        target_project_id="moz-fx-data-shared-prod",
-        billing_projects=("moz-fx-data-shared-prod",),
-        only_tables=["telemetry_live.event_v4"],
-        priority_weight=100,
-        parallelism=1,
-        owner="jklukas@mozilla.com",
-    )
-
+    # We also separate out variant pings that share the main ping schema since these
+    # ultrawide tables can sometimes have unique performance problems.
     copy_deduplicate_first_shutdown_ping = bigquery_etl_copy_deduplicate(
         task_id="copy_deduplicate_first_shutdown_ping",
         target_project_id="moz-fx-data-shared-prod",
@@ -122,7 +117,27 @@ with models.DAG(
         owner="jklukas@mozilla.com",
     )
 
+    copy_deduplicate_saved_session_ping = bigquery_etl_copy_deduplicate(
+        task_id="copy_deduplicate_saved_session_ping",
+        target_project_id="moz-fx-data-shared-prod",
+        billing_projects=("moz-fx-data-shared-prod",),
+        only_tables=["telemetry_live.saved_session_v4"],
+        priority_weight=50,
+        parallelism=1,
+        owner="jklukas@mozilla.com",
+    )
+
     # Events.
+
+    copy_deduplicate_event_ping = bigquery_etl_copy_deduplicate(
+        task_id="copy_deduplicate_event_ping",
+        target_project_id="moz-fx-data-shared-prod",
+        billing_projects=("moz-fx-data-shared-prod",),
+        only_tables=["telemetry_live.event_v4"],
+        priority_weight=100,
+        parallelism=1,
+        owner="jklukas@mozilla.com",
+    )
 
     event_events = bigquery_etl_query(
         task_id="event_events",
