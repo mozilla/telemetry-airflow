484d8363ce862b556e5f7e860c4d83762c62bba5
Alexander Nicholson <anicholson@mozilla.com>
2022-04-06 11:13:35 -0400

    Added DAG for static publish script (#1481)

---
+++ dags/publish_bqetl_static.py
@@ -0,0 +1,54 @@
+"""
+Daily deployment of static bigquery-etl data to various projects.
+
+See the publish script [here](https://github.com/mozilla/bigquery-etl/blob/main/bigquery_etl/publish_static.py).
+"""
+
+from airflow import DAG
+from datetime import timedelta, datetime
+from utils.gcp import gke_command
+from utils.tags import Tag
+
+IMAGE = "gcr.io/moz-fx-data-airflow-prod-88e0/bigquery-etl:latest"
+
+default_args = {
+    "owner": "anicholson@mozilla.com",
+    "email": [
+        "telemetry-alerts@mozilla.com",
+        "anicholson@mozilla.com",
+    ],
+    "depends_on_past": False,
+    "start_date": datetime(2022, 4, 4),
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=30),
+}
+
+tags = [Tag.ImpactTier.tier_2]
+
+with DAG(
+    "publish_bqetl_static",
+    default_args=default_args,
+    schedule_interval="@daily",
+    doc_md=__doc__,
+    tags=tags,
+) as dag:
+
+    publish_static_mozdata = gke_command(
+        task_id="publish_static_mozdata",
+        command=[
+            "script/publish_static",
+            "--project_id", "mozdata"
+        ],
+        docker_image=IMAGE,
+    )
+
+    publish_static_shared_prod = gke_command(
+        task_id="publish_static_shared_prod",
+        command=[
+            "script/publish_static",
+            "--project_id", "moz-fx-data-shared-prod"
+        ],
+        docker_image=IMAGE,
+    )
