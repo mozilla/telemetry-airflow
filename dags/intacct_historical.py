b320d75cb6a2dfc9753856e9fded96ea6c2f68af
Sean Rose <srose@mozilla.com>
2022-04-21 14:52:47 -0700

    Use `execution_timeout` for FivetranSensor in `poke` mode.
    `timeout` only applies to sensors in `reschedule` mode.

--- dags/intacct_historical.py
+++ dags/intacct_historical.py
@@ -80,7 +80,7 @@ with DAG(
                 fivetran_conn_id='fivetran',
                 connector_id=connector_id,
                 poke_interval=5,
-                timeout=3*60*60,  # Timeout of 3 hours
+                execution_timeout=timedelta(hours=3),
                 priority_weight=fivetran_sync_start.priority_weight + 1,
             )
 
