eb2bbc1b67e15d2407ed83e07c967532a70e15ac
evgeny pavlov <epavlov@mozilla.com>
2022-06-27 15:24:06 -0700

    Bump taar image version

--- dags/taar_weekly.py
+++ dags/taar_weekly.py
@@ -27,7 +27,7 @@ TAAR_DATAFLOW_SERVICE_ACCOUNT = Variable.get("taar_dataflow_service_account_emai
 
 # This uses a circleci built docker image from github.com/mozilla/taar_gcp_etl
 TAAR_ETL_CONTAINER_IMAGE = (
-    "gcr.io/moz-fx-data-airflow-prod-88e0/taar_gcp_etl:0.6.4"
+    "gcr.io/moz-fx-data-airflow-prod-88e0/taar_gcp_etl:0.6.5"
 )
 DELETE_DAYS = 29
 
