48b62bb2c87e23f858f90e5a100303b12edf09b1
skahmann3 <16420065+skahmann3@users.noreply.github.com>
2022-08-15 10:30:02 -0700

    Remove outdated forecasts.
    Expected forecast output:
    1.  QDAU for desktop
    2. DAU for mobile
    
    No CDOU or QCDOU forecasts.

--- dags/kpi_forecasting.py
+++ dags/kpi_forecasting.py
@@ -2,11 +2,11 @@
 See [kpi-forecasting in the docker-etl repository]
 (https://github.com/mozilla/docker-etl/blob/main/jobs/kpi-forecasting).
 
-This DAG runs the forecast for year-end KPI values for Desktop QCDOU, Mobile CDOU and Pocket.
-The forecast is only updated once per week since values aren't expected to change significantly
-day-to-day. The output powers KPI dashboards.
+This DAG runs the forecast for year-end KPI values for Desktop QDAU and Mobile DAU. These forecasts can then be aggregated to calculate Desktop QCDOU and Mobile CDOU.
+The forecast is only updated once per week since values aren't expected to change significantly day-to-day. 
+The output powers KPI dashboards and monthly revenue forecasts.
 
-This DAG is low priority.
+This DAG is high priority for week 1 of the month. This DAG is low priority for weeks 2-4 of each month. 
 """
 
 from datetime import datetime, timedelta
@@ -31,26 +31,7 @@ default_args = {
 tags = [Tag.ImpactTier.tier_3]
 
 with DAG("kpi_forecasting", default_args=default_args, schedule_interval="0 4 * * SAT", doc_md=__doc__, tags=tags,) as dag:
-    kpi_forecasting_mobile = gke_command(
-        task_id="kpi_forecasting_mobile",
-        command=[
-            "python", "kpi-forecasting/kpi_forecasting.py",
-            "-c",
-        ] + ["kpi-forecasting/yaml/mobile.yaml"],
-        docker_image="gcr.io/moz-fx-data-airflow-prod-88e0/kpi-forecasting_docker_etl:latest",
-        dag=dag,
-    )
-
-    kpi_forecasting_desktop = gke_command(
-        task_id="kpi_forecasting_desktop",
-        command=[
-            "python", "kpi-forecasting/kpi_forecasting.py",
-            "-c",
-        ] + ["kpi-forecasting/yaml/desktop.yaml"],
-        docker_image="gcr.io/moz-fx-data-airflow-prod-88e0/kpi-forecasting_docker_etl:latest",
-        dag=dag,
-    )
-
+    
     kpi_forecasting_desktop_non_cumulative = gke_command(
         task_id="kpi_forecasting_desktop_non_cumulative",
         command=[
@@ -71,33 +52,6 @@ with DAG("kpi_forecasting", default_args=default_args, schedule_interval="0 4 * 
         dag=dag,
     )
 
-    wait_for_desktop_usage = ExternalTaskSensor(
-        task_id="wait_for_desktop_usage",
-        external_dag_id="bqetl_main_summary",
-        external_task_id="telemetry_derived__firefox_desktop_usage__v1",
-        execution_delta=timedelta(hours=2),
-        check_existence=True,
-        mode="reschedule",
-        allowed_states=ALLOWED_STATES,
-        failed_states=FAILED_STATES,
-        pool="DATA_ENG_EXTERNALTASKSENSOR",
-        email_on_retry=False,
-        dag=dag,
-    )
-    wait_for_mobile_usage = ExternalTaskSensor(
-        task_id="wait_for_mobile_usage",
-        external_dag_id="bqetl_nondesktop",
-        external_task_id="telemetry_derived__mobile_usage__v1",
-        execution_delta=timedelta(hours=1),
-        check_existence=True,
-        mode="reschedule",
-        allowed_states=ALLOWED_STATES,
-        failed_states=FAILED_STATES,
-        pool="DATA_ENG_EXTERNALTASKSENSOR",
-        email_on_retry=False,
-        dag=dag,
-    )
-
     wait_for_unified_metrics = ExternalTaskSensor(
         task_id="wait_for_unified_metrics",
         external_dag_id="bqetl_unified",
@@ -112,7 +66,5 @@ with DAG("kpi_forecasting", default_args=default_args, schedule_interval="0 4 * 
         dag=dag,
     )
 
-    wait_for_mobile_usage >> kpi_forecasting_mobile
-    wait_for_unified_metrics >> kpi_forecasting_desktop
     wait_for_unified_metrics >> kpi_forecasting_desktop_non_cumulative
     wait_for_unified_metrics >> kpi_forecasting_mobile_non_cumulative
