66c5f1d6fb83a36f9628bdc855d10e8b7dca146f
mikaeld <mducharme@mozilla.com>
2022-12-01 15:51:40 -0500

    add DAGs with errors to test CI

---
+++ dags/import_error_dag.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    'circular_dag',
+    # These args will get passed on to each operator
+    # You can override them on a per-task basis during operator initialization
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    tags=['example'],
+) as dag:
+    import fake_package
+    # t1, t2 and t3 are examples of tasks created by instantiating operators
+    t1 = EmptyOperator()
+
+    t2 = EmptyOperator()
+
+    t3 = EmptyOperator()
+
+    t1 >> [t2, t3]
