52b34b153cb34c6a1bd96a502e21fd0a75da9506
Daniel Thorn <dthorn@mozilla.com>
2022-12-06 15:47:15 -0800

    Add airflow task for deploying views from bigquery-etl

---
+++ dags/bqetl_views.py
@@ -0,0 +1,41 @@
+"""
+Nightly deploy of bigquery etl views.
+"""
+
+from airflow import DAG
+from datetime import timedelta, datetime
+from utils.gcp import gke_command
+from utils.tags import Tag
+
+default_args = {
+    "owner": "ascholtz@mozilla.com",
+    "email": [
+        "ascholtz@mozilla.com",
+        "dthorn@mozilla.com",
+        "telemetry-alerts@mozilla.com",
+    ],
+    "depends_on_past": False,
+    "start_date": datetime(2022, 12, 6),
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=30),
+}
+
+tags = [Tag.ImpactTier.tier_1]
+
+with DAG("bqetl_views", default_args=default_args, schedule_interval="@daily", doc_md=__doc__, tags=tags,) as dag:
+    docker_image = "gcr.io/moz-fx-data-airflow-prod-88e0/bigquery-etl:latest"
+
+    publish_views = gke_command(
+        task_id="publish_views",
+        cmds=["bash", "-c"],
+        command=[
+            "script/bqetl generate all && "
+            "script/bqetl view publish --target-project=moz-fx-data-shared-prod && "
+            "script/bqetl view publish --target-project=mozdata --user-facing-only && "
+            "script/publish_public_data_views --target-project=moz-fx-data-shared-prod && "
+            "script/publish_public_data_views --target-project=mozdata"
+        ],
+        docker_image=docker_image,
+    )
