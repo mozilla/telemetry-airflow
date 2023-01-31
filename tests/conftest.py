7e7aaa2a6e225edb939a00f602999ec22a044f2a
mikaeld <mducharme@mozilla.com>
2023-01-30 23:36:01 -0500

    add unit tests for dag validity

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,29 @@
 import pathlib
+import sys
+import warnings
+
+import pytest
+from airflow.models import DagBag
+from dotenv import load_dotenv
 
 # get absolute project directory path no matter the environment
-PROJECT_DIR = pathlib.Path(".").resolve().parent
+PROJECT_DIR = pathlib.Path(__file__).resolve().parent.parent
+
+
+@pytest.fixture(scope="session")
+def get_dag_bag() -> DagBag:
+    # load dev connection and variables
+    assert load_dotenv(PROJECT_DIR / "resources" / "dev_connections.env")
+    assert load_dotenv(PROJECT_DIR / "resources" / "dev_variables.env")
+
+    # Replicate Airflow adding dags, plugins folders in system path at runtime
+    sys.path.insert(0, str(PROJECT_DIR))
+    sys.path.insert(1, str(PROJECT_DIR / "dags"))
+    sys.path.insert(2, str(PROJECT_DIR / "plugins"))
+
+    # Supress warnings from loading DAGs
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        dagbag = DagBag(dag_folder=PROJECT_DIR / "dags", include_examples=False)
+
+    return dagbag
---
+++ tests/dags/test_dag_validity.py
@@ -0,0 +1,44 @@
+def test_dag_validity(get_dag_bag):
+    """Test all DAGs can be parsed.
+
+    This test should be equivalent to the integration test using airflow CLI.
+    At the moment, there is a discrepancy between this unit test and the integration
+    test. Once equivalent, this unit test should replace to the integration test.
+
+    """
+    dagbag = get_dag_bag
+
+    data = []
+    for filename, errors in dagbag.import_errors.items():
+        # TODO investigate why this is the only `conn_id` causing an error
+        if "The conn_id `google_cloud_airflow_dataproc` isn't defined" in errors:
+            continue
+        data.append({"filepath": filename, "error": errors})
+    if data:
+        print(data)
+        raise AssertionError
+
+
+def test_dag_tags(get_dag_bag):
+    """Check tags in all DAGs are valid."""
+
+    valid_tags = {
+        "impact/tier_1", "impact/tier_2", "impact/tier_3", "repo/bigquery-etl",
+        "repo/telemetry-airflow", "repo/private-bigquery-etl"}
+    dagbag = get_dag_bag
+
+    for dag_name, dag in dagbag.dags.items():
+        for tag in dag.tags:
+            assert tag in valid_tags, f"DAG: {dag_name}: Invalid tag `{tag}`"
+
+
+def test_dag_tags_required(get_dag_bag):
+    """Check at least one tag per DAG is of the required type"""
+
+    required_tag_type = "impact"
+    dagbag = get_dag_bag
+
+    for dag_name, dag in dagbag.dags.items():
+        assert [tag for tag in dag.tags if
+                required_tag_type in tag], f"DAG: {dag_name}: Missing required tag " \
+                                           f"type `{required_tag_type}`"
