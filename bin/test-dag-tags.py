c1717c118d1149d50c32aacf0069c9a0ad7a4232
kik-kik <kignasiak@mozilla.com>
2023-02-09 15:03:20 +0100

    added triage/record-only tag

--- bin/test-dag-tags.py
+++ bin/test-dag-tags.py
@@ -4,7 +4,7 @@ import subprocess
 from typing import Dict
 
 
-VALID_TAGS = ("impact/tier_1", "impact/tier_2", "impact/tier_3", "repo/bigquery-etl", "repo/telemetry-airflow", "repo/private-bigquery-etl",)
+VALID_TAGS = ("impact/tier_1", "impact/tier_2", "impact/tier_3", "repo/bigquery-etl", "repo/telemetry-airflow", "repo/private-bigquery-etl", "triage/record_only",)
 REQUIRED_TAG_TYPES = ("impact",)
 
 
