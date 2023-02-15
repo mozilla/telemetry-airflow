8b8eb3c45737e913a12e09e38614c5982e931d21
Wil Stuckey <wstuckey@mozilla.com>
2023-02-15 11:58:13 -0600

    Add merino jobs to Airflow

---
+++ dags/merino_jobs.py
@@ -0,0 +1,76 @@
+import datetime
+from typing import Any, Dict, List
+
+from airflow import DAG
+from airflow.hooks.base import BaseHook
+
+from dags.operators.gcp_container_operator import GKEPodOperator
+from dags.utils.tags import Tag
+
+DOCS = """\
+    Merino Jobs
+
+    Dag for orchestrating jobs that build datasets that are used in merino.
+    The jobs are run via the GKEPodOperator 
+"""
+
+def merino_job(name: str, arguments: List[str], env_vars: Dict[str, Any]):
+    return GKEPodOperator(
+        task_id=name,
+        name=name,
+        image="mozilla/merino-py:latest",
+        project_id="moz-fx-data-gke-sandbox",
+        gcp_conn_id="google_cloud_gke_sandbox",
+        cluster_name="wstuckey-gke-sandbox",
+        location="us-west1",
+        cmds=["python", "-m", "merino.jobs.cli"],
+        arguments=arguments,
+        env_vars=env_vars,
+        email=[
+            "wstuckey@mozilla.com",
+        ],
+
+    )
+
+default_args = {
+    "owner": "wstuckey@mozilla.com",
+    "start_date": datetime.datetime(2019, 7, 25),
+    "email": ["wstuckey@mozilla.com"],
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "depends_on_past": False,
+    # If a task fails, retry it once after waiting at least 5 minutes
+    "retries": 1,
+    "retry_delay": datetime.timedelta(minutes=5),
+}
+
+tags = [Tag.ImpactTier.tier_3.__str__()]
+
+# Run weekly on mondays at 5am UTC
+with DAG(
+    "merino_jobs", schedule_interval="0 5 * * 1", doc_md=DOCS, default_args=default_args, tags=tags,
+) as dag:
+
+    conn = BaseHook.get_connection("merino_elasticsearch")
+
+    wikipedia_indexer_copy_export = merino_job(
+        "wikipedia_indexer_copy_export",
+        arguments=["wikipedia-indexer", "copy-export"],
+        env_vars=dict(
+            MERINO_JOBS__WIKIPEDIA_INDEXER__GCP_PATH="merino-jobs-dev/wikipedia-exports",
+            MERINO_JOBS__WIKIPEDIA_INDEXER__GCP_PROJECT="contextual-services-dev",
+        ),
+    )
+
+    wikipedia_indexer_build_index = merino_job(
+        "wikipedia_indexer_build_index",
+        arguments=["wikipedia-indexer", "index"],
+        env_vars=dict(
+            MERINO_JOBS__WIKIPEDIA_INDEXER__ES_CLOUD_ID=conn.host,
+            MERINO_JOBS__WIKIPEDIA_INDEXER__ES_API_KEY=conn.password,
+        ),
+    )
+
+
+
+    wikipedia_indexer_copy_export >> wikipedia_indexer_build_index
