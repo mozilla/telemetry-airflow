67657bbebc3748c6fe831a5c0b94e59036e61e18
wil stuckey <wil.stuckey@gmail.com>
2023-04-18 15:51:55 -0500

    DENG-830 Add prod task and a task to email on success as a test for notification (#1689)
    * DENG-830 Add prod task and a task to email on success as a test for notification
    
    * Add default env_vars
    
    * linter fixes

--- dags/merino_jobs.py
+++ dags/merino_jobs.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, List, Optional
 
 from airflow import DAG
 from airflow.hooks.base import BaseHook
+from airflow.operators.email import EmailOperator
 from operators.gcp_container_operator import GKEPodOperator
 from utils.tags import Tag
 
@@ -17,8 +18,11 @@ DOCS = """\
 def merino_job(
     name: str, arguments: List[str], env_vars: Optional[Dict[str, Any]] = None
 ):
+    default_env_vars = {"MERINO_ENV": "production"}
+
     if env_vars is None:
         env_vars = {}
+
     return GKEPodOperator(
         task_id=name,
         name=name,
@@ -29,7 +33,7 @@ def merino_job(
         location="us-west1",
         cmds=["python", "-m", "merino.jobs.cli"],
         arguments=arguments,
-        env_vars=env_vars,
+        env_vars=default_env_vars.update(env_vars),
         email=[
             "wstuckey@mozilla.com",
         ],
@@ -75,9 +79,6 @@ with DAG(
             "--gcp-project",
             "moz-fx-data-shared-prod",
         ],
-        env_vars={
-            "MERINO_ENV": "production",
-        },
     )
 
     wikipedia_indexer_build_index_for_staging = merino_job(
@@ -97,7 +98,6 @@ with DAG(
             "moz-fx-data-shared-prod",
         ],
         env_vars={
-            "MERINO_ENV": "production",
             # Using the API key in the argument list leaks the sensitive data into the airflow UI.
             "MERINO_JOBS__WIKIPEDIA_INDEXER__ES_API_KEY": es_staging_connection.password,
         },
@@ -120,7 +120,6 @@ with DAG(
             "moz-fx-data-shared-prod",
         ],
         env_vars={
-            "MERINO_ENV": "production",
             # Using the API key in the argument list leaks the sensitive data into the airflow UI.
             "MERINO_JOBS__WIKIPEDIA_INDEXER__ES_API_KEY": es_prod_connection.password,
         },
@@ -146,4 +145,27 @@ with DAG(
         ],
     )
 
-    prepare_domain_metadata_stage
+    prepare_domain_metadata_prod = merino_job(
+        "nav_suggestions_prepare_domain_metadata_prod",
+        arguments=[
+            "navigational-suggestions",
+            "prepare-domain-metadata",
+            "--src-gcp-project",
+            "moz-fx-data-shared-prod",
+            "--dst-gcp-project",
+            "moz-fx-merino-prod-1c2f ",
+            "--dst-gcs-bucket",
+            "merino-images-prodpy",
+        ],
+    )
+
+    on_domain_success = EmailOperator(
+        task_id="email_on_domain_success",
+        to="wstuckey@mozilla.com",
+        subject="Navigational Suggestions Domain Metadata job successful",
+        html_content="""
+        Job completed. Download the new top picks json file on GCS.
+        """,
+    )
+
+    [prepare_domain_metadata_stage, prepare_domain_metadata_prod] >> on_domain_success
