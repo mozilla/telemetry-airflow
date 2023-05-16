e26c6de6702bfde63d7035fe540d10f996070e29
Arkadiusz Komarzewski <akomarzewski@mozilla.com>
2023-05-16 16:20:33 +0200

    Update update_orphaning contact emails

--- dags/update_orphaning_dashboard_etl.py
+++ dags/update_orphaning_dashboard_etl.py
@@ -24,7 +24,7 @@ default_args = {
     "owner": "akomar@mozilla.com",
     "depends_on_past": False,
     "start_date": datetime(2019, 10, 12),
-    "email": ["telemetry-alerts@mozilla.com", "rstrong@mozilla.com", "akomar@mozilla.com"],
+    "email": ["telemetry-alerts@mozilla.com", "ahabibi@mozilla.com", "akomar@mozilla.com"],
     "email_on_failure": True,
     "email_on_retry": True,
     "retries": 2,
