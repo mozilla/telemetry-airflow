10461208c63a24f1f4780f6238766c83251046ae
kik-kik <kignasiak@mozilla.com>
2023-05-26 14:59:32 +0200

    reassigning DAG owners as per "adopt a DAG" doc

--- dags/graphics_telemetry.py
+++ dags/graphics_telemetry.py
@@ -21,13 +21,12 @@ from utils.dataproc import moz_dataproc_pyspark_runner, get_dataproc_parameters
 from utils.tags import Tag
 
 default_args = {
-    "owner": "bewu@mozilla.com",
+    "owner": "kik@mozilla.com",
     "depends_on_past": False,
     "start_date": datetime.datetime(2020, 11, 26),
     "email": [
         "telemetry-alerts@mozilla.com",
-        "bewu@mozilla.com",
-        "mmynttinen@mozilla.com",
+        "kik@mozilla.com",
     ],
     "email_on_failure": True,
     "email_on_retry": True,
@@ -55,12 +54,14 @@ with DAG(
     tags=tags,
 ) as dag:
     # Jobs read from/write to s3://telemetry-public-analysis-2/gfx/telemetry-data/
-    write_aws_conn_id = 'aws_dev_telemetry_public_analysis_2_rw'
+    write_aws_conn_id = "aws_dev_telemetry_public_analysis_2_rw"
     is_dev = os.environ.get("DEPLOY_ENVIRONMENT") == "dev"
     if is_dev:
-        aws_access_key, aws_secret_key = ('replace_me', 'replace_me')
+        aws_access_key, aws_secret_key = ("replace_me", "replace_me")
     else:
-        aws_access_key, aws_secret_key, _ = AwsBaseHook(aws_conn_id=write_aws_conn_id, client_type='s3').get_credentials()
+        aws_access_key, aws_secret_key, _ = AwsBaseHook(
+            aws_conn_id=write_aws_conn_id, client_type="s3"
+        ).get_credentials()
 
     wait_for_main_ping = ExternalTaskSensor(
         task_id="wait_for_copy_deduplicate_main_ping",
@@ -89,17 +90,22 @@ with DAG(
             cluster_name="graphics-trends-{{ ds }}",
             job_name="graphics-trends",
             python_driver_code="https://raw.githubusercontent.com/mozilla/python_mozetl/main/mozetl/graphics/graphics_telemetry_trends.py",
-            init_actions_uris=["gs://dataproc-initialization-actions/python/pip-install.sh"],
-            additional_metadata={'PIP_PACKAGES': " ".join(PIP_PACKAGES)},
+            init_actions_uris=[
+                "gs://dataproc-initialization-actions/python/pip-install.sh"
+            ],
+            additional_metadata={"PIP_PACKAGES": " ".join(PIP_PACKAGES)},
             additional_properties={
                 "spark:spark.jars": "gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar",
                 "spark-env:AWS_ACCESS_KEY_ID": aws_access_key,
-                "spark-env:AWS_SECRET_ACCESS_KEY": aws_secret_key
+                "spark-env:AWS_SECRET_ACCESS_KEY": aws_secret_key,
             },
             py_args=[
-                "--s3-bucket", S3_BUCKET,
-                "--s3-prefix", S3_PREFIX,
-                "--weekly-fraction", "0.003",
+                "--s3-bucket",
+                S3_BUCKET,
+                "--s3-prefix",
+                S3_PREFIX,
+                "--weekly-fraction",
+                "0.003",
             ],
             idle_delete_ttl=14400,
             num_workers=2,
@@ -107,7 +113,7 @@ with DAG(
             gcp_conn_id=params.conn_id,
             service_account=params.client_email,
             storage_bucket=params.storage_bucket,
-        )
+        ),
     )
 
     graphics_dashboard = SubDagOperator(
@@ -121,17 +127,22 @@ with DAG(
             cluster_name="graphics-dashboard-{{ ds }}",
             job_name="graphics-dashboard",
             python_driver_code="https://raw.githubusercontent.com/mozilla/python_mozetl/main/mozetl/graphics/graphics_telemetry_dashboard.py",
-            init_actions_uris=["gs://dataproc-initialization-actions/python/pip-install.sh"],
-            additional_metadata={'PIP_PACKAGES': " ".join(PIP_PACKAGES)},
+            init_actions_uris=[
+                "gs://dataproc-initialization-actions/python/pip-install.sh"
+            ],
+            additional_metadata={"PIP_PACKAGES": " ".join(PIP_PACKAGES)},
             additional_properties={
                 "spark:spark.jars": "gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar",
                 "spark-env:AWS_ACCESS_KEY_ID": aws_access_key,
-                "spark-env:AWS_SECRET_ACCESS_KEY": aws_secret_key
+                "spark-env:AWS_SECRET_ACCESS_KEY": aws_secret_key,
             },
             py_args=[
-                "--output-bucket", "telemetry-public-analysis-2",
-                "--output-prefix", "gfx/telemetry-data/",
-                "--release-fraction", "0.003",
+                "--output-bucket",
+                "telemetry-public-analysis-2",
+                "--output-prefix",
+                "gfx/telemetry-data/",
+                "--release-fraction",
+                "0.003",
             ],
             idle_delete_ttl=14400,
             num_workers=2,
@@ -139,7 +150,7 @@ with DAG(
             gcp_conn_id=params.conn_id,
             service_account=params.client_email,
             storage_bucket=params.storage_bucket,
-        )
+        ),
     )
 
     wait_for_main_ping >> graphics_trends
