a34971d6e63f2ecce03c44488dc6901a2cdf888a
Mikaël Ducharme <mducharme@mozilla.com>
2023-06-21 15:03:39 -0400

    chore: use PostgreSQL for local environment and update README.md (#1729)

--- config/airflow_local_settings.py
+++ config/airflow_local_settings.py
@@ -1,15 +1,11 @@
-from gevent import monkey
-
-monkey.patch_all()
-
 STATE_COLORS = {
-    "queued": 'gray',
-    "running": 'lime',
-    "success": '#0000FF',
-    "failed": 'red',
-    "up_for_retry": 'gold',
-    "up_for_reschedule": 'turquoise',
-    "upstream_failed": 'orange',
-    "skipped": 'pink',
-    "scheduled": 'tan',
+    "queued": "gray",
+    "running": "lime",
+    "success": "#0000FF",
+    "failed": "red",
+    "up_for_retry": "gold",
+    "up_for_reschedule": "turquoise",
+    "upstream_failed": "orange",
+    "skipped": "pink",
+    "scheduled": "tan",
 }
