9d7ac1fbb8ae656e7b4bca13ab9691ada43aa8de
Daniel Thorn <dthorn@mozilla.com>
2023-08-15 11:39:20 -0700

    Import timetable relative to plugins folder

--- dags/shredder.py
+++ dags/shredder.py
@@ -1,10 +1,9 @@
 from datetime import datetime, timedelta
 
 from airflow import DAG
+from timetable import MultiWeekTimetable
 from utils.gcp import gke_command
 from utils.tags import Tag
-
-from plugins.timetable import MultiWeekTimetable
 
 docs = """
 ### shredder
