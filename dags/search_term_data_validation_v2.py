376c4039836cb28f3e6352fc4a633f8c4ff2fc96
mikaeld <mducharme@mozilla.com>
2023-09-20 15:48:38 -0400

    test for ctroy

--- dags/search_term_data_validation_v2.py
+++ dags/search_term_data_validation_v2.py
@@ -42,13 +42,9 @@ with DAG(
     search_term_data_validation = gke_command(
         task_id="search_term_data_validation_v2",
         command=[
-            "python",
-            "search_term_data_validation_v2/main.py",
-            "--data_validation_origin",
-            "moz-fx-data-shared-prod.search_terms.sanitization_job_data_validation_metrics",
-            "--data_validation_reporting_destination",
-            "moz-fx-data-shared-prod.search_terms_derived.search_term_data_validation_reports_v1",
+            'python -c "print(123)"',
         ],
-        docker_image="gcr.io/moz-fx-data-airflow-prod-88e0/search-alert_docker_etl:latest",
+        docker_image="gcr.io/moz-fx-data-airflow-prod-88e0/search-term-data-validation-v2_docker_etl:latest",
         dag=dag,
     )
+
