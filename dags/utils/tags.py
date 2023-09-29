1f4302a23ef7db8bc09bfe517a3f7ee2c8f02a23
Mike Williams <mwilliams@mozilla.com>
2023-09-29 11:22:31 -0400

    linting

--- dags/utils/tags.py
+++ dags/utils/tags.py
@@ -1,6 +1,4 @@
-"""
-Module with Airflow tag definitions
-"""
+"""Module with Airflow tag definitions."""
 
 from enum import Enum
 
@@ -9,20 +7,18 @@ class InvalidTagError(AttributeError):
     pass
 
 class Tag(Enum):
-    """
-    Enum containing available Airflow tags
-    """
+    """Enum containing available Airflow tags."""
 
     def __getattr__(self, item: str) -> str:
         """
         This is to simplify accessing enum values.
         Instead of Tag.ImpactTier.value.tier_1.value we can just use
-        Tag.ImpactTier.tier_1
+        Tag.ImpactTier.tier_1.
 
         # source: https://newbedev.com/enum-of-enums-in-python
         """
 
-        if item == '_value_':
+        if item == "_value_":
             raise InvalidTagError
 
         try:
@@ -34,9 +30,7 @@ class Tag(Enum):
 
 
     class ImpactTier(Enum):
-        """
-        Valid options for Impact tier tag
-        """
+        """Valid options for Impact tier tag."""
 
         tier_1: str = "impact/tier_1"
         tier_2: str = "impact/tier_2"
@@ -45,7 +39,7 @@ class Tag(Enum):
     class Triage(Enum):
         """
         Tag for representing that an engineer on triage
-        should attempt to resolve the problem themselves
+        should attempt to resolve the problem themselves.
         """
 
         confidential: str = "triage/confidential"
