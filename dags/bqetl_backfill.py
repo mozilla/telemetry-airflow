422fcd675a9e37abb036136f647697ef797ae89f
Frank Bertsch <frank.bertsch@gmail.com>
2023-10-24 11:40:13 -0400

    Update docs; use taskflow output

--- dags/bqetl_backfill.py
+++ dags/bqetl_backfill.py
@@ -33,16 +33,19 @@ frank@mozilla.com
     params={
         "table_name": Param(
             "{dataset}.{table}",
+            title="Table Name",
             type="string",
             description="[Required] The table to backfill, must be of the form {dataset}.{table}",
         ),
         "sql_dir": Param(
             "sql",
+            title="SQL Directory in bigquery_etl",
             type="string",
             description="[Required] Path to directory which contains queries.",
         ),
         "project_id": Param(
             "moz-fx-data-shared-prod",
+            title="GCP Project ID",
             type="string",
             description="[Required] GCP project ID that the query is in (the bqetl directory)",
             enum=[
@@ -58,22 +61,30 @@ frank@mozilla.com
         ),
         "start_date": Param(
             f"{datetime.date.today()}",
+            title="Start Date",
             type="string",
             format="date",
             description="[Required] First date to be backfilled, inclusive",
         ),
         "end_date": Param(
             f"{datetime.date.today()}",
+            title="End Date",
             type="string",
             format="date",
             description="[Required] Last date to be backfilled, inclusive",
         ),
-        "dry_run": Param(True, type="boolean", description="Dry run the backfill"),
+        "dry_run": Param(
+            True, title="Dry Run?", type="boolean", description="Dry run the backfill"
+        ),
         "exclude": Param(
-            [], type=["null", "array"], description="Dates to exclude from the backfill"
+            [],
+            title="Excluded Dates",
+            type=["null", "array"],
+            description="Dates to exclude from the backfill",
         ),
         "max_rows": Param(
             10,
+            title="Max Rows",
             type="number",
             description="Number of rows to return (will be visible in logs)",
             minimum=0,
@@ -81,6 +92,7 @@ frank@mozilla.com
         ),  # Frank made up this max!
         "parallelism": Param(
             8,
+            title="Parallelism",
             type="number",
             description="Number of threads to use to run backfills",
             minimum=1,
@@ -88,13 +100,21 @@ frank@mozilla.com
         ),  # Frank made up this max!
         "no_partition": Param(
             False,
+            title="No Partition",
             type="boolean",
-            description="(WARNING: Will overwrite table!) Disable writing results to a partition. Overwrites entire destination table.",
+            description="(WARNING: Will overwrite table!) If True, disables writing results to a partition. Overwrites entire destination table.",
         ),
         "destination_table": Param(
             None,
+            title="Destination Table",
             type=["null", "string"],
             description="(Only set this field if your destination table is different than the project or query!) Destination table name the results are written to. If not set, determines destination table based on query.",
+        ),
+        "run_checks": Param(
+            True,
+            title="Run Checks?",
+            type="boolean",
+            description="Whether to run checks during backfill.",
         ),
     },
 )
@@ -134,6 +154,11 @@ def bqetl_backfill_dag():
         if context["params"]["no_partition"]:
             cmd.append("--no_partition")
 
+        if context["params"]["run_checks"]:
+            cmd.append("--checks")
+        else:
+            cmd.append("--no-checks")
+
         if not all(isinstance(c, str) for c in cmd):
             raise Exception(
                 f"All GKE arguments must be strings! Did you do something surprising to the DAG params?\nArgs: {cmd}"
@@ -143,17 +168,13 @@ def bqetl_backfill_dag():
 
         return cmd
 
-    run_backfill = gke_command(
+    gke_command(
         reattach_on_restart=True,
         task_id="bqetl_backfill",
-        command="{{ "
-        + "task_instance.xcom_pull(task_ids='generate_backfill_command')"
-        + " }}",
+        command=generate_backfill_command(),
         docker_image="gcr.io/moz-fx-data-airflow-prod-88e0/bigquery-etl:latest",
         gcp_conn_id="google_cloud_airflow_gke",
     )
-
-    generate_backfill_command() >> run_backfill
 
 
 dag = bqetl_backfill_dag()
