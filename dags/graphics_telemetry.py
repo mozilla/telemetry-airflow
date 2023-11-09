233f6ffa0ffdb4d8460aa2d09fdc76474bef8c0d
kik-kik <kignasiak@mozilla.com>
2023-11-09 11:15:26 +0100

    updating python_moztelemetry package to use the new git tag reference

--- dags/graphics_telemetry.py
+++ dags/graphics_telemetry.py
@@ -34,7 +34,7 @@ default_args = {
 }
 
 PIP_PACKAGES = [
-    "git+https://github.com/mozilla/python_moztelemetry.git@v0.10.4#egg=python-moztelemetry",
+    "git+https://github.com/mozilla/python_moztelemetry.git@v0.10.7#egg=python-moztelemetry",
     "git+https://github.com/FirefoxGraphics/telemetry.git#egg=pkg&subdirectory=analyses/bigquery_shim",
     "boto3==1.16.20",
     "six==1.15.0",
