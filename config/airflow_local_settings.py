6283bbb0629097d8919279ff964515dfee06a4bb
Sean Rose <srose@mozilla.com>
2024-03-07 13:04:58 -0800

    Add missing Airflow state colors for "restarting", "deferred", and "removed".

--- config/airflow_local_settings.py
+++ config/airflow_local_settings.py
@@ -2,10 +2,13 @@ STATE_COLORS = {
     "queued": "gray",
     "running": "lime",
     "success": "#0000FF",
+    "restarting": "violet",
     "failed": "red",
     "up_for_retry": "gold",
     "up_for_reschedule": "turquoise",
     "upstream_failed": "orange",
     "skipped": "pink",
+    "deferred": "mediumpurple",
+    "removed": "lightgrey",
     "scheduled": "tan",
 }
