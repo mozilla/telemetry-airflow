d80b20c2d86a699f165240944b1666070f026dcd
Katie Windau <kwindau@mozilla.com>
2024-05-01 11:51:47 -0500

    work in progress

--- dags/cloudflare_device_usage.py
+++ dags/cloudflare_device_usage.py
@@ -59,6 +59,45 @@ def generate_device_type_timeseries_api_call(strt_dt, end_dt, agg_int, location)
         device_usage_api_url = "https://api.cloudflare.com/client/v4/radar/http/timeseries/device_type?name=human&botClass=LIKELY_HUMAN&dateStart=%sT00:00:00.000Z&dateEnd=%sT00:00:00.000Z&location=%s&name=bot&botClass=LIKELY_AUTOMATED&dateStart=%sT00:00:00.000Z&dateEnd=%sT00:00:00.000Z&location=%s&format=json&aggInterval=%s" % (strt_dt, end_dt, location, strt_dt, end_dt, location, agg_int)
     return device_usage_api_url
 
+###NOTE - this function should be used in device & OS but not browser
+def get_timeseries_api_call_date_ranges(start_date, end_date, max_days_interval): 
+    """ Input start date, end date as string in YYYY-MM-DD format, max days interval as int, and returns a dataframe of intervals to use"""
+
+    #Initialize arrays we will later use to make the dataframe
+    sd_array = []
+    ed_array = []
+
+    #Convert the start date and end date to date objects
+    start_dt = datetime.strptime(start_date, '%Y-%m-%d').date()
+    end_dt = datetime.strptime(end_date, '%Y-%m-%d').date()
+
+    #Calculate # of days between start date and end date
+    days_between_start_dt_and_end_dt = (end_dt - start_dt).days
+
+    #Divide the # of days by how the max # of days you want in each API request
+    nbr_iterations = int(days_between_start_dt_and_end_dt/max_days_interval) - 1
+
+    #Initialize the current start date with the start date
+    current_start_dt = start_dt
+
+    #For each iteration
+    for i in range(nbr_iterations):        
+        #Append current start dt 
+        sd_array.append(str(current_start_dt))
+        #Calculate end date
+        current_end_date = current_start_dt + timedelta(days=max_days_interval)
+        if current_end_date <= end_dt:
+            ed_array.append(str(current_end_date))
+        else: 
+            ed_array.append(str(end_dt))
+
+        #Update the current start date to be 1 day after the last end date
+        current_start_dt = current_end_date + timedelta(days = 1)
+
+    dates_df = pd.DataFrame({"Start_Date": sd_array,
+                             "End_Date": ed_array})
+    return dates_df
+
 
 def parse_device_type_timeseries_response_human(result):
     """ Takes the response JSON and returns parsed information"""
@@ -113,14 +152,22 @@ with DAG(
 ) as dag:
 
     #Define OS usage task
-    get_device_usage_data = PythonOperator(task_id="get_device_usage_data",
+    get_data = PythonOperator(task_id="get_device_usage_data",
                                 python_callable=get_device_usage_data,
                                 #op_args=[device_type, location],
                                 execution_timeout=timedelta(minutes=20))
-    load_device_usage_data_to_gcs = EmptyOperator(task_id="load_device_usage_data_to_gcs")
-    load_device_usage_results_to_bq = EmptyOperator(task_id="load_device_usage_results_to_bq")
-    load_device_usage_errors_to_bq = EmptyOperator(task_id="load_device_usage_errors_to_bq")
+    load_results_to_bq = EmptyOperator(task_id="load_results_to_bq")
+    load_errors_to_bq = EmptyOperator(task_id="load_errors_to_bq")
+
+    archive_results = EmptyOperator(task_id="archive_results")
+    archive_errors = EmptyOperator(task_id="archive_errors")
+
+    del_results_from_gcs_stg = EmptyOperator(task_id="delete_results_from_gcs_stg")
+    del_errors_from_gcs_stg = EmptyOperator(task_id="delete_errors_from_gcs_stg")
+
     run_device_qa_checks = EmptyOperator(task_id="run_device_qa_checks")
 
-get_device_usage_data >> load_device_usage_data_to_gcs >> load_device_usage_results_to_bq 
-load_device_usage_results_to_bq >> load_device_usage_errors_to_bq >> run_device_qa_checks
+get_data >> load_results_to_bq >> archive_results >> del_results_from_gcs_stg
+get_data >> load_errors_to_bq >> archive_errors >> del_errors_from_gcs_stg
+
+[del_results_from_gcs_stg , del_errors_from_gcs_stg] >> run_device_qa_checks
