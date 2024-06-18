393fb726a3ff513cf4580e1d93213afc07b140ac
Marlene-M-Hirose <mhirose@mozilla.com>
2024-06-18 09:42:01 -1000

    take out arguments from DAG

--- dags/microsoft_store.py
+++ dags/microsoft_store.py
@@ -7,31 +7,6 @@ from operators.gcp_container_operator import GKEPodOperator
 from utils.tags import Tag
 
 # Deploy value associated with Microsoft Store keys in k8s secret `airflow-gke-secrets` in environments Microsoft variables
-microsoft_client_id = Secret(
-    deploy_type="env",
-    deploy_target="MICROSOFT_CLIENT_ID",
-    secret="airflow-gke-secrets",
-    key="MICROSOFT_CLIENT_ID",
-)
-microsoft_client_secret = Secret(
-    deploy_type="env",
-    deploy_target="MICROSOFT_CLIENT_SECRET",
-    secret="airflow-gke-secrets",
-    key="MICROSOFT_CLIENT_SECRET",
-)
-microsoft_tenant_id = Secret(
-    deploy_type="env",
-    deploy_target="MICROSOFT_TENANT_ID",
-    secret="airflow-gke-secrets",
-    key="MICROSOFT_TENANT_ID",
-)
-microsoft_store_app_list = Secret(
-    deploy_type="env",
-    deploy_target="MICROSOFT_STORE_APP_LIST",
-    secret="airflow-gke-secrets",
-    key="MICROSOFT_STORE_APP_LIST",
-)
-
 docs = """
 This DAG runs the daily download of aggregated data from the Microsoft Store API.
 #### Owner
@@ -82,9 +57,6 @@ with DAG(
             "python",
             "sql/moz-fx-data-shared-prod/microsoft_derived/microsoft_app_acquisitions_v1/query.py",
             "--date={{ ds }}",
-            "--microsoft_client_id={{ var.value.MICROSOFT_CLIENT_ID }}",
-            "--microsoft_store_app_list={{ var.value.MICROSOFT_STORE_APP_LIST }}",
-            "--microsoft_tenant_id={{ var.value.MICROSOFT_TENANT_ID }}",
         ],
         image="gcr.io/moz-fx-data-airflow-prod-88e0/bigquery-etl:latest",
         owner="mhirose@mozilla.com",
@@ -103,9 +75,6 @@ with DAG(
             "python",
             "sql/moz-fx-data-shared-prod/microsoft_derived/microsoft_app_conversions_v1/query.py",
             "--date={{ ds }}",
-            "--microsoft_client_id={{ var.value.MICROSOFT_CLIENT_ID }}",
-            "--microsoft_store_app_list={{ var.value.MICROSOFT_STORE_APP_LIST }}",
-            "--microsoft_tenant_id={{ var.value.MICROSOFT_TENANT_ID }}",
         ],
         image="gcr.io/moz-fx-data-airflow-prod-88e0/bigquery-etl:latest",
         owner="mhirose@mozilla.com",
@@ -124,9 +93,6 @@ with DAG(
             "python",
             "sql/moz-fx-data-shared-prod/microsoft_derived/microsoft_app_installs_v1/query.py",
             "--date={{ ds }}",
-            "--microsoft_client_id={{ var.value.MICROSOFT_CLIENT_ID }}",
-            "--microsoft_store_app_list={{ var.value.MICROSOFT_STORE_APP_LIST }}",
-            "--microsoft_tenant_id={{ var.value.MICROSOFT_TENANT_ID }}",
         ],
         image="gcr.io/moz-fx-data-airflow-prod-88e0/bigquery-etl:latest",
         owner="mhirose@mozilla.com",
