68eee9acd551bb575dcb84e68bb3e05cbf48fac2
Jared Snyder <jsnyder@mozilla.com>
2024-07-01 07:25:36 -0500

    added wait task sensor

--- dags/search_forecasting.py
+++ dags/search_forecasting.py
@@ -10,8 +10,10 @@ import os
 from datetime import datetime, timedelta
 
 from airflow import DAG
+from airflow.sensors.external_task import ExternalTaskSensor
 
 from operators.gcp_container_operator import GKEPodOperator
+from utils.constants import ALLOWED_STATES, FAILED_STATES
 from utils.tags import Tag
 
 default_args = {
@@ -56,3 +58,21 @@ with DAG(
             arguments=["python", script_path, "-c", config_path],
             image=IMAGE,
         )
+
+        # all the search forecasting metrics come from the search_revenue_levers_daily
+        # table which is run in the bqetl_search_dashboard dag
+        # as the search_derived__search_revenue_levers_daily__v1 task
+        # see: https://workflow.telemetry.mozilla.org/dags/bqetl_search_dashboard/grid
+        wait_task_sensor = ExternalTaskSensor(
+            task_id="wait_for_search_dashboard",
+            external_dag_id="bqetl_search_dashboard",
+            external_task_id="search_derived__search_revenue_levers_daily__v1",
+            execution_delta=timedelta(days=7),
+            check_existence=True,
+            mode="reschedule",
+            allowed_states=ALLOWED_STATES,
+            failed_states=FAILED_STATES,
+            pool="DATA_ENG_EXTERNALTASKSENSOR",
+        )
+
+        wait_task_sensor >> forecast_task
