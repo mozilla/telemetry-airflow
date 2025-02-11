81b42bb9e1a8a1cc599bf7e4b679221dc94cf5f3
bwu <bewu@mozilla.com>
2025-02-11 17:37:54 -0500

    fix(backfill): Fix bqetl_backfill_complete emails

--- dags/bqetl_backfill_complete.py
+++ dags/bqetl_backfill_complete.py
@@ -18,8 +18,8 @@ tags = [Tag.ImpactTier.tier_3]
 default_args = {
     "email": [
         "ascholtz@mozilla.com",
-        "bewu@mozilla.com"
-        "wichan@mozilla.com"
+        "bewu@mozilla.com",
+        "wichan@mozilla.com",
     ]
 }
 
