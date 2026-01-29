55634565ac7c9f0f7ef02055989b2526fd0ac793
CircleCI job <dataops@mozilla.com>
2026-01-29 19:12:04 +0000

    Automatic commit - DAGs from commit eb67ba4 build 29605 [skip ci]

--- dags/shredder.py
+++ dags/shredder.py
@@ -5,6 +5,7 @@ from kubernetes.client import models as k8s
 from timetable import MultiWeekTimetable
 
 from operators.gcp_container_operator import GKEPodOperator, OnFinishAction
+from utils.glean_v2_backfill import column_removal_backfill_tables
 from utils.tags import Tag
 
 docs = """
@@ -86,23 +87,6 @@ common_task_args = {
     "reattach_on_restart": True,
     "dag": dag,
 }
-
-# For https://mozilla-hub.atlassian.net/browse/DENG-8494
-# Should match https://github.com/mozilla/mozilla-schema-generator/blob/main/mozilla_schema_generator/configs/glean_v2_allowlist.yaml
-column_removal_backfill_tables = [
-    "org_mozilla_fenix_nightly_stable.metrics_v1",
-    "org_mozilla_fenix_nightly_stable.sync_v1",
-    "org_mozilla_fenix_nightly_stable.first_session_v1",
-    "org_mozilla_fenix_nightly_stable.adjust_attribution_v1",
-    "org_mozilla_fenix_nightly_stable.health_v1",
-    "org_mozilla_fenix_nightly_stable.captcha_detection_v1",
-    "org_mozilla_fennec_aurora_stable.metrics_v1",
-    "org_mozilla_fennec_aurora_stable.sync_v1",
-    "org_mozilla_fennec_aurora_stable.captcha_detection_v1",
-    "org_mozilla_fennec_aurora_stable.first_session_v1",
-    "org_mozilla_fennec_aurora_stable.health_v1",
-    "org_mozilla_fennec_aurora_stable.adjust_attribution_v1",
-]
 
 # handle telemetry main and main use counter separately to ensure they run continuously
 # and don't slow down other tables. run them in a separate project with their own slot
