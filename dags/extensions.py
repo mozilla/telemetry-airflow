c4cfbe4bc8ec76e9902a262a39f34a9ceeab8a87
CircleCI job <dataops@mozilla.com>
2026-02-19 17:00:27 +0000

    Automatic commit - DAGs from commit c19a7bc build 29825 [skip ci]

--- dags/extensions.py
+++ dags/extensions.py
@@ -11,14 +11,14 @@ Loads the table moz-fx-data-shared-prod.external_derived.chrome_extensions_v1
 
 Note - if it fails, please alert the DAG owner, but do not re-run.
 
-Owner: kwindau@mozilla.com
+Owner: kik@mozilla.com
 """
 
 default_args = {
-    "owner": "kwindau@mozilla.com",
+    "owner": "kik@mozilla.com",
     "start_date": datetime.datetime(2025, 4, 13, 0, 0),
     "end_date": None,
-    "email": ["kwindau@mozilla.com"],
+    "email": ["kik@mozilla.com"],
     "depends_on_past": False,
     "retry_delay": datetime.timedelta(seconds=1800),
     "email_on_failure": True,
