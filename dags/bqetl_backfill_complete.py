156bdcca2db0f0f670d77bfe4836e5b5b1772a46
CircleCI job <dataops@mozilla.com>
2026-05-11 15:49:08 +0000

    Automatic commit - DAGs from commit 1b0c423 build 30448 [skip ci]

--- dags/bqetl_backfill_complete.py
+++ dags/bqetl_backfill_complete.py
@@ -92,7 +92,7 @@ with DAG(
 
         @task
         def prepare_pod_parameters(entry):
-            return [f"script/bqetl backfill complete { entry['qualified_table_name'] }"]
+            return [f"script/bqetl backfill complete { entry['qualified_table_name'] } --copy-table-permissions"]
 
         process_backfill = GKEPodOperator(
             task_id="process_backfill",
