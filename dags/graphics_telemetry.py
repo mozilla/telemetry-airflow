019aab5c1e63ca553a7b9e96b1609a6ecaa8ef3c
Ben Wu <12437227+BenWu@users.noreply.github.com>
2026-07-30 11:29:59 -0400

    chore(graphics): [DENG-11194] Change graphics_telemetry to docker-etl (#2404)

--- dags/graphics_telemetry.py
+++ dags/graphics_telemetry.py
@@ -16,11 +16,10 @@ the job should be considered healthy.
 import datetime
 
 from airflow import DAG
-from airflow.operators.subdag import SubDagOperator
 from airflow.sensors.external_task import ExternalTaskSensor
 
+from operators.gcp_container_operator import GKEPodOperator
 from utils.constants import ALLOWED_STATES, FAILED_STATES
-from utils.dataproc import get_dataproc_parameters, moz_dataproc_pyspark_runner
 from utils.tags import Tag
 
 default_args = {
@@ -30,17 +29,13 @@ default_args = {
     "email": [
         "telemetry-alerts@mozilla.com",
         "benwu@mozilla.com",
+        "bhood@mozilla.com",
     ],
     "email_on_failure": True,
     "email_on_retry": True,
     "retries": 2,
     "retry_delay": datetime.timedelta(minutes=20),
 }
-
-PIP_PACKAGES = [
-    "git+https://github.com/mozilla/python_moztelemetry.git@v0.10.8#egg=python-moztelemetry",
-    "git+https://github.com/FirefoxGraphics/telemetry.git#egg=bigquery_shim&subdirectory=analyses/bigquery_shim",
-]
 
 GCS_BUCKET = "moz-fx-data-static-websit-8565-analysis-output"
 GCS_PREFIX = "gfx/telemetry-data/"
@@ -68,76 +63,46 @@ with DAG(
         dag=dag,
     )
 
-    params = get_dataproc_parameters("google_cloud_airflow_dataproc")
-
-    graphics_trends = SubDagOperator(
+    graphics_trends = GKEPodOperator(
         task_id="graphics_trends",
+        image="us-docker.pkg.dev/moz-fx-data-artifacts-prod/docker-etl/graphics-dashboard:latest",
+        arguments=[
+            "graphics_dashboard.trends",
+            "--output-bucket",
+            GCS_BUCKET,
+            "--output-prefix",
+            GCS_PREFIX,
+            "--sample-id-count",
+            "1",
+            "--billing-project",
+            "mozdata",
+            "--end-date",
+            "{{ ds }}",
+            "--start-date",
+            "{{ macros.ds_add(ds, -28) }}"
+        ],
         dag=dag,
-        subdag=moz_dataproc_pyspark_runner(
-            parent_dag_name=dag.dag_id,
-            image_version="1.5-debian10",
-            dag_name="graphics_trends",
-            default_args=default_args,
-            cluster_name="graphics-trends-{{ ds }}",
-            job_name="graphics-trends",
-            python_driver_code="https://raw.githubusercontent.com/mozilla/python_mozetl/main/mozetl/graphics/graphics_telemetry_trends.py",
-            init_actions_uris=[
-                "gs://dataproc-initialization-actions/python/pip-install.sh"
-            ],
-            additional_metadata={"PIP_PACKAGES": " ".join(PIP_PACKAGES)},
-            additional_properties={
-                "spark:spark.jars": "gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar",
-            },
-            py_args=[
-                "--gcs-bucket",
-                GCS_BUCKET,
-                "--gcs-prefix",
-                GCS_PREFIX,
-                "--weekly-fraction",
-                "0.003",
-            ],
-            idle_delete_ttl=14400,
-            num_workers=2,
-            worker_machine_type="n2-standard-4",
-            gcp_conn_id=params.conn_id,
-            service_account=params.client_email,
-            storage_bucket=params.storage_bucket,
-        ),
     )
 
-    graphics_dashboard = SubDagOperator(
+    graphics_dashboard = GKEPodOperator(
         task_id="graphics_dashboard",
+        image="us-docker.pkg.dev/moz-fx-data-artifacts-prod/docker-etl/graphics-dashboard:latest",
+        arguments=[
+            "graphics_dashboard.dashboard",
+            "--output-bucket",
+            GCS_BUCKET,
+            "--output-prefix",
+            GCS_PREFIX,
+            "--sample-id-count",
+            "1",
+            "--billing-project",
+            "mozdata",
+            "--end-date",
+            "{{ ds }}",
+            "--time-window",
+            "14",
+        ],
         dag=dag,
-        subdag=moz_dataproc_pyspark_runner(
-            parent_dag_name=dag.dag_id,
-            image_version="1.5-debian10",
-            dag_name="graphics_dashboard",
-            default_args=default_args,
-            cluster_name="graphics-dashboard-{{ ds }}",
-            job_name="graphics-dashboard",
-            python_driver_code="https://raw.githubusercontent.com/mozilla/python_mozetl/main/mozetl/graphics/graphics_telemetry_dashboard.py",
-            init_actions_uris=[
-                "gs://dataproc-initialization-actions/python/pip-install.sh"
-            ],
-            additional_metadata={"PIP_PACKAGES": " ".join(PIP_PACKAGES)},
-            additional_properties={
-                "spark:spark.jars": "gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar",
-            },
-            py_args=[
-                "--output-bucket",
-                GCS_BUCKET,
-                "--output-prefix",
-                GCS_PREFIX,
-                "--release-fraction",
-                "0.003",
-            ],
-            idle_delete_ttl=14400,
-            num_workers=2,
-            worker_machine_type="n2-highmem-4",
-            gcp_conn_id=params.conn_id,
-            service_account=params.client_email,
-            storage_bucket=params.storage_bucket,
-        ),
     )
 
     wait_for_main_ping >> graphics_trends
