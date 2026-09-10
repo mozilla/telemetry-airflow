9884a1a225bde996c2aa9017a4ae1cd1b36ef928
Anna Scholtz <anna@scholtzan.net>
2026-09-09 20:56:02 -0700

    Revert "chore(stable-write): Use stable-write SA in shredder and copy_dedupe …"
    This reverts commit 50cdfa584269a28a2d50e09088b1becd6791cf7e.

--- dags/shredder.py
+++ dags/shredder.py
@@ -86,7 +86,6 @@ common_task_args = {
     "on_finish_action": OnFinishAction.DELETE_POD.value,
     "reattach_on_restart": True,
     "dag": dag,
-    "service_account_name": "stable-table-editor",
 }
 
 # handle telemetry main and main use counter separately to ensure they run continuously
